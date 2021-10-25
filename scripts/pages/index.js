3628d79d5f658e64b31e7423e1a375ff8fa7b91b
atoulmet <alexia.toulmet@gmail.com>
2021-10-25 23:42:40 +0200

    comment code

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -27,14 +27,16 @@
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
+
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM()
+            const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
     };
 
     async function init() {
+        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
