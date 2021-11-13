c6a47e9ed9e82208d8e362dcb71b83726fc6f717
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-13 20:51:27 +0100

    Afficher les infos des photographs

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,30 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const localisationEx = `${city}, ${country}`;
+    const prixEx = `${price}€/jour`;
 
     function getUserCardDOM() {
+        //Setup elements
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const nom = document.createElement( 'h2' );
+        nom.textContent = name;
+        const loca = document.createElement( 'h3' );
+        loca.textContent = localisationEx;
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+        const prix = document.createElement( 'h4' );
+        prix.textContent = prixEx;
+
+        //Append
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(nom); 
+        article.appendChild(loca);
+        article.appendChild(tag);
+        article.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }
