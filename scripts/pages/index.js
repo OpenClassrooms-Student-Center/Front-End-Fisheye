f1b302b174692bb547a4f41eabc6aee2f50935e5
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-15 17:13:46 +0100

    Fetch DATA From json files + Display in index.html

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,86 +1,35 @@
 /*--------- DOM ELEMENTS ---------*/
- 
+let thePhotographers;
+let theMedia;
 /*--------- EVENTS ---------*/
 
 /*--------- FUNCTIONS ---------*/
-async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Mimi Keel",
-            "id": 243,
-            "city": "London",
-            "country": "UK",
-            "tagline": "Voir le beau dans le quotidien",
-            "price": 400,
-            "portrait": "MimiKeel.jpg"
-        },
-        {
-            "name": "Ellie-Rose Wilkens",
-            "id": 930,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Capturer des compositions complexes",
-            "price": 250,
-            "portrait": "EllieRoseWilkens.jpg"
-        },
-        {
-            "name": "Tracy Galindo",
-            "id": 82,
-            "city": "Montreal",
-            "country": "Canada",
-            "tagline": "Photographe freelance",
-            "price": 500,
-            "portrait": "TracyGalindo.jpg"
-        },
-        {
-            "name": "Nabeel Bradford",
-            "id": 527,
-            "city": "Mexico City",
-            "country": "Mexico",
-            "tagline": "Toujours aller de l'avant",
-            "price": 350,
-            "portrait": "NabeelBradford.jpg"
-        },
-        {
-            "name": "Rhode Dubois",
-            "id": 925,
-            "city": "Barcelona",
-            "country": "Spain",
-            "tagline": "Je crée des souvenirs",
-            "price": 275,
-            "portrait": "RhodeDubois.jpg"
-        },
-        {
-            "name": "Marcel Nikolic",
-            "id": 195,
-            "city": "Berlin",
-            "country": "Germany",
-            "tagline": "Toujours à la recherche de LA photo",
-            "price": 300,
-            "portrait": "MarcelNikolic.jpg"
-        }
-    ]
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers]})
-}
 
-async function displayData(photographers) {
+const fetchPhotographers = async () => {
+    await fetch('./data/photographers.json')
+    .then( function  (resp) {
+        return resp.json();
+    })
+    .then( function (data) {
+        console.log(data);
+        thePhotographers = data.photographers;
+        theMedia = data.media;
+        console.log(thePhotographers);
+    });
+};
+
+const photographersDisplay = async () => {
+    await fetchPhotographers();
+
     const photographersSection = document.querySelector(".photographer_section");
 
-    photographers.forEach((photographer) => {
+    thePhotographers.forEach((photographer) => {
         const photographerModel = photographerFactory(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
         photographersSection.appendChild(userCardDOM);
     });
-};
+}
 
-async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-    
-init();
+photographersDisplay();
+
     
