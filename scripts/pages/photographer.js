8824461222a9869fce5e95000e439b2fb6ccc99f
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-11-30 23:04:05 +0100

    Get data of 1 photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -5,8 +5,33 @@
 //Recuperer l'id du photographe
 const photographe = window.location.search.split("?").join("");
 
-
 /*--------- EVENTS ---------*/
+const fetchPhotographers = async () => {
+    await fetch('./data/photographers.json')
+        .then( function  (resp) {
+            return resp.json();
+        })
+        .then( function (data) {
+            console.log(data);
+            thePhotographers = data.photographers;
+            // theMedia = data.media;
+
+            for (let i = 0; i < thePhotographers.length; i++) {
+
+                if(thePhotographers[i].id == photographe){
+                    console.log(thePhotographers[i]);
+                }
+            }
+        });
+};
+
+
+const photographersDisplay = async () => {
+    await fetchPhotographers();
+}
+
+photographersDisplay();
+
 
 
 /*--------- FUNCTIONS ---------*/
