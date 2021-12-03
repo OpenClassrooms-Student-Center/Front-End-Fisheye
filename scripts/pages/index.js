e080cbc4e88c84139b0ec8bb33c1b443d81486b5
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-12-03 18:46:11 +0100

    Toggle filter + css update

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -40,11 +40,20 @@ const photographersDisplay = async () => {
         const p = document.createElement( 'p' );
         p.textContent = '#'+tag;
         photographerTag.appendChild(p);
-        tagFilters.appendChild(photographerTag);
+        //Event when a tag is selected
         photographerTag.addEventListener("click",() => {
             photographerTag.classList.toggle("active")
             if (photographerTag.classList.contains("active")) {
+                photographersSection.innerHTML = "";
+
+                
                 console.log(tag);
+            } else {               
+                thePhotographers.forEach((photographer) => {
+                    const photographerModel = photographerFactory(photographer);
+                    const userCardDOM = photographerModel.getUserCardDOM();
+                    photographersSection.appendChild(userCardDOM);
+                });
             }
         });
     });
