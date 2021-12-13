61c33fdc3f00ed5393f7227719fecdbb34b00222
Johan <johan.sanniercaffier@gmail.com>
2021-12-13 12:01:27 +0100

    Ajout du dossier photos

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photos/profile/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
