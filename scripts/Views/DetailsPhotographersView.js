16c6d87bab8a7f19546702b5b851db9bd0ee289c
lea-bana <barreau.lea@gmail.com>
2021-12-17 12:51:48 +0100

    add medias + photographer page

--- scripts/Views/DetailsPhotographersView.js
+++ scripts/Views/DetailsPhotographersView.js
@@ -1,11 +1,13 @@
 class DetailsPhotographersView {
   static async showDetailsPhotographer(photographer) {
     const photographersSection = document.querySelector(".photograph_header");
+    console.log(photographer);
+    const picture = `assets/photographers/${photographer.portrait}`;
 
     let html = `
     
-    <div class="photograph-info">
-    <div class=photograph-name>
+    <div class="photograph-header">
+    <div class="photograph-name">
     <h1>${photographer.name}</h1>
     </div>
     <div class="photograph-txt">
@@ -13,18 +15,30 @@ class DetailsPhotographersView {
       <p>${photographer.tagline}</p>
     </div>
 </div>
-  <button class="contact_button">Contactez-moi</button>
-  <div class="photograph-img">
-    <img src="${photographer.picture}" alt="${photographer.alt}"/>
-  </div>  
+
+<button class="contact_button" onclick="displayModal()">
+Contactez-moi
+</button>
+
+<div class="photograph-img">
+  <img src="${picture}" class="cardpicture"></img>
+</div>  
     `;
 
     photographersSection.innerHTML = html;
-
-    /*photographers.forEach((photographer) => {
-      const photographerModel = photographerPageFactory(photograph);
-      const userBigCardDOM = photographerModel.getPhotographerCardDOM();
-      photographersSection.appendChild(userBigCardDOM);
-    });*/
+  }
+  static async showListMediasPhotographer(medias, photographer) {
+    const mediasSection = document.querySelector(".photograph_medias");
+    for (let index = 0; index < medias.length; index++) {
+      const enregmedia = medias[index];
+      console.log(enregmedia.image);
+      const pictures = `assets/Sample Photos/${photographer.name}/${enregmedia.image}`;
+      const video = `assets/Sample Photos/${photographer.name}/${enregmedia.video}`;
+      console.log(video);
+      let newImage = document.createElement("img");
+      newImage.setAttribute("src", pictures);
+      newImage.setAttribute("class", "picturespage");
+      mediasSection.appendChild(newImage);
+    }
   }
 }
