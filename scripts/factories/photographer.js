7ef9f18aad8eb8a117130a24f34d7d44e182363f
Ambre GONDRY <gondry.ambre@gmail.com>
2022-01-05 16:09:38 +0100

    add home css + update photographers.json + add home link on headers logo

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -10,29 +10,31 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' ); // création du container
 
         const photographerLink = document.createElement('a'); // création du lien vers le profile du photographe
-        photographerLink.setAttribute('href', `../photographer.html?id=${id}`); // définition de l'url
+        photographerLink.setAttribute('href', `http://localhost:5500/photographer.html?id=${id}`); // définition de l'url
         article.appendChild(photographerLink); // définition du lien comme enfant du container
 
-        const img = document.createElement( 'img' ); // création de la photo de profil
-        img.setAttribute("src", picture); // définition de l'image
-        img.setAttribute("alt", `${name}`); // définition du nom de l'image
-        photographerLink.appendChild(img); // est l'enfant du lien du profil
+        const profilePicture = document.createElement( 'img' ); // création de la photo de profil
+        profilePicture.setAttribute("src", picture); // définition de l'image
+        profilePicture.setAttribute("alt", `${name}`); // définition du nom de l'image
+        photographerLink.appendChild(profilePicture); // est l'enfant du lien du profil
 
-        const h2 = document.createElement( 'h2' ); // création du nom du profil
-        h2.textContent = name; // on affiche le nom sous forme de texte
-        photographerLink.appendChild(h2); // est l'enfant du lien du profil
+        const profileName = document.createElement( 'h2' ); // création du nom du profil
+        profileName.textContent = name; // on affiche le nom sous forme de texte
+        photographerLink.appendChild(profileName); // est l'enfant du lien du profil
 
-        const h3 = document.createElement( 'h3' ); // création de la localisation du photographe
-        h3.textContent = location; // on l'affiche sous forme de texte
-        photographerLink.appendChild(h3); // est l'enfant du lien
+        const profileLocation = document.createElement( 'h3' ); // création de la localisation du photographe
+        profileLocation.textContent = location; // on l'affiche sous forme de texte
+        photographerLink.appendChild(profileLocation); // est l'enfant du lien
 
-        const p = document.createElement( 'p' ); // création de la citation
-        p.textContent = tagline; // qu'on affiche sous forme de texte
-        photographerLink.appendChild(p); // est l'enfant du lien
+        const profileQuote = document.createElement( 'p' ); // création de la citation
+        profileQuote.setAttribute('class', 'quote');
+        profileQuote.textContent = tagline; // qu'on affiche sous forme de texte
+        photographerLink.appendChild(profileQuote); // est l'enfant du lien
 
-        const rate = document.createElement( 'p' ); // création du tarif
-        rate.textContent = dailyRate; // affichage sous forme de texte
-        photographerLink.appendChild(rate); // enfant du lien
+        const profileRate = document.createElement( 'p' ); // création du tarif
+        profileRate.setAttribute('class', 'rate');
+        profileRate.textContent = dailyRate; // affichage sous forme de texte
+        photographerLink.appendChild(profileRate); // enfant du lien
 
         return (article); // on retourne la carte du photographe
     }
