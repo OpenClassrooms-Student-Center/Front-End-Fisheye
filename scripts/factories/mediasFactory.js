3e1413106d215391b7c7ee71c9ecaa7c5eac9246
Maera <baeitsmae@gmail.com>
2022-01-17 17:07:04 +0100

    test

--- scripts/factories/mediasFactory.js
+++ scripts/factories/mediasFactory.js
@@ -1,30 +1,32 @@
 function mediasFactory(data) {
-    const { name, portrait, city, country, tagline, price , id } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
-        const link = document.createElement('a');
-            link.setAttribute("href", id + ".html");
+      
         const img = document.createElement('img');
             img.setAttribute("src", picture); 
+
         const photographerName = document.createElement('h2');
             photographerName.textContent = name;
+
         const photographerCity = document.createElement('p');
             photographerCity.textContent = city + ', ' + country; 
             photographerCity.style.fontSize = "13px";
             photographerCity.style.color = "#901C1C";
             photographerCity.style.marginBottom = "3px";
+
         const photographerTagline = document.createElement('p');
             photographerTagline.textContent = tagline;
             photographerTagline.style.fontSize = "10px";
             photographerTagline.style.marginBottom = "3px";
+
         const photographerPrice = document.createElement('p');
             photographerPrice.textContent = price + '€/jour';
             photographerPrice.style.fontSize = "9px";
 
-        article.appendChild(link);
         article.appendChild(photographerName);
         article.appendChild(photographerCity);
         article.appendChild(photographerTagline);
@@ -34,6 +36,6 @@ function mediasFactory(data) {
         return (article);
         
     }
-    return { name, picture, city, country, tagline, price, id, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
 
