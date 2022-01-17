5095443ba523c9ad44de2b2869d759d691e44c2a
Maera <baeitsmae@gmail.com>
2022-01-17 23:38:20 +0100

    mediasFactory

--- scripts/factories/mediasFactory.js
+++ scripts/factories/mediasFactory.js
@@ -33,18 +33,81 @@ function profileFactory(data) {
             photographerTagline.style.fontSize = "18px";
             photographerTagline.style.marginTop = "20px";
 
+        const priceContainer = document.createElement('div');
+            priceContainer.style.background = "#D88876";
+            priceContainer.style.position = "fixed";
+            priceContainer.style.bottom = "0px";
+            priceContainer.style.right = "30px";
+            priceContainer.style.height = "50px";
+            priceContainer.style.padding = "0 20px";
+
+        const body = document.querySelector('body');
+
         const photographerPrice = document.createElement('p');
             photographerPrice.textContent = price + '€/jour';
+            photographerPrice.style.fontSize = "24px";
+            photographerPrice.style.lineHeight = "50px";
 
         profile.appendChild(photographerName);
         profile.appendChild(photographerCity);
         profile.appendChild(photographerTagline);
         headerPh.appendChild(profile);
         headerPh.appendChild(img);
+        body.appendChild(priceContainer);
+        priceContainer.appendChild(photographerPrice);
         
         return (profile);
         
     }
     return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
+
+/////////////////////////////////////////////////////////////////////////////
+
+function mediasFactory(data) {
+    const { image, video, title, likes } = data;
+
+    let media = `assets/medias/Mimi/${image}`;
+
+    function getMediasCardDOM() {
+
+      const figure = document.createElement('article');
+            figure.style.marginBottom = "40px";
+            figure.style.width = "350px";
+
+        const img = document.createElement('img');
+            img.setAttribute("src", media);
+            img.style.width = "350px";
+            img.style.height = "300px";
+            img.style.objectFit = "cover"; 
+
+        const mediaCaption = document.createElement('div');
+            mediaCaption.style.display = "flex";
+            mediaCaption.style.justifyContent = "space-between";
+
+        const mediaTitle = document.createElement('p');
+            mediaTitle.textContent = title;
+            mediaTitle.style.fontSize = "20px";
+            mediaTitle.style.color = "#901C1C";
+
+        const mediaLike = document.createElement('p');
+            mediaLike.innerHTML = likes + '<i class="fas fa-heart"></i>';
+            mediaLike.style.fontSize = "20px";
+            mediaLike.style.color = "#901C1C";
+    
+        figure.appendChild(img);
+        figure.appendChild(mediaCaption);
+        mediaCaption.appendChild(mediaTitle);
+        mediaCaption.appendChild(mediaLike);
+
+
+        return (figure);
+        
+    }
+    return { image, video, title, likes, getMediasCardDOM }
+}
+
+
+
+
 
