73f4ad85e2d926ad2d79f9872c983031202be05c
Maera <baeitsmae@gmail.com>
2022-01-19 02:32:23 +0100

    Modification du lien (searchParams)

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,7 +6,8 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement('article');
         const link = document.createElement('a');
-            link.setAttribute("href", id + ".html");
+        //    link.setAttribute("href", id + ".html");
+        link.setAttribute("href", "photographer.html?id=" + id);
         const img = document.createElement('img');
             img.setAttribute("src", picture); 
         const photographerName = document.createElement('h2');
