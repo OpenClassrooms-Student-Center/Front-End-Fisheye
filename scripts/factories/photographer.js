02a7e0cc175683607591fcf8104b51fc8bb2f59c
inesodtap <odoultapiaines@gmail.com>
2022-01-19 12:09:13 +0100

    :hammer: add ESLint

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,12 +5,16 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
         article.appendChild(img);
         article.appendChild(h2);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
