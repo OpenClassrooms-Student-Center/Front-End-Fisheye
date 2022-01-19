3406da4eaf27eabf7934d01144c48261d703fd4c
Johan <johan.sanniercaffier@gmail.com>
2022-01-19 12:56:50 +0100

    Reset du code de la navigation fléchée diapo

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -396,7 +396,6 @@ function lightboxModal() {
           case 'ArrowLeft':
             newDataIndex--;
             lightbox.removeChild(newMedia);
-            // Appel d'une boucle sur tous les médias du photographe de la page afin de trouver le média qui correspond au nouveau data-index
             for (let index = 0; index < media.length; index++) {
               const element = media[index];
               const minusOne = media[0].getAttribute('data-index') - 1;
@@ -405,54 +404,19 @@ function lightboxModal() {
                 // Si ce média possède une source (est donc est une balise img)
                 if (element.src) {
                   // Je créé une nouvelle balise img à laquelle j'ajoute les informations du nouveau média avant de l'insérer dans le DOM
-                  newMedia = createImg(element, lightbox);
+                  createImg(element, lightbox);
                 } else {
                   // Si ce média ne possède pas de source, dans ce cas c'est une balise vidéo
                   // Je récupère les informations et la balise source que je passe dans des nouvelles balises crées avant de l'injecter dans le DOM
-                  newMedia = createVid(element, lightbox);
+                  createVid(element, lightbox);
                 }
-                break;
-              } else if (newDataIndex == minusOne) {
-                newDataIndex =
-                  media[media.length - 1].getAttribute('data-index');
+                // console.log(newMedia);
               }
             }
+            // ------------
             break;
           case 'ArrowRight':
-            newDataIndex++;
-            lightbox.removeChild(newMedia);
-            // Appel d'une boucle sur tous les médias du photographe de la page afin de trouver le média qui correspond au nouveau data-index
-            for (let index = 0; index < media.length; index++) {
-              const element = media[index];
-              const length = media[media.length - 1].getAttribute('data-index');
-              const parsed = parseInt(length, 10);
-              const One = 1;
-              const plusOne = One + parsed;
-              // Si il y a un élément qui correspond au nouveau data-index (un média suivant dans la liste)
-              if (element.getAttribute('data-index') == newDataIndex) {
-                // Si ce média possède une source (est donc est une balise img)
-                if (element.src) {
-                  // Je créé une nouvelle balise img à laquelle j'ajoute les informations du nouveau média avant de l'insérer dans le DOM
-                  newMedia = createImg(element, lightbox);
-                  // Si ce média ne possède pas de source, dans ce cas c'est une balise vidéo
-                } else {
-                  // Je récupère les informations et la balise source que je passe dans des nouvelles balises crées avant de l'injecter dans le DOM
-                  newMedia = createVid(element, lightbox);
-                }
-                break;
-              } else if (newDataIndex == plusOne) {
-                newDataIndex = media[0].getAttribute('data-index');
-                if (element.src) {
-                  // Je créé une nouvelle balise img à laquelle j'ajoute les informations du nouveau média avant de l'insérer dans le DOM
-                  newMedia = createImg(element, lightbox);
-                  // Si ce média ne possède pas de source, dans ce cas c'est une balise vidéo
-                } else {
-                  // Je récupère les informations et la balise source que je passe dans des nouvelles balises crées avant de l'injecter dans le DOM
-                  newMedia = createVid(element, lightbox);
-                }
-                break;
-              }
-            }
+            console.log(element);
             break;
           default:
             break;
