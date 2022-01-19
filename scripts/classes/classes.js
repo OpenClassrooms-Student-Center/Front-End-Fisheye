739e2365e405231b921bc63c8d458b9bc55d2719
Johan <johan.sanniercaffier@gmail.com>
2022-01-19 11:29:46 +0100

    WIP accessibilité clavier

---
+++ scripts/classes/classes.js
@@ -0,0 +1,13 @@
+// Création de l'objet profil du photographe
+// eslint-disable-next-line no-unused-vars
+class Profile {
+  constructor(name, city, country, tagline, portrait, price, id) {
+    this.name = name;
+    this.city = city;
+    this.country = country;
+    this.tagline = tagline;
+    this.portrait = portrait;
+    this.price = price;
+    this.id = id;
+  }
+}
