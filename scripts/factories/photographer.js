989414f99fc39166778aa53c20f08efd06fa1420
inesodtap <odoultapiaines@gmail.com>
2022-01-19 20:33:53 +0100

    :construction: Add data on home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -12,8 +12,25 @@ function photographerFactory(data) {
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
 
+        const locationCity = document.createElement('p');
+        locationCity.textContent = city;
+
+        const locationCountry = document.createElement ('p');
+        locationCountry.textContent = country;
+
+        const description = document.createElement ('p');
+        description.textContent = tagline;
+
+        const pricePerDay = document.createElement ('p');
+        pricePerDay.textContent = price;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(locationCity);
+        article.appendChild(locationCountry);
+        article.appendChild(description);
+        article.appendChild(pricePerDay);
+
 
         return (article);
     }
