9af03b3363163b7dfef07bc692a9fb084cff3e41
Maera <baeitsmae@gmail.com>
2022-01-20 03:40:19 +0100

    Insertions + style des medias des photographes

--- scripts/factories/mediasFactory.js
+++ scripts/factories/mediasFactory.js
@@ -1,13 +1,9 @@
-//FONCTION POUR AFFICHER LE PROFIL DU PHOTOGRAPHE 
-
-
-
 // FONCTION POUR AFFICHER LES MEDIAS DU PHOTOGRAPHE
-
 function mediasFactory(data) {
     const { image, video, title, likes } = data;
-
-    let media = `assets/medias/Mimi/${image}`;
+    
+    let mediaImage = `assets/medias/${image}`;
+    let mediaVideo = `assets/medias/${video}`;
 
     function getMediasCardDOM() {
 
@@ -15,12 +11,12 @@ function mediasFactory(data) {
             figure.style.marginBottom = "40px";
             figure.style.width = "350px";
 
-        const img = document.createElement('img');
-            img.setAttribute("src", media);
-            img.style.width = "350px";
-            img.style.height = "300px";
-            img.style.objectFit = "cover"; 
-
+    const img = document.createElement('img');
+        img.setAttribute("src", mediaImage);
+        img.style.width = "350px";
+        img.style.height = "300px";
+        img.style.objectFit = "cover";
+        
         const mediaCaption = document.createElement('div');
             mediaCaption.style.display = "flex";
             mediaCaption.style.justifyContent = "space-between";
@@ -34,12 +30,23 @@ function mediasFactory(data) {
             mediaLike.innerHTML = likes + '<i class="fas fa-heart"></i>';
             mediaLike.style.fontSize = "20px";
             mediaLike.style.color = "#901C1C";
+
+            if(video != undefined) {
+                const vid = document.createElement('video');
+                vid.setAttribute("src", mediaVideo);
+                vid.className = 'vid';
+                vid.style.width = "350px";
+                vid.style.height = "300px";
+                figure.appendChild(vid);
+                vid.style.objectFit = "cover";
+            };
     
         figure.appendChild(img);
         figure.appendChild(mediaCaption);
         mediaCaption.appendChild(mediaTitle);
         mediaCaption.appendChild(mediaLike);
 
+        
 
         return (figure);
         
