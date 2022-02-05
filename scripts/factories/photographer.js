d2dcc0331f55a484dc21917ab3a02944e2a2b248
Hotram89 <marion.dournel@gmail.com>
2022-02-05 15:00:44 +0100

    fix: factorise getUserCardDOM()

---
+++ scripts/factories/photographer.js
@@ -0,0 +1,34 @@
+// fonction pour lancer la creation des cards en html
+export async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+// fonction pour constuire des elements html
+function photographerFactory(data) {
+  const { name, portrait, city, country, tagline, price } = data;
+
+  function getUserCardDOM() {
+    const htmlArticle = `
+          <a class="photographer-link">
+              <img src="assets/photographers/${portrait}">
+              <h2>${name}</h2>
+              </a>
+          <div class="photographer-details">
+                      <h3>${city}, ${country}</h3>
+                      <h4>${tagline}</h4>
+                      <h5>${price}€ /jour</h5>
+          </div>`;
+    const article = document.createElement("article");
+    article.innerHTML = htmlArticle;
+
+    return article;
+  }
+
+  return { getUserCardDOM, name, portrait, city, country, tagline, price };
+}
