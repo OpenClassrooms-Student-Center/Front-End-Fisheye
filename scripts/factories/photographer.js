e7a2ef83655935958d53769f44f16482a56d4d77
rMaloberti <r.maloberti@yahoo.com>
2022-02-07 17:04:43 +0100

    feat(index): update photographerFactory function

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,14 +4,17 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        article.classList.add("user-card");
+        const img = document.createElement("img");
+        img.classList.add("user-card__img");
+        img.setAttribute("src", picture);
+        const h2 = document.createElement("h2");
+        h2.classList.add("user-card__title");
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
