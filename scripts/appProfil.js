2ff61f8b1866f3dd95c87a8322b8e0a300d7233d
Hotram89 <marion.dournel@gmail.com>
2022-02-16 19:59:30 +0100

    add medias and gallery section

--- scripts/appProfil.js
+++ scripts/appProfil.js
@@ -1,16 +1,20 @@
 import { getPhotographers } from "./factories/apiProvider.js";
-import { createHTML }  from "./factories/builderPageProfil.js";
+import { createHTML, buildGallery } from "./factories/builderPageProfil.js";
 
 ////////////////////////////////////////
 
 // aller chercher tous les photographes du json
 getPhotographers().then((res) => {
   const allPhotographers = res.photographers;
+  const allMedias = res.media;
   console.log(allPhotographers);
+  console.log(allMedias);
 
   //afficher tous les id des photographes du json
-  allPhotographers.forEach((artist) => compare(artist, photographerId))
-  return allPhotographers;
+  allPhotographers.forEach((artist) => compare(artist, photographerId));
+  //   return allPhotographers;
+  //affiche tous les medias des photographes du json
+  allMedias.forEach((pix) => findMedias(pix, photographerId));
 });
 
 let url = new URL(window.location.href);
@@ -21,9 +25,14 @@ console.log(photographerId);
 function compare(a, b) {
   if (a.id == b) {
     createHTML(a);
-    
-    
   }
 }
 
-getPhotographers()
+function findMedias(a, b) {
+  if (a.photographerId == b) {
+    console.log(a.title);
+    buildGallery(a);
+  }
+}
+
+getPhotographers();
--- scripts/factories/builderPageProfil.js
+++ scripts/factories/builderPageProfil.js
@@ -1,31 +1,42 @@
-
 ///creer l'image du photographe
 function createHTML(el) {
-
-    //creer l'image du photographe
-    const userImg = `
+  //creer l'image du photographe
+  const userImg = `
         <img src="./assets/photographers/${el.portrait}"></img>`;
-  
-    let image = document.createElement("div");
-    image.innerHTML = userImg;
-    const photographersHeader = document.querySelector(".photograph-header");
-    const contactBtn = document.querySelector(".contact_button");
-    photographersHeader.appendChild(image);
 
-    // creer le paragraphe dans le header
+  let image = document.createElement("div");
+  image.innerHTML = userImg;
+  const photographersHeader = document.querySelector(".photograph-header");
+  const contactBtn = document.querySelector(".contact_button");
+  photographersHeader.appendChild(image);
 
-    const htmlHeader = `
+  // creer le paragraphe dans le header
+
+  const htmlHeader = `
                   <h2>${el.name}</h2>       
               <div class="photographer-details">
                           <h3>${el.city}, ${el.country}</h3>
                           <h5>${el.tagline}</h5>
                          
               </div>`;
-    const article = document.createElement("article");
-    article.innerHTML = htmlHeader;
-    photographersHeader.insertBefore(article, contactBtn);
-  }
- 
+  const article = document.createElement("article");
+  article.innerHTML = htmlHeader;
+  photographersHeader.insertBefore(article, contactBtn);
+}
 
-export { createHTML}
+function buildGallery(el) {
+  //recuperer les elements du dom
+  const phtographGallery = document.querySelector(".gallerie");
 
+  const htmlInbox = `<a>
+    
+    <img src="./assets/photos/${el.image}"></img>
+    <h3>${el.title} </h3>
+    
+</a>`;
+  const article = document.createElement("article");
+  article.innerHTML = htmlInbox;
+  phtographGallery.appendChild(article);
+}
+
+export { createHTML, buildGallery };
