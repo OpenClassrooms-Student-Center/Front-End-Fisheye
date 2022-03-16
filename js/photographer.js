676a422f8fa76360150e96141c6ac8f6931ac4da
MarilyneD <marilynedestombes@gmail.com>
2022-03-16 15:11:19 +0100

    affichage photos

--- js/photographer.js
+++ js/photographer.js
@@ -24,51 +24,43 @@ async function getPhotographers() {
   }
   
   
-  async function init() {
-    let photographers = await getPhotographers();
-    const content = document.getElementById("photographer_section");
-    photographers.forEach((photographer) =>
-      content.appendChild(photographerCardFactory(photographer, photographers))
-    );
-  }
-  
 
 
 async function init() {
  
    let photographers = await getPhotographers();
-   console.log ("affichage de photographers obtenu par fetch",photographers);
+   //console.log ("affichage de photographers obtenu par fetch",photographers);
    //////////////////////////////////// lecture de paramètre du lien URL     /////////////                                    NEWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW////                            
    const queryParams = window.location.search;
-   console.log ("query params de window.location.search      ",queryParams);
+   //console.log ("query params de window.location.search      ",queryParams);
    const urlParams = new URLSearchParams (queryParams);
-   console.log ("new URLSearchParams     ",urlParams);
+   //console.log ("new URLSearchParams     ",urlParams);
    const photographerArtistId = urlParams.get ('id')
-   console.log ("id lue sur l'URL avec get      ",photographerArtistId);
+   //console.log ("id lue sur l'URL avec get      ",photographerArtistId);
    
    
    const choosenphotographer = photographers.find(function(item) {return item.id == photographerArtistId});
-   console.log ("choosenphotographer",choosenphotographer);
+   //console.log ("choosenphotographer",choosenphotographer);
    const index = photographers.indexOf(choosenphotographer);
-   console.log("index", index);
+   //console.log("index", index);
  
    const content = document.getElementById("photograph-header");
    content.appendChild(photographerHeader(photographers[index]));
 
 
  ///   ajout des medias ///
- 
-
+ let medias = await getMedias();
+ console.log ("voici les médias obtenus par fetch" ,medias);
+ const choosenGallery = medias.filter(function(item){return item.photographerId == photographerArtistId});
+ console.log ("résultat du filter avec l'ID du photographe",choosenGallery);
+ const gallery = document.getElementById("gallery");
+ choosenGallery.forEach((item) =>
+    gallery.appendChild(photographerGallery(photographers[index],item))
+  );
 }
   
 
  init();
-
-
-
-
-
-
 
 
 
@@ -104,12 +96,22 @@ function photographerHeader(photographer) {
 
 
 
-
-
-
 /////////////////// create photographer gallery ///////////////////
 
+function photographerGallery(photographer,photo) {
+  const photoCard = document.createElement("article");
+  const photoCardImg = document.createElement("img");
+  const photoCardTitle = document.createElement("h2");
+  photoCardImg.src = "../assets/photographers/" + photographer.name + "/" + photo.image;
+  photoCardTitle.textContent = photo.title;
+ //console.log(photoCardImg.src);
 
+
+  photoCard.appendChild(photoCardImg);
+  photoCard.appendChild(photoCardTitle);
+
+return photoCard;
+}
 
 
 
