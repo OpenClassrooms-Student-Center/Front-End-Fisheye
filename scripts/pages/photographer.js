99d9b3fc8b7ce3cc5d24087f26a161a2d7a72827
Madeline <grammaticomadeline@hotmail.fr>
2022-03-24 17:31:43 +0100

    fix(get articleId) : return photographer object by id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,19 +1,20 @@
 //Mettre le code JavaScript lié à la page photographer.html
 async function initPhotographe() {
     const articleId =  getArticleId();
-    const article = getArticle(articleId);
+    const article = await getArticle(articleId);
+    
 }
 
 function getArticleId() {
     return new URL(location.href).searchParams.get("id")
 }
 
-async function getArticle() {
-    return fetch("../data/photographers.json")
-        .then(res=> res.json())
-        .then(data => console.table(data.photographers))
-        
+async function getArticle(articleId) {
+    fetch("../data/photographers.json")
+        .then(res => res.json())
+        .then(data => {
+            return data.photographers.filter((photographe)=> photographe.id === parseInt(articleId, 10))
+        })
 }
 
 initPhotographe ()
-export default initPhotographe
