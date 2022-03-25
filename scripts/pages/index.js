016bd7d4937e28eceb0531b074eabd0a6b109f73
ERIC CLERMONTET <emc@go4.social>
2022-03-25 14:26:34 +0100

    getphotographers fetch promise V1

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,15 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+        return fetch("data/photographers.json")
+            .then(response => {
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+            return response.json();
+          })
+            .then(data => {const photographers = data.photographers;
+            return ({photographers})
+            });
+        }
+        console.log(getPhotographers());
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
