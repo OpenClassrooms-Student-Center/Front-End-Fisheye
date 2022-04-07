e8cc9d41dbf2c30a6bf795d3cdbf99b7764fce89
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-04-07 16:59:14 +0200

    feat(index): add call to data

---
+++ scripts/factories/PhotographersFactory.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+function photographerFactory (data) {
+  const { name, portrait } = data
+
+  const picture = `assets/photographers/${portrait}`
+
+  function getUserCardDOM () {
+    const article = document.createElement('article')
+    const img = document.createElement('img')
+    img.setAttribute('src', picture)
+    const h2 = document.createElement('h2')
+    h2.textContent = name
+    article.appendChild(img)
+    article.appendChild(h2)
+    return (article)
+  }
+
+  return {
+    name,
+    picture,
+    getUserCardDOM
+  }
+}
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,27 +1,14 @@
 async function getPhotographers () {
-  // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      name: 'Ma data test',
-      id: 1,
-      city: 'Paris',
-      country: 'France',
-      tagline: 'Ceci est ma data test',
-      price: 400,
-      portrait: 'account.png'
-    },
-    {
-      name: 'Autre data test',
-      id: 2,
-      city: 'Londres',
-      country: 'UK',
-      tagline: 'Ceci est ma data test 2',
-      price: 500,
-      portrait: 'account.png'
-    }
-  ]
-  // et bien retourner le tableau photographers seulement une fois
-  return ({ photographers: [...photographers, ...photographers, ...photographers] })
+  const photographersData = fetch('./data/photographers.json')
+    .then(function (res) {
+      if (res.ok) {
+        return res.json()
+      }
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+  return photographersData
 }
 
 async function displayData (photographers) {
