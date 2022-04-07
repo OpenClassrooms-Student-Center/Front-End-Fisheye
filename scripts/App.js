af5bb2b8cfe6cad347de63c6b9e727d0afba9b2f
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-04-07 16:21:39 +0200

    feat(eslint): add eslint default settings + fix

---
+++ scripts/App.js
@@ -0,0 +1,12 @@
+class App {
+  constructor () {
+    console.log('App constructor')
+  }
+
+  async main () {
+    console.log('App main')
+  }
+}
+
+const app = new App()
+app.main()
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,1 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Mettre le code JavaScript lié à la page photographer.html
