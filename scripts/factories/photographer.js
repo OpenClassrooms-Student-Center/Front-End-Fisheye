f1d5e69cf1d8f26c94441161749c78a93584c2ef
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-04-07 23:53:57 +0200

    feat(index): css and accessibility

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -14,12 +14,37 @@ function photographerFactory (data) {
 
   function getUserCardDOM () {
     const article = document.createElement('article')
+    article.classList.add('photographerCard')
+    const photographerLink = document.createElement('a')
+    photographerLink.setAttribute('href', 'photographer.html?id=' + id)
+    photographerLink.setAttribute('title', `Lien vers le photographe ${name}`)
+    photographerLink.classList.add('photographerCard__link')
     const img = document.createElement('img')
+    img.classList.add('photographerCard__img')
     img.setAttribute('src', picture)
+    img.setAttribute('alt', '')
     const h2 = document.createElement('h2')
+    h2.classList.add('photographerCard__title')
     h2.textContent = name
-    article.appendChild(img)
-    article.appendChild(h2)
+    photographerLink.appendChild(img)
+    photographerLink.appendChild(h2)
+    article.appendChild(photographerLink)
+    const photographerInfos = document.createElement('div')
+    photographerInfos.classList.add('photographerCard__infos')
+    const locationElt = document.createElement('p')
+    locationElt.textContent = `${city}, ${country}`
+    locationElt.classList.add('photographerCard__infos--location')
+    const taglineElt = document.createElement('p')
+    taglineElt.textContent = tagline
+    taglineElt.classList.add('photographerCard__infos--tagline')
+    const priceElt = document.createElement('p')
+    priceElt.textContent = `${price}€/jour`
+    priceElt.classList.add('photographerCard__infos--price')
+    photographerInfos.appendChild(locationElt)
+    photographerInfos.appendChild(taglineElt)
+    photographerInfos.appendChild(priceElt)
+    article.appendChild(photographerInfos)
+
     return (article)
   }
 
@@ -27,7 +52,6 @@ function photographerFactory (data) {
     city,
     country,
     id,
-    location: `${city}, ${country}`,
     name,
     portrait,
     price,
