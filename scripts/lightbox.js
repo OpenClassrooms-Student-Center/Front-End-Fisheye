643030bb9766f6722de342c61fcf11ce74a0ba7c
loic <loicanaelbdf@gmail.com>
2022-04-10 04:29:43 -0700

    adding 'previous' and 'next' event on the lightbox

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,22 +1,22 @@
 export default class LightBox {
-    constructor() {
-        this.currentIndex = 0;
-    }
+  constructor(){
+      this.currentIndex = 0
+  }
     // Initialisation lightbox sur le click + navigation
     lightboxOpen(mediaItems, mediaName) {
         let selectMedias = document.querySelectorAll('.ph-media');
-        console.log(selectMedias);
-        selectMedias.forEach((media, index) => media.addEventListener("click", () => {
+        selectMedias.forEach((media,index) => media.addEventListener("click", () => {
             let lightBoxMedia = document.getElementById('lightbox-media');
             let lightBoxName = document.getElementById('lightbox-name');
             let mediaDisplay = mediaItems[index];
             let nameDisplay = mediaName[index];
-            document.getElementById('lightbox').style.display = 'block';
+            let openLightbox = document.querySelector('.lightbox');
+            openLightbox.classList.add('show');
             lightBoxMedia.innerHTML = `${mediaDisplay}`;
             lightBoxName.innerHTML = `${nameDisplay}`;
-            this.currentIndex = index;
         }))
-
+        this.previous(mediaItems, mediaName);
+        this.next(mediaItems, mediaName);
         this.close();
         return this;
     }
@@ -24,8 +24,42 @@ export default class LightBox {
     // Fermeture de la lightbox
     close() {
         document.querySelector('.close-lightbox').addEventListener('click', () => {
-            let lightbox = document.getElementById('lightbox');
-            lightbox.style.display = 'none';
+            let lightbox = document.querySelector('.lightbox');
+            lightbox.classList.remove('show');
+        })
+    }
+    // Média précédent
+    previous(mediaItems, mediaName) {
+        let previous = document.getElementById('previous')
+        previous.addEventListener('click', () => {
+            this.currentIndex -= 1;
+            //si l'index est inferieure a 0 on la reaffecte la valeur du dernier element des medias
+            if (this.currentIndex < 0) {
+                this.currentIndex = mediaItems.length - 1;
+            }
+            let lightBoxMedia = document.getElementById('lightbox-media');
+            let lightBoxName = document.getElementById('lightbox-name');
+            let mediaDisplay = mediaItems[this.currentIndex];
+            let nameDisplay = mediaName[this.currentIndex];
+            lightBoxMedia.innerHTML = `${mediaDisplay}`;
+            lightBoxName.innerHTML = `${nameDisplay}`;
+        })
+    }
+    // Média suivant
+    next(mediaItems, mediaName) {
+        let next = document.getElementById('next')
+        next.addEventListener('click', () => {
+            this.currentIndex += 1;
+            //si l'index est au dernier element des medias, on reinitialise sa valeur 
+            if (this.currentIndex > mediaItems.length - 1) {
+                this.currentIndex = 0;
+            }
+            let lightBoxMedia = document.getElementById('lightbox-media');
+            let lightBoxName = document.getElementById('lightbox-name');
+            let mediaDisplay = mediaItems[this.currentIndex];
+            let nameDisplay = mediaName[this.currentIndex];
+            lightBoxMedia.innerHTML = `${mediaDisplay}`;
+            lightBoxName.innerHTML = `${nameDisplay}`;
         })
     }
   
