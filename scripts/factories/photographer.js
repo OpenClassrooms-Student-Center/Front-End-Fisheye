79f3a32e2a85299e9c9cbfd49c9c62d2108148d4
Wilfried <wilfriedprombo18@gmail.com>
2022-04-20 16:20:25 +0100

    :construction: Add photographer's information in the header of his page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,47 +1,87 @@
 function photographerFactory(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+  function getUserCardDOM() {
+    const article = document.createElement("article");
 
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
 
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
 
-        const a = document.createElement( 'a' );
-        a.setAttribute("href", `/photographer.html?photographerId=${id}`);
-        a.setAttribute("aria-label", `${name}`);
+    const a = document.createElement("a");
+    a.setAttribute("href", `/photographer.html?photographerId=${id}`);
+    a.setAttribute("aria-label", `${name}`);
 
-        const p = document.createElement( 'p' );
+    const p = document.createElement("p");
 
-        const spanLocation = document.createElement( 'span' );
-        spanLocation.setAttribute("class", "location");
-        spanLocation.textContent = `${city}, ${country}`;
+    const spanLocation = document.createElement("span");
+    spanLocation.setAttribute("class", "location");
+    spanLocation.textContent = `${city}, ${country}`;
 
-        const spanTagline = document.createElement( 'span' );
-        spanTagline.setAttribute("class", "tagline");
-        spanTagline.textContent = `${tagline}`;
+    const spanTagline = document.createElement("span");
+    spanTagline.setAttribute("class", "tagline");
+    spanTagline.textContent = `${tagline}`;
 
-        const spanPrice = document.createElement( 'span' );
-        spanPrice.setAttribute("class", "price");
-        spanPrice.textContent = `${price}/jour`;
-        
-        a.appendChild(img);
-        a.appendChild(h2);
+    const spanPrice = document.createElement("span");
+    spanPrice.setAttribute("class", "price");
+    spanPrice.textContent = `${price}/jour`;
 
-        p.appendChild(spanLocation);
-        p.appendChild(spanTagline);
-        p.appendChild(spanPrice);
+    a.appendChild(img);
+    a.appendChild(h2);
 
-        article.appendChild(a);
-        article.appendChild(p);
-        
-        return article;
-    }
+    p.appendChild(spanLocation);
+    p.appendChild(spanTagline);
+    p.appendChild(spanPrice);
 
-    return { name, id, city, country, tagline, price, picture, getUserCardDOM };
+    article.appendChild(a);
+    article.appendChild(p);
+
+    return article;
+  }
+
+  function getPhotographerHeader() {
+    const photographerHeader = document.querySelector(".photographer_header");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("class", "photographer_header__img");
+
+    const h2 = document.createElement("h2");
+    h2.setAttribute("class", "name");
+    h2.textContent = name;
+    
+    const spanLocation = document.createElement("span");
+    spanLocation.setAttribute("class", "location");
+    spanLocation.textContent = `${city}, ${country}`;
+
+    const spanTagline = document.createElement("span");
+    spanTagline.setAttribute("class", "tagline");
+    spanTagline.textContent = `${tagline}`;
+
+    const p = document.createElement("p");
+
+    p.appendChild(h2);
+    p.appendChild(spanLocation);
+    p.appendChild(spanTagline);
+    p.setAttribute("class", "photographer_header__content");
+
+    photographerHeader.appendChild(p);
+    photographerHeader.appendChild(img);
+  }
+
+  return {
+    name,
+    id,
+    city,
+    country,
+    tagline,
+    price,
+    picture,
+    getUserCardDOM,
+    getPhotographerHeader,
+  };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,42 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+  // Retourne le tableau de photographes
+  const photographerApi = new PhotographerApi("/data/photographers.json");
+  return photographerApi.get();
+}
+
+async function displayPhotographerHeader(photographers) {
+  // Récupère l'id d'un photographe
+  const id = await getPhotographerId();
+
+  // filtre les données du photographe correspondant à l'id récupéré
+  const photographer = await filterDataOnePhotographer(photographers, id);
+
+  const photographerModel = photographerFactory(photographer);
+
+  // Affiche l'entête du photographe
+  photographerModel.getPhotographerHeader();
+}
+
+async function getPhotographerId() {
+  const link = window.location.search;
+  const searchParams = new URLSearchParams(link);
+
+  // Retourne l'id du photographe contenu dans le lien
+  return parseInt(searchParams.get("photographerId"), 10);
+}
+
+async function filterDataOnePhotographer(photographers, photographerId) {
+  return photographers.filter(
+    (photographer) => photographer.id === photographerId
+  )[0];
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const photographers = await getPhotographers();
+
+  // Affiche l'entête du photographe
+  displayPhotographerHeader(photographers);
+}
+
+init();
