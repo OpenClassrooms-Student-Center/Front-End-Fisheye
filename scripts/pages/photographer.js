5f0a808372c2608490323e192c32804b5b617c90
STEPHANE POLETTI <polettistephane@msn.com>
2022-04-23 22:57:38 +0200

    first commit

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,44 @@
 //Mettre le code JavaScript lié à la page photographer.html
+class Photographer {
+    constructor(name, id, city, country, tagline, price, portrait) {
+        this._name = name;
+        this._id = id;
+        this._city = city;
+        this._country = country;
+        this._tagline = tagline;
+        this._price = price;
+        this._portrait = portrait;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get city() {
+        return this._city;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    get tagline() {
+        return this._tagline;
+    }
+
+    get price() {
+        return this._price;
+    }
+
+    get portrait() {
+        return `assets/photographers/${this._portrait}`;
+    }
+}
+
+newPhotographer = new Photographer("Mimi Keel", 243, "London", "UK", "Voir le beau dans le quotidien", 400, "MimiKeel.jpg")
+
+console.log(newPhotographer)
