41694f7dcad1a7ae53288e170e5fb1791801a792
STEPHANE POLETTI <polettistephane@msn.com>
2022-04-24 22:24:53 +0200

    branchement API partiel / fetch ok mais pas affichage des donnees

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,59 +1,41 @@
-async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
+const photographers = [];
 
-    //fetch()
+    async function getPhotographers() {
 
+        await fetch('data/photographers.json')
+            .then(function(res) {
+                if (res.ok) {
+                return res.json();
+                }
+            })
+            .then(function(value) {
+                console.log(value);
+            })
+            .catch(function(err) {
+                // Une erreur est survenue
+            });
+            
+        return ({
+            photographers: [...photographers]
+        });
 
-    const photographers = [
-        {
-            "name": "Ma data test 1",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test 2",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Mimi Keel",
-			"id": 243,
-			"city": "London",
-			"country": "UK",
-			"tagline": "Voir le beau dans le quotidien",
-			"price": 400,
-			"portrait": "MimiKeel.jpg"
-        },
-    ]
-// ESSAI DANS CONSOLE
-    console.log(photographers);
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]})
-}
+    };
 
-async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer_section");
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-};
+        photographers.forEach((photographer) => {
+            const photographerModel = photographerFactory(photographer);
+            const userCardDOM = photographerModel.getUserCardDOM();
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
 
-async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
-
-init();
+    async function init() {
+        // Récupère les datas des photographes
+        const { photographers } = await getPhotographers();
+        displayData(photographers);
+    };
+    
+    init();
+    
