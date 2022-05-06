3d5d613e0bc25c42c5adb3acc62fc0a65e0cf885
Madeline <grammaticomadeline@hotmail.fr>
2022-05-06 19:41:52 +0200

    fix(app) : add a new filter

--- scripts/functions/getSelectedSort.js
+++ scripts/functions/getSelectedSort.js
@@ -2,43 +2,78 @@ import createPhotoCard from "../templates/Card.js";
 import sliderFactory from "../factories/slider.js";
 
 export default function getSelectedSort(data) {
-    const selectedSort = document.querySelector("#select")
     let sortMedia = [...data]
-    const cardContainer = document.querySelector(".photo-field");
 
-    selectedSort.addEventListener("change", (e) => {
-        const mediaCard = document.getElementsByClassName("cardMedia");
+    
+    const optionsContainer = document.querySelector(".filter__options")
+    const btndrop = document.querySelector(".button__top")
+    const btnValue = document.querySelector(".button__top span")
+    const liItems = document.querySelectorAll(".dropdown li")
+    const first = document.querySelector(".first__option")
+    btnValue.innerHTML = first.innerHTML
+
+    let toggleIndex 
+    btndrop.addEventListener('click', dropDown)
+    function dropDown() {
+       
+        if(!toggleIndex) {
+            optionsContainer.style.height = `${optionsContainer.scrollHeight}px`
+            toggleIndex = true
+            btndrop.style.display = "none"
+            return 
+        } 
+        optionsContainer.style.height = 0
+        btndrop.style.display = "flex"
+        toggleIndex = false
         
-        const totalPhoto = mediaCard.length
-        cardContainer.innerHTML = ""
+    }
+    console.log(liItems)
+    liItems.forEach((item) => {
+        const a = item.querySelector("a")
+        console.log(a.innerHTML)
+        item.addEventListener(('click'),() => {
+               
+            if (a.innerHTML === "Popularité") {
+                dropDown()
+                btnValue.innerHTML = a.innerHTML
+                sortMedia.sort((a,b) => {
+                    return b.likes - a.likes
+                })
 
-        let sortOption = e.target.value
-        if (sortOption === "likes") {
-            sortMedia.sort((a,b) => {
-                return b.likes - a.likes
-            })
-        } else if (sortOption === "date") {
-            sortMedia.sort((a,b) => {
-                return new Date(a._date).valueOf() - new Date(b._date).valueOf()
-            })
-        } else if (sortOption === "title") {
-            sortMedia.sort((a,b) => {
-                if (a.title.toLowerCase() < b.title.toLowerCase()) {
-                    return -1
-                } else  if (a.title.toLowerCase() > b.title.toLowerCase()) {
-                    return 1
-                }
-            })
-        }
-        sortMedia.forEach((media) => {
-                
-            const card = document.createElement("article")
-            card.classList.add("cardMedia")
-            card.innerHTML = createPhotoCard(media, name)
-            const elt = cardContainer.appendChild(card)
+            } else if (a.innerHTML === "Date") {
+                dropDown()
+                btnValue.innerHTML = a.innerHTML
+                sortMedia.sort((a,b) => {
+                    return new Date(a._date) - new Date(b._date)
+                })
+
+            } else if (a.innerHTML === "Titre") {
+                dropDown()
+                btnValue.innerHTML = a.innerHTML
+                sortMedia.sort((a,b) => {
+                    if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                        return -1
+                    } else  if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                        return 1
+                    }
+                })
+            }
+            const cardContainer = document.querySelector(".photo-field");
+            cardContainer.innerHTML = ""
+            console.table(sortMedia)
             
-            elt.addEventListener("click", () => sliderFactory(media, sortMedia, name))
+            sortMedia.forEach((media) => {
+                
+                const card = document.createElement("article")
+                card.classList.add("cardMedia")
+                card.innerHTML = createPhotoCard(media)
+                const elt = cardContainer.appendChild(card)
+                
+                elt.addEventListener("click", () => sliderFactory(media, sortMedia, name))
+            })
         })
+
     })
+    
     return sortMedia
  }
