450b7c46958cf190b271a80ce21efd10845faded
Benjamin Siret <benjaminsiret@outlook.fr>
2022-05-12 16:02:37 +0200

    add getSelectedPhotographer function

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,20 @@
+async function getPhotographers() {
+  const url = "/data/photographers.json";
+  const response = await fetch(url);
+  const data = await response.json();
+  return { photographers: data.photographers };
+}
 
+async function getSelectedPhotographer(id) {
+  const data = await getPhotographers();
+  const selectedPhotographer = data.photographers.filter(
+    (photographer) => photographer.id === id
+  )[0];
+  console.log(selectedPhotographer.name);
+  return selectedPhotographer;
+}
+
+const params = new URL(document.location).searchParams;
+const photographerId = parseInt(params.get("id"));
+
+getSelectedPhotographer(photographerId);
