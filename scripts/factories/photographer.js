53725dc72a0213d50d2dd8742bfa13808f5778c7
Benjamin Siret <benjaminsiret@outlook.fr>
2022-05-12 14:01:53 +0200

    add css for home-page, create innerHTML in getUserCardDOM

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,18 @@
 function photographerFactory(data) {
   const { name, portrait, city, country, price, tagline, id } = data;
 
-  const picture = `assets/photographers/${portrait}`;
-
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const link = document.createElement("a");
-    const img = document.createElement("img");
-    const h2 = document.createElement("h2");
-    const h3 = document.createElement("h3");
-    const quote = document.createElement("p");
-    const pricing = document.createElement("p");
 
-    img.setAttribute("src", picture);
-    img.setAttribute("alt", "photographer portrait");
-    h2.textContent = name;
-    h3.textContent = `${city}, ${country}`;
-    quote.textContent = `${tagline}`;
-    pricing.textContent = `${price}/jour`;
-
-    link.setAttribute("href", `photographer.html?id=${id}`);
-    link.appendChild(img);
-    link.appendChild(h2);
-    article.appendChild(link);
-    article.appendChild(h3);
-    article.appendChild(quote);
-    article.appendChild(pricing);
+    article.innerHTML = ` <a href="photographer.html?id=${id}" class="photographer-link" alt="${name}">
+                          <img src="assets/photographers/${portrait}" class="portrait" alt="">
+                          <h2 class="name">${name}</h2>
+                          </a>
+                          <h3 class="location">${city}, ${country}</h3>
+                          <p class="tagline">${tagline}</p>
+                          <p class="price">${price}€/jour</p>
+                        `;
     return article;
   }
-  return { name, picture, id, getUserCardDOM };
+  return { name, id, getUserCardDOM };
 }
