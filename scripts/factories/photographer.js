2b35729f579ecefbb7f99e53f2672bb295b5c236
Benjamin Siret <benjaminsiret@outlook.fr>
2022-05-12 11:07:05 +0200

    add favicon, import all useful data

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,20 +1,32 @@
 function photographerFactory(data) {
-  const { name, portrait, city, country, price, tagline } = data;
+  const { name, portrait, city, country, price, tagline, id } = data;
 
   const picture = `assets/photographers/${portrait}`;
 
   function getUserCardDOM() {
     const article = document.createElement("article");
+    const link = document.createElement("a");
     const img = document.createElement("img");
-    img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
-    h2.textContent = name;
     const h3 = document.createElement("h3");
+    const quote = document.createElement("p");
+    const pricing = document.createElement("p");
+
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "photographer portrait");
+    h2.textContent = name;
     h3.textContent = `${city}, ${country}`;
-    article.appendChild(img);
-    article.appendChild(h2);
+    quote.textContent = `${tagline}`;
+    pricing.textContent = `${price}/jour`;
+
+    link.setAttribute("href", `photographer.html?id=${id}`);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(link);
     article.appendChild(h3);
+    article.appendChild(quote);
+    article.appendChild(pricing);
     return article;
   }
-  return { name, picture, getUserCardDOM };
+  return { name, picture, id, getUserCardDOM };
 }
