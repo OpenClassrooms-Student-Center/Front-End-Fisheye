22a517b1956c0f7b0f2c12ab0c307ecb06f41014
Khaera MB <khaera.belkadi@gmail.com>
2022-05-18 12:09:07 +0200

    delete Comms

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,8 +4,8 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
     
     function getUserCardDOM() {
-         let str = `
-         <article>
+         let str = 
+         `<article>
             <a href="${urlPhotographers}.html">
                 <img src="${picture}" alt="${name}">
                 <h2 alt= "${name}">${name}</h2>
@@ -14,25 +14,6 @@ function photographerFactory(data) {
             <p>${tagline}</p>
             <small>${price}€/jour</small>
           </article>`; 
-
-        /*const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", name);
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        h2.setAttribute("alt", name);
-        const h4 = document.createElement( 'h4' );
-        h4.textContent = city + ', ' + country;
-        const p = document.createElement( 'p' );
-        p.textContent = tagline;
-        const small = document.createElement( 'small' );
-        small.textContent = price + '€/jours';
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h4);
-        article.appendChild(p);
-        article.appendChild(small);*/
         return (str);
     }
     return { name, city, country, tagline, price, portrait, getUserCardDOM }
