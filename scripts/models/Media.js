7d37b8abb4ce1e83bc19a08e29ac49a9b0ed37f9
Wilfried <wilfriedprombo18@gmail.com>
2022-06-02 13:56:57 +0100

    :art: Improve structure
    Update scripts

---
+++ scripts/models/Media.js
@@ -0,0 +1,10 @@
+class Media {
+    constructor(data) {
+        this.id = data.id
+        this.photographerId = data.photographerId
+        this.title = data.title
+        this.likes = data.likes
+        this.date = data.date
+        this.price = data.price
+    }
+}
--- scripts/models/Picture.js
+++ scripts/models/Picture.js
@@ -1,39 +1,6 @@
-class Picture {
+class Picture extends Media {
     constructor(data) {
-        this._id = data.id
-        this._photographerId = data.photographerId
-        this._title = data.title
-        this._image = data.image
-        this._likes = data.likes
-        this._date = data.date
-        this._price = data.price
-    }
-
-    get id() {
-        return this._id
-    }
-
-    get photographerId() {
-        return this._photographerId
-    }
-
-    get title() {
-        return this._title
-    }
-
-    get image() {
-        return this._image
-    }
-
-    get likes() {
-        return this._likes
-    }
-
-    get date() {
-        return this._date
-    }
-
-    get price() {
-        return this._price
+        super(data)   
+        this.image = data.image
     }
 }
