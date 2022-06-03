a75f041b504e58a58cb4f57164e00ff026e25d77
Benjamin Siret <benjaminsiret@outlook.fr>
2022-06-03 17:47:24 +0200

    implement form in contact modal, add css for it

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -9,6 +9,7 @@ function closeModal() {
 }
 
 // TODO: créer les regex pour les champs prénom / nom / email
+
 // TODO: ajouter une max length pour le champ message
 // TODO: créer les event listener et fonctions qui vérifient la validité des champs
 // TODO: créer la fonction validate qui va afficher les infos saisies dans la console
