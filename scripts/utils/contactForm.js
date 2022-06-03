7765ad84f5d789d01ec0c09eef6b40359f4287ad
Benjamin Siret <benjaminsiret@outlook.fr>
2022-06-03 14:25:10 +0200

    add all images

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,14 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
 }
+
+// TODO: créer les regex pour les champs prénom / nom / email
+// TODO: ajouter une max length pour le champ message
+// TODO: créer les event listener et fonctions qui vérifient la validité des champs
+// TODO: créer la fonction validate qui va afficher les infos saisies dans la console
