315a8c653f00377f4b3fd5b47a9b5bac47403a34
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2022-06-07 08:58:56 +0200

    PhotographerHeader+formulaireContact+trie+css

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,49 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const profilMedia = document.querySelector("#profil__media");
+
+let typeSort = ""; // Type de tri des média
+let listMediaId = []; //Variable qui va permttre le filtre avec la list des ID des médias
+let allLikes = 0; // Tout les likes des médias
+
+//Obtenir l'ID du photographe pour charger les données
+function getParamsUrl(url) {
+  let string = url.search;
+  return string.substring(3);
+}
+const idUser = getParamsUrl(window.location);
+
+async function setData() {
+  let response = await fetch("../data/photographers.json");
+  if (!response.ok) {
+    return "error";
+  }
+  let data = await response.json();
+
+  let photographer = data.photographers.find((element) => element.id == idUser);
+  let media = data.media.filter((m) => m.photographerId == idUser);
+
+  setDataInHtml(photographer, media);
+}
+setData();
+
+function setDataInHtml(photographer, media) {
+  setProfilHeader(photographer);
+  setProfilMedia(media);
+  setSectionInfo(photographer, media);
+}
+
+
+//Mettre info dans la presentation du photographe
+function setProfilHeader(photographer) {
+  document.getElementById(
+    "modalTitle"
+  ).innerHTML = `Contactez-moi<br>${photographer.name}`;
+  document.getElementById("profilName").innerText = photographer.name;
+  document.getElementById(
+    "profilLocation"
+  ).innerText = `${photographer.city}, ${photographer.country}`;
+  document.getElementById("profilTagline").innerText = photographer.tagline;
+
+  document.getElementById(
+    "profilImage"
+  ).src = `assets/photographers/${photographer.portrait}`;
+}
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,44 @@
+const modal = document.querySelector(".modal");
+const form = document.forms["contact"];
+const closeBtnForm = document.querySelector(".contact_close_button");
+const submitBtn = document.querySelector("button[type='submit']");
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  modal.style.display = "flex";
+  closeBtnForm.focus();
+}
+
+function getElementsForm(form) {
+  let names = form.querySelectorAll("input[type='text']");
+  let email = form.querySelector("input[type='email']");
+  let message = form.querySelector("textarea");
+  return [names[0], names[1], email, message];
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  modal.style.display = "none";
+
+  //reset form
+  let elements = getElementsForm(form);
+  elements.forEach((element) => (element.value = ""));
 }
+
+// Evenement validation du formulaire
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  let elements = getElementsForm(this);
+  elements.forEach((element) => console.log(element.value));
+
+  if (document.activeElement == submitBtn) {
+    closeModal();
+  }
+});
+
+setFocusOnlyInContainer(
+  ".modal",
+  ".contact_close_button",
+  "button[type='submit']",
+  closeModal
+);
