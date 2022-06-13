860ced2abddded2966b5b4e76483d988c5557df0
Dwain06 <dwain.mobile@gmail.com>
2022-06-13 16:24:56 +0200

    add (contact modal) fade-in-out transition

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,21 @@
 // DOM elements
-const modal = document.getElementById("contact_modal");
+const modal = document.querySelector(".contact_modal");
+
+document.querySelector(".contact_button").addEventListener("click", displayModal);
+document.querySelector("#close_form").addEventListener("click", closeModal);
 
 function displayModal() {
+    modal.classList.add('fade-out');
 	modal.style.display = "block";
+    window.setTimeout(() => {
+        modal.classList.remove('fade-out');
+      }, 50)
 }
 
 function closeModal() {
-    modal.style.display = "none";
+    modal.classList.add('fade-out');
+    window.setTimeout(() => {
+        modal.style.display = "none";
+        modal.classList.remove('fade-out');
+      }, 300)
 }
