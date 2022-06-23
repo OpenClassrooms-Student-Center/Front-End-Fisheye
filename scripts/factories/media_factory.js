7830697144b433425db3eb379b354dee22d49514
Dwain06 <dwain.mobile@gmail.com>
2022-06-23 15:01:00 +0200

    fix (photographer page): keyboard acces to medias

--- scripts/factories/media_factory.js
+++ scripts/factories/media_factory.js
@@ -27,6 +27,7 @@ class MediaFactory {
         //Set attributes and class for the CSS
         media.setAttribute("src", this.mediaPath);
         media.setAttribute("alt", this.title);
+        media.setAttribute("tabindex", "0");
         divLikes.classList.add('likes');
         heart.classList.add('fa-regular', 'fa-heart');
 
