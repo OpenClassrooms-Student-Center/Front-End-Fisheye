fd0bd13f489d3b526d807528bc5a3594d6439fa4
Katarina Bajic <bajickatarina.pro@icloud.com>
2022-06-30 20:46:53 +0200

    Page d'accueil : implémentation JS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -5,30 +5,34 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("class", "photographer_profil");
         const link = document.createElement("a");
         link.setAttribute("href", `photographer.html?id=${id}`);
+        article.appendChild(link);
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
         img.setAttribute("class", "picture_profil");
+        img.setAttribute
         link.appendChild(img);
-        article.appendChild(link);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.setAttribute("class", "h2");
         article.appendChild(h2);
         const photographersInfo = document.createElement("div");
-        photographersInfo.setAttribute("class", "photographer_description");
         article.appendChild(photographersInfo);
         const cityAndCountry = document.createElement('h3');
         cityAndCountry.textContent = `${city}, ${country}`;
+        cityAndCountry.setAttribute("class", "h3");
         photographersInfo.appendChild(cityAndCountry);
         const taglineP = document.createElement('p');
-        taglineP.setAttribute("class", "text_description");
         taglineP.textContent = `${tagline}`;
+        taglineP.setAttribute("class", "text_description");
         photographersInfo.appendChild(taglineP);
         const priceP = document.createElement('p');
         priceP.setAttribute("class", "price");
-        priceP.textContent = `${price}`;
+        priceP.textContent = `${price}€/jour`;
+        priceP.setAttribute("class", "prix");
         photographersInfo.appendChild(priceP);
         return (article);
     }
