5771a358030a410f7b9f8fd14db85a8f0833a9d3
laminemessaci <lamine.messaci18@gmail.com>
2022-07-06 13:38:16 +0200

    reformate code

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -61,28 +61,6 @@ export class Form {
         console.log(isValidFormData.data);
         this.closeModal();
 
-        (function ($) {
-          showSwal = function (type) {
-            "use strict";
-            if (type === "success-message") {
-              swal({
-                title: "Congratulations!",
-                text: "You entered the correct answer",
-                type: "success",
-                button: {
-                  text: "Continue",
-                  value: true,
-                  visible: true,
-                  className: "btn btn-primary",
-                },
-              });
-            } else {
-              swal("Error occured !");
-            }
-          };
-        })(jQuery);
-      }
-    });
   }
 
   /**
