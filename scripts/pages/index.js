011494023cea802365b475f6fe808ede4b1cc07f
woengus <olivierbiard@woengus.com>
2022-07-12 19:31:31 +0200

    index photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,22 @@
-let photographersData = []; //tableau pour y stocker les données du json
-const fetchPhotographers = async () => {
+let photographers = []; //tableau pour y stocker les données du json
 
-    let fetchUrl = "/data/photographers.json";
-    await fetch(fetchUrl) //asynchrone
-    .then((res) => res.json()) //promise
-    .then((data) => {
-        photographersData = data;
-        console.log(photographersData)
-    })
-}
-fetchPhotographers();
 
 
 async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-       
-        const photographers = [
-            {
-                "name": `bob`,
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            }
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        let fetchUrl = "/data/photographers.json";
+        await fetch(fetchUrl) //asynchrone
+        .then((res) => res.json()) //promise
+        .then((data) => {
+            photographers = data.photographers;
+            console.log(data)
+            console.log(photographers)
+        })
+        
+      
     }
 
-    async function displayData(photographers) {
+    async function displayData() {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
@@ -52,8 +28,11 @@ async function getPhotographers() {
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        //const { photographers } = await getPhotographers();
+        //displayData(photographers);
+        await getPhotographers()
+        console.log(photographers)
+        displayData();
     };
     
     init();
