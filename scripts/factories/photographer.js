c15f812c81b471beed33677906dd65f50c0fa68b
anais meirone <anais.meirone@live.fr>
2022-07-17 17:22:46 +0200

    adding photographer datas with json file, using fetch method

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,25 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const photographerCity = document.createElement('p');
+        const photographerTagline = document.createElement('span');
+        const photographerPrice = document.createElement('p');
         h2.textContent = name;
+        photographerCity.textContent = `${city}, ${country}`;
+        photographerTagline.textContent = tagline;
+      photographerPrice.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(photographerCity);
+        article.appendChild(photographerTagline);
+        article.appendChild(photographerPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
