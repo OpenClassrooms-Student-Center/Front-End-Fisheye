86e5bcbfaa60a0247c9c342cb8e38d496438e972
Madeline <grammaticomadeline@hotmail.fr>
2022-07-19 17:11:46 +0200

    fix(app) : fix css

--- scripts/factories/presentation.js
+++ scripts/factories/presentation.js
@@ -5,13 +5,17 @@ export default function presentationFactory() {
   function createPresentation(newPresentation) {
     const presentationInnerHTML = `<aside class="photographer__presentation" aria-label="informations">
         <h1 id="${newPresentation.name}" class="photographer__name">${newPresentation.name}</h1>
-        <p class="photographer__location" aria-describedby="${newPresentation.name}">${newPresentation.city}, ${newPresentation.country}</p>
+        <p class="photographer__location" aria-describedby="${newPresentation.name}">${newPresentation.city},
+         ${newPresentation.country}</p>
         <p class="photographer__quote" aria-describedbynewPresentation.name}">${newPresentation.tagline} </p>
         </aside>
-        <button id="btnOppenModal" class="contact_button" aria-label="contactez moi" aria-describedby="${newPresentation.name}">
+        <button id="btnOppenModal" class="contact_button" 
+        aria-label="contactez moi" aria-describedby="${newPresentation.name}">
             Contactez-moi
         </button>
-        <img class="photographer__profile portrait" src="${newPresentation._pathPortrait}" aria-label="${newPresentation.name}" alt="profil de ${newPresentation.name}" aria-describedby="${newPresentation.name}">`;
+        <img class="photographer__profile portrait" src="${newPresentation._pathPortrait}" 
+        aria-label="${newPresentation.name}" alt="profil de ${newPresentation.name}" 
+        aria-describedby="${newPresentation.name}">`;
 
     const section = document.createElement('section');
     section.innerHTML = presentationInnerHTML;
