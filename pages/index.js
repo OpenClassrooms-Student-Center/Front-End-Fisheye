76f07425f6f592965aee2cac3a3a8eaf9c6358f6
queva <queval.loic@gmail.com>
2022-07-22 22:08:28 +0200

    change some css files into scss file, get data photographers, display required data, create variables file, move header css into a unique header scss file, add normilize

---
+++ pages/index.js
@@ -0,0 +1,37 @@
+async function getPhotographers() {
+    const getPhotographers = fetch('../../data/photographers.json')
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+                    .then(data => {
+                            return data.photographers
+                        }
+                    )
+            } else {
+                console.error('Retour du serveur : ', response.status)
+            }
+        })
+    return (
+        await getPhotographers
+    )
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector('.photographer_section');
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    await displayData(photographers);
+}
+
+init()
+
+
--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,30 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
+    console.log({ name, portrait, city, country, tagline, price });
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `../assets/photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = city + ", " + country;
+        location.className = " location"
+        const quote = document.createElement('p');
+        quote.textContent = tagline;
+        quote.style.margin = '0';
+        const rate = document.createElement('p');
+        rate.textContent = price + "€/jour";
+        rate.style.margin = '0';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(quote);
+        article.appendChild(rate);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
