6a39b2c073af80940bee1c122048fc8c2b5f44ec
Valentin Duffet <vavadu07@gmail.com>
2022-07-23 16:27:01 +0200

    fetch json data & finish homepage

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,34 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price, country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const linkPhotographer = document.createElement('a');
+        linkPhotographer.setAttribute("href", "/")
+        const containerImg = document.createElement( 'div' );
+        containerImg.setAttribute("class", "container_img")
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const userName = document.createElement( 'h2' );
+        userName.textContent = name;
+        const userLocation = document.createElement( 'h3' );
+        userLocation.textContent = `${city}, ${country}`;
+        const userTagline = document.createElement( 'span' );
+        userTagline.setAttribute("class", "tagline")
+        userTagline.textContent = tagline;
+        const userPrice = document.createElement( 'span')
+        userPrice.setAttribute("class", "price")
+        userPrice.textContent = `${price}€/jour`;
+
+        article.appendChild(linkPhotographer)
+        linkPhotographer.appendChild(containerImg)
+        containerImg.appendChild(img);
+        linkPhotographer.appendChild(userName);
+        article.appendChild(userLocation);
+        article.appendChild(userTagline);
+        article.appendChild(userPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
