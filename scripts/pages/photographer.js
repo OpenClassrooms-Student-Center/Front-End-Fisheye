7d49c08fe8abc18f47b268013aaf50f534fd3641
woengus <olivierbiard@woengus.com>
2022-08-02 13:37:20 +0200

    adding css for photographers page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,8 +1,8 @@
 //Mettre le code JavaScript lié à la page photographer.html
-let photographer = {}; //objet pour stocke le photographe
-let medias = []; //tableau pour stocke les médias
-
-
+let photographer = {}; //objet pour stocker le photographe
+let medias = []; //tableau pour stocker les médias
+let img =[];
+let vid =[];
 async function getPhotographerAndMedia() {
         //récupérer l'id passé dans la page
 let str = window.location.href;
@@ -14,7 +14,7 @@ console.log(id);
         await fetch(fetchUrl) //asynchrone
         .then((res) => res.json()) //promise
         .then((data) => {
-          
+            
             for(let i = 0; i < data.photographers.length; i++) {
                 //console.log(data.photographers[i].id);
                 if(id === data.photographers[i].id) {
@@ -22,26 +22,58 @@ console.log(id);
                     break;
                 }
             }
+          
             medias = data.media.filter( element => {
                 /*if(id === element.photographerId) {
                     return element
+                }
+                if(element.image) {
+                    image.push(element.image)
+                    return image.photographerId;
+                }
+                else {
+                    video.push(element.video)
+                    return video.photographerId;
                 }*/
                 return id === element.photographerId;
+                
+            
             })
-            /* data.media.forEach(element => {
-                if(id === element.photographerId) {
-                    medias.push(element)
-                }
-
-            });*/
-            console.log(medias)
+            
+            //console.log(medias)
             //console.log(data.media)
+            
         })
 }
+ 
+
 const displayPhotographerAndMedia = async () =>{
     await getPhotographerAndMedia();
+   
     document.querySelector(".photographer").innerHTML = `<div class="flex"><h2>${photographer.name}</h2><h3>${photographer.city}, ${photographer.country}</h3><h4>${photographer.tagline}</h4></div><button class="contact_button">Contactez-moi</button><img src="/assets/photographers/${photographer.portrait}" alt="photo de ${photographer.name}"></img>`;
-};
+       
+    
+        document.querySelector(".photos").innerHTML = medias.map((media)=>  `<div id="${media.id}">
+        <img src="/assets/photographers/${photographer.name}/${media.image}" class="media_img" alt="image de ${media.image}">
+        <div class="flex">
+        <h3>${media.title}</h3>
+        <h4>${media.likes} <i class="fa-solid fa-heart"></i></h4>
+        </div>
+        </div>`).join("");
+        
+    
+    
+        
+    
+    
+    
+      
+    
+
+   
+    
+    
+}
 
 displayPhotographerAndMedia();
 
