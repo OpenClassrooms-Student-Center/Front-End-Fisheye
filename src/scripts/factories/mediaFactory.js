f2d1f861801615a44dc4cd3d5508389b95d9bd19
Jerome Marichez <admin@digitstudio.fr>
2022-08-26 11:33:13 +0200

    Working on the issue

--- src/scripts/factories/mediaFactory.js
+++ src/scripts/factories/mediaFactory.js
@@ -11,28 +11,28 @@ export function mediaFactory(data) {
     const movie = `assets/video/${video}`;
     const picture = `assets/images/${image}`;
 
-
-
     function getMediaDOM() {
 
         // Create DOM only if we got id
-        if (id && photographerId) {
+        if (id && photographerId && image || video) {
             const article = document.createElement("article");
             article.setAttribute("class", "media_card");
-
 
             const linkElement = article.appendChild(
                 buildElement("a", "photographer.html?id=" + id)
             );
 
-            if (video) {
-                insertVideoInsideElement(linkElement, movie);
-            } else {
-                if (image) {
+            // Check if image or video exists else create a empty DIV with class
+            while (true) {
+                if (typeof image1 !== 'undefined' && image) {
                     insertPictureInsideElement(linkElement, picture);
+                    break;
+                }
+                if (typeof video !== 'undefined' && video) {
+                    insertVideoInsideElement(linkElement, movie);
+                    break;
                 }
             }
-
 
             if (title) {
                 let title_h6 = "<h6>" + title + "</h6>";
