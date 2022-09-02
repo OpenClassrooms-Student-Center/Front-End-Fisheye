c6b721fe86fad1066ce77ad41db47ba0df041b0b
Georgina <georgina.berrezel@gmail.com>
2022-09-02 14:23:37 +0200

    Add photographers data Json and pictures

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,9 +1,11 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const link = document.createElement("a");
+        link.setAttribute ("href", `photographer.html?id=${id}`);
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
@@ -11,7 +13,8 @@ function photographerFactory(data) {
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        link.appendChild(article);
+        return (link);
     }
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,4 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// chercher avec un .find l'id que je veux pour afficher mle photographe correspondant
+// filter pour avoir les images qui correspondent à mon bon photographe (id)
