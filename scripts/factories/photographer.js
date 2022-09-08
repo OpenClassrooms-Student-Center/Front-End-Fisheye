a5b7bde2c458d346baa96fc2696ea7acc01ec1b9
HaydenLeDev <loicroth.pro@gmail.com>
2022-09-08 16:43:22 +0200

    css page acceuille

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,29 +4,45 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement('a');
-        const info = document.createElement('info');
-        article.classList.add("profil");
-        info.classList.add("profil-info");
-        const img = document.createElement('img');
+        //Création de la sturcture
+        const article = document.createElement('div'); // Créer la carte photographe
+        const info = document.createElement('div'); // Création de la div info
+        const cilckable = document.createElement('a'); // Création de la div clickable
+        article.classList.add("profil"); // On donne la classe  
+        info.classList.add("profil-info"); // On donne la classe a la div info
+        cilckable.classList.add("profil-principal"); // On donne la classe a la div principal
+        cilckable.href = "test"; //Lien de la carte photo
+        //Création des éléments se trouvant dans la div clickable
+        const img = document.createElement('img'); // Création de l'image
         img.setAttribute("src", picture)
         const h2 = document.createElement('h2');
+
+        //Création des éléments se trouvant dans la div info
         const h3 = document.createElement('h3');
         const p1 = document.createElement('p');
         const p2 = document.createElement('p');
+
+        //Set les infos
         h2.textContent = name;
         h3.textContent = city + ", " + country;
         p1.textContent = tagline;
         p2.textContent = price + "€/jour";
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(p2);
+        p2.classList.add("profil-info-price");
 
-        article.appendChild(info);
+        //Ajout les infos a cilckable
+        cilckable.appendChild(img);
+        cilckable.appendChild(h2);
+
+        //Ajout les infos a info
         info.appendChild(h3);
         info.appendChild(p1);
         info.appendChild(p2);
+
+        //Ajout les div a article
+        article.appendChild(cilckable);
+        article.appendChild(info);
+        
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
