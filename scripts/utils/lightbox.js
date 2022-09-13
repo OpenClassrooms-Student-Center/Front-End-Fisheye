c0e49e3b385b670fb21ba5f8f8b4a381384c184b
HaydenLeDev <loicroth.pro@gmail.com>
2022-09-13 17:17:19 +0200

    gauche droite

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,22 +1,23 @@
 var index = 0;
 
 
-function displayLightbox(id){
+async function displayLightbox(id){
     const lightbox_modal = document.getElementById("lightbox_modal");
     console.log(id);
-    creationListe(id);
 
+    creationListe(id);
     const photo = document.querySelector(".lightbox_modal_media");
-    const photographerModel = photoCardFactory( data[1][index], data[0]);
+    const photographerModel = photoCardFactory(data[1][index], data[0]);
     const userCardDOM = photographerModel.getPhotoLightboxDOM();
     photo.appendChild(userCardDOM);
 
 
     lightbox_modal.style.display = "flex";
     document.documentElement.style.overflow = 'hidden';
+    console.log("TEST: " + index);
 }
 
-function closeLightbox() {
+async function closeLightbox() {
     const lightbox_modal = document.getElementById("lightbox_modal");
 
     const photo = document.querySelector(".lightbox_modal_media");
@@ -29,10 +30,39 @@ function closeLightbox() {
 }
 
 async function creationListe(id){
-    while (id !=  data[1][index].id){
+    index = -1; // Car dans la boucle on fait +1 au début cela evite le beug pour la premiere incrémentation
+    let flag = true;
+    while (flag){
         index++;
+        if (id ==  data[1][index].id){
+            flag = false;
+        }
+        
     }
+    console.log("a: " + index);
 
 }
 
+async function former(){
+    index--;
+    replaceContain();
+}
+
+async function next(){
+    index++;
+    replaceContain();
+}
+
+function replaceContain(){
+    const photo = document.querySelector(".lightbox_modal_media");
+    if(data[1][index]){
+        if (photo != "") {
+        photo.innerHTML = "";
+    }
+    const photographerModel = photoCardFactory(data[1][index], data[0]);
+    const userCardDOM = photographerModel.getPhotoLightboxDOM();
+    photo.appendChild(userCardDOM);
+    }
+    
+}
 
