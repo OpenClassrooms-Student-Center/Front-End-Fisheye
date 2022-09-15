c55eae176f8e43d7cd53565775f91cd200a57f37
kriss <cgdamoniac@gmail.com>
2022-09-15 09:35:09 +0200

    début fetch + template + mise en forme

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,25 @@
+// ici pour récupérer les données nécessaires (id, tagline, city, etc.)
+
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.insertAdjacentHTML('beforeend', `
+            <a href="https://www.mozilla.com">
+                <img src='${picture}' alt='photo de profil'>
+                <h2>${name}</h2>
+            </a>
+            <div>
+                <p>${city}, ${country}</p>
+                <p>${tagline}</p>
+                <small>${price}€/jour</small>
+            </div>
+            
+        `)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, portrait, id, city, country, tagline, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,17 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // fetch sur json
+    const getPhotographers = async () =>{
+        return fetch("data/photographers.json")
+              .then(function (response){
+                  console.log(response);
+                  return response.json();
+                
+              })
+              .catch(function (error){
+                  console.log(error);
+              });
     }
 
     async function displayData(photographers) {
@@ -42,4 +31,7 @@
     };
     
     init();
+
     
+    
+})  
