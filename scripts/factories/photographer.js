5d3e2d4ac04243d3dcbd6e379bffa2d59e8da791
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-10-12 18:54:43 +0200

    ajout fect

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,8 @@
 // la fonction photographerFactory a pour paramêtre data (le photographers.json)
 function photographerFactory(data) {
   // la constante se compose d'un objet doté  de name et portrait et dont la valeur renvoi à data
-  const { name, portrait } = data;
+  const { name, portrait, price } = data;
+  console.log(price);
   /* la constante picture à pour valeur le chemin assets/photographers
      et fera une interpolation de chaine
      dans ce chemin afin d'insérer un portrait dedans.
@@ -9,21 +10,25 @@ function photographerFactory(data) {
   const picture = `assets/photographers/${portrait}`;
   // la fonction getUserCardDOM permet :
   function getUserCardDOM() {
-    // recupère l'article
+    // crée l'article mais n'existe que dans le javascript
     const article = document.createElement("article");
-    // recupère l'image
+    // crée l'image mais n'existe que dans le javascript
     const img = document.createElement("img");
     // le setAttribute montre le changement de la valeur src en picture => le src renverra grâce à son lien à la picture
     img.setAttribute("src", picture);
-    // crée le h2
+    // crée le h2 mais n'existe que dans le javascript
     const h2 = document.createElement("h2");
-    // crée le name
+    // le h2 aura le texte suivant ="name"
     h2.textContent = name;
     // ajoute une image à l'article
     article.appendChild(img);
     // ajoute un h2 à l'article
     article.appendChild(h2);
     // retourne à l'article
+    const divPrice = document.createElement("div");
+    divPrice.textContent = price;
+    article.appendChild(divPrice)
+    
     return article;
   }
   return { name, picture, getUserCardDOM };
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,32 +1,35 @@
-async function getPhotographers() {
-  // la fonction asynchrone getPhotographers s'activera et affichera les infos de photographes
-  // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-    
-  ];
-  console.log(getPhotographers);
-  // et bien retourner le tableau photographers seulement une fois
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+ async function getPhotographers() {
+//   // la fonction asynchrone getPhotographers s'activera et affichera les infos de photographes
+//   // Penser à remplacer par les données récupérées dans le json
+//   const photographers = [
+//     {
+//       name: "Ma data test",
+//       id: 1,
+//       city: "Paris",
+//       country: "France",
+//       tagline: "Ceci est ma data test",
+//       price: 400,
+//       portrait: "account.png",
+//     },
+//     {
+//       name: "Autre data test",
+//       id: 2,
+//       city: "Londres",
+//       country: "UK",
+//       tagline: "Ceci est ma data test 2",
+//       price: 500,
+//       portrait: "account.png",
+//     },
+//
+//   ];
+//   console.log(getPhotographers);
+//   // et bien retourner le tableau photographers seulement une fois
+//   return {
+//     photographers: [...photographers, ...photographers, ...photographers],
+//   };
+  // fetch permet de faire une requete sur une url
+  
+  return fetch("./data/photographers.json").then(response => response.json());
 }
 
 async function displayData(photographers) {
@@ -47,3 +50,5 @@ async function init() {
 }
 
 init();
+
+
