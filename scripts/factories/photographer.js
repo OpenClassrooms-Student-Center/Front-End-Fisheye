5b7161237299202d4e04b4320ce82bb3465171b6
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-10-12 12:35:52 +0200

    reindentation de tout le code

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,17 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    article.appendChild(img);
+    article.appendChild(h2);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
