06b201bd8c8a5479c50bb0194a3576d074bc511c
chris <antit571@sfr.fr>
2022-10-12 11:14:49 +0200

    test récupération des images pour lightbox

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -52,11 +52,144 @@ class AppFactory {
 
         // on fabrique les medias (renvoi le tableau des medias)
         let mediasList = dataMedia.map(media => new Media(media))
-        console.log(mediasList)
-        // ---Trie par Popularité
-        sortLikes(mediasList)
-        // render des medias
-        let forIndex = ''
+
+
+        //-----Création des card Média--------/
+        // et crétion du bloc totalLikes  !!!!!!!!!!!!!!!!!!
+        let renderMedia = () => {
+            let forIndex = ''
+            let totalLikes = 0
+
+            mediasList.forEach(media => {
+                // valeur de totalLikes
+                totalLikes = totalLikes += media.likes
+                // tout les médias pour création
+                forIndex = forIndex + media.getAllMedia()
+            })
+            const eltParent = document.getElementById('section')
+            eltParent.insertAdjacentHTML('beforeend', forIndex)
+
+            //------------------------------------//
+            // render total des likes
+            // récup l élément html
+            const blocStat = document.querySelector('.bloc-stat')
+            // --template totalLikes
+            let totalLikesTemplate = `
+            <div id='likes-content'>
+              <p>${totalLikes}</p>
+              <i class="fa-solid fa-heart"></i>
+            </div>
+            
+        `
+            // -- display totalLikes template
+            blocStat.insertAdjacentHTML('afterbegin', totalLikesTemplate)
+            // -- ajouter enlever likes
+            addLikes()
+        }
+        renderMedia()
+
+        // ------- Création des cards Médias ------- //
+        /**
+         * Création des cards Médias
+         */
+        const renderCard = () => {
+            let forIndex = ''
+            mediasList.forEach(media => {
+                // tout les médias pour création
+                forIndex = forIndex + media.getAllMedia()
+            })
+            const eltParent = document.getElementById('section')
+            eltParent.insertAdjacentHTML('beforeend', forIndex)
+            // -- ajouter enlever likes
+            addLikes()
+        }
+
+        // --------------- Reset card média dom ----------------- //
+        const resetRenderCard = () => {
+            const eltParent = document.getElementById('section')
+            eltParent.innerHTML = ''
+        }
+
+        //-------------------------------------------------------//
+        //---------------- gestion des tris ---------------------//
+        const selected = document.getElementById('popularity')
+        const options = document.querySelectorAll('.selector')
+
+
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+
+                // ---Ranger par Date
+                if (selected.textContent == 'Date') {
+                    resetRenderCard()
+                    sortDate(mediasList)
+                    renderCard()
+
+                }
+                // --- Ranger par Titre
+                if (selected.textContent == 'Titre') {
+                    resetRenderCard()
+                    sortTitle(mediasList)
+                    renderCard()
+
+                }
+
+            })
+        })
+
+        //------------------------------------------------//
+        // --------------- lightbox ------------------ //
+        const arrayCardInDom = document.querySelectorAll('.card-image')
+        console.log(arrayCardInDom);
+        //----open lightbox
+
+        const imageCard = document.querySelectorAll('.picture')
+        imageCard.forEach(image => {
+            image.addEventListener('click', () => {
+                const lightbox = document.querySelector('.lightbox')
+                lightbox.style.display = 'block'
+            })
+        })
+
+
+
+        //----close lightbox
+        const closedButton = document.querySelector('.close-lightbox')
+        const lightbox = document.querySelector('.lightbox')
+        closedButton.addEventListener('click', () => {
+            lightbox.style.display = 'none'
+        })
+        //---Récupération de lélément par son id
+        const getImageById = (id) => {
+            return arrayCardInDom.find(element => element.id == id)
+        }
+
+        //----Traitement display card
+        arrayCardInDom.forEach(card => {
+            console.log(card);
+            card.addEventListener('click', (e) => {
+                let cardId = e.currentTarget.dataset.id;
+
+                // 0 => récupéré l image
+                let image = document.querySelector('.picture')
+                console.log(image);
+                // 0+ => récupéré le titre
+
+                // 1 => affiché image clické dans la modal
+                const cible = document.querySelector('.lightbox-container')
+                cible.insertAdjacentHTML('beforeend', image)
+                // 2 => next image
+                // 3 => prévious image
+
+            })
+
+
+        })
+
+
+
+        //***********************************test */
+        /*let forIndex = ''
         let totalLikes = 0
 
         mediasList.forEach(media => {
@@ -81,13 +214,13 @@ class AppFactory {
             
         `
         // -- display totalLikes template
-        blocStat.insertAdjacentHTML('afterbegin', totalLikesTemplate)
+        blocStat.insertAdjacentHTML('afterbegin', totalLikesTemplate)*/
 
         //*****************************************************
         //*********  TEST FONCTION LIKES isoler dans likes.js***********/
-        console.log(totalLikes);
+        //console.log(totalLikes);
         // ---Traitement des likes et totalLikes
-        addLikes()
+        //addLikes()
 
 
 
