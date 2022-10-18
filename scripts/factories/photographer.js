48d922e3b7b50554fd790b2f645cacd6af9988e7
abaddon101 <mathieu.roussel.pro@gmail.com>
2022-10-18 11:50:47 +0200

    ajout dans photographer.js\factories des variables suivantes divTagLine, divCityAndCountry, et ajout de fonctionalite pour la variable divPrice : string €/jour color grey fontSize 13px, reindentation du code de la fonction photographerFactory(data)

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 // la fonction photographerFactory a pour paramêtre data (le photographers.json)
 function photographerFactory(data) {
   // la constante se compose d'un objet doté  de name et portrait et dont la valeur renvoi à data
-  const { name, portrait, price } = data;
+  const { city, country, name, tagline, portrait, price } = data;
   console.log(price);
   /* la constante picture à pour valeur le chemin assets/photographers
      et fera une interpolation de chaine
@@ -12,23 +12,44 @@ function photographerFactory(data) {
   function getUserCardDOM() {
     // crée l'article mais n'existe que dans le javascript
     const article = document.createElement("article");
-    // crée l'image mais n'existe que dans le javascript
-    const img = document.createElement("img");
-    // le setAttribute montre le changement de la valeur src en picture => le src renverra grâce à son lien à la picture
-    img.setAttribute("src", picture);
-    // crée le h2 mais n'existe que dans le javascript
-    const h2 = document.createElement("h2");
-    // le h2 aura le texte suivant ="name"
-    h2.textContent = name;
-    // ajoute une image à l'article
-    article.appendChild(img);
-    // ajoute un h2 à l'article
-    article.appendChild(h2);
-    // retourne à l'article
-    const divPrice = document.createElement("div");
-    divPrice.textContent = price;
-    article.appendChild(divPrice)
     
+
+    // create the image element
+    const img = document.createElement("img");
+    // le setAttribute montre le changement de la valeur src en picture
+    // => le src renverra grâce à son lien à la picture
+    img.setAttribute("src", picture);
+    article.appendChild(img);
+    img.style.borderRadius = "100%";
+    img.style.objectFit = "cover";
+
+
+    // create the h2 element to the article
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    article.appendChild(h2);
+
+    // create the city and country element
+    const divCityAndCountry = document.createElement("div");
+    divCityAndCountry.textContent = city + ", " + country;
+    article.appendChild(divCityAndCountry);
+    divCityAndCountry.style.color = "#901c1c";
+
+    // create the tagline element
+    const divTagLine = document.createElement("div");
+    divTagLine.textContent = tagline;
+    article.appendChild(divTagLine);
+    divTagLine.style.color = "black";
+    divTagLine.style.fontSize = "13px";
+
+    // create the Price element
+    const divPrice = document.createElement("div");
+    divPrice.textContent = price + "€/jour";
+    article.appendChild(divPrice);
+    divPrice.style.color = "grey";
+    divPrice.style.fontSize = "13px";
+
+    // retourne à l'article
     return article;
   }
   return { name, picture, getUserCardDOM };
