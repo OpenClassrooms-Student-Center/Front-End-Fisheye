0f9cdce7893f1b24820271dfe1a884c66d5ff950
Aurelie Menin <atypik77@hotmail.fr>
2023-01-05 21:57:38 +0100

    Add infos photographers (location, tagline, daily rate)

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, id, price, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", `Portrait du photographe ${name}`);
+
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const location = document.createElement("p");
+    location.classList.add("location");
+    location.textContent = `${city}, ${country}`;
+
+    const resume = document.createElement("p");
+    resume.classList.add("resume");
+    resume.textContent = tagline;
+
+    const dailyRate = document.createElement("p");
+    dailyRate.classList.add("dailyRate");
+    dailyRate.textContent = `${price}€/jour`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(resume);
+    article.appendChild(dailyRate);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
