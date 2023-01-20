2adfc8cf13d0b0526cc12cae9bcf70f576a05be8
nassmim <nassmim972@gmail.com>
2023-01-20 19:26:16 +0100

    les informations sur les photographes sont récupérées et affichées sur la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,33 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(data, priceBasis) {
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const photographerParagraph = document.createElement('p'),
+            locationDiv = document.createElement('div')
+            taglineDiv = document.createElement('div'),
+            priceDiv = document.createElement('div');
+
+        locationDiv.textContent = `${city}, ${country}`;
+        taglineDiv.textContent = tagline;
+        priceDiv.textContent = price + priceBasis;
+
+        
         article.appendChild(img);
         article.appendChild(h2);
+
+        photographerParagraph.append(locationDiv, taglineDiv, priceDiv)
+
+        article.appendChild(photographerParagraph)
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,36 +1,34 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+
+        const photographersFetchingURL = '../../data/photographers.json';
+
+        let response;
+        try {
+            response = await fetch(photographersFetchingURL)
+        } catch {
+            console.log("Erreur dans le code de requête des photographes")
+        }
+
+        if (response.ok) {
+            results = await response.json()
+        } else {
+            console.log("Le réseau n'a pas renvoyé une bonne réponse")
+        }
+
+
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: results.photographers,
+            media: results.media
+        })
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
+        const priceBasis = '€/jour';
 
         photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
+            const photographerModel = photographerFactory(photographer, priceBasis);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
@@ -43,4 +41,3 @@
     };
     
     init();
-    
