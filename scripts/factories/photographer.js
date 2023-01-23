2e4e20fe272f763db8369ea2b41fba22f3274c7a
nassmim <nassmim972@gmail.com>
2023-01-23 05:12:40 +0100

    navigation mise en place et récupération des paramètres via l'url

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,39 +1,70 @@
 function photographerFactory(data, priceBasis) {
+
     const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture);
-        img.setAttribute("alt", name)
-
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-
-        const photographerParagraph = document.createElement('p'),
+    function createElements() {
+        const article = document.createElement( 'article' ),
+            link = document.createElement('a'),
+            img = document.createElement( 'img' ),
+            h2 = document.createElement( 'h2' ),
+            paragraph = document.createElement('p'),
             locationDiv = document.createElement('div')
             taglineDiv = document.createElement('div'),
             priceDiv = document.createElement('div');
 
-        photographerParagraph.setAttribute("aria-label", "Paragraphe de présentation du photographe")
+        return [article, link, img, h2, paragraph, locationDiv, taglineDiv, priceDiv]
+    }
+
+    function setAttributes(link, img, paragraph, locationDiv, taglineDiv, priceDiv) {
+        link.setAttribute('href', `./photographer.html?id=${id}`)
+        link.setAttribute('aria-label', `Lien de redirection vers le profil du photographe ${name}`)
+        img.setAttribute("src", picture)
+        img.setAttribute("alt", name)        
+        paragraph.setAttribute("aria-label", `Paragraphe de présentation du photographe ${name}`)
+        locationDiv.setAttribute("aria-label", `Location du photographe ${name}`)
+        taglineDiv.setAttribute("aria-label", `Tagline du photographe ${name}`)
+        priceDiv.setAttribute("aria-label", `Prix à la journée du photographe ${name}`)        
+    }
+
+    function attachContents(h2, locationDiv, taglineDiv, priceDiv) {
+        h2.textContent = name;
         locationDiv.textContent = `${city}, ${country}`;
         taglineDiv.textContent = tagline;
-        priceDiv.textContent = price + priceBasis;
+        priceDiv.textContent = price + priceBasis;        
+    }
 
+    function addClasses(article, link, paragraph, locationDiv, taglineDiv, priceDiv) {
         article.classList.add("photographer-view")
-        photographerParagraph.classList.add("photographer-view__presentation")
+        link.classList.add("photographer-view__link")
+        paragraph.classList.add("photographer-view__presentation")
         locationDiv.classList.add("photographer-view__location")
         taglineDiv.classList.add("photographer-view__tagline")
-        priceDiv.classList.add("photographer-view__price")
+        priceDiv.classList.add("photographer-view__price")        
+    }
+
+    function appendChilds(article, link, img, h2, paragraph, locationDiv, taglineDiv, priceDiv) {
+
+        link.append(img, h2)
         
-        article.appendChild(img);
-        article.appendChild(h2);
+        paragraph.append(locationDiv, taglineDiv, priceDiv)
+        
+        article.appendChild(link);
+        article.appendChild(paragraph)        
+    }
+    
+    function getUserCardDOM() {
 
-        photographerParagraph.append(locationDiv, taglineDiv, priceDiv)
+        const [article, photographerLink, img, h2, photographerParagraph, locationDiv, taglineDiv, priceDiv] = createElements()
 
-        article.appendChild(photographerParagraph)
+        setAttributes(photographerLink, img, photographerParagraph, locationDiv, taglineDiv, priceDiv)
+
+        attachContents(h2, locationDiv, taglineDiv, priceDiv)
+
+        addClasses(article, photographerLink, photographerParagraph, locationDiv, taglineDiv, priceDiv)
+        
+        appendChilds(article, photographerLink, img, h2, photographerParagraph, locationDiv, taglineDiv, priceDiv)
 
         return (article);
     }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
+function getParams() {
+
+    const paramsURL = (new URL(document.location)).searchParams,
+        id = paramsURL.get('id')
+    console.log(id)
+}
+
+
+getParams()
