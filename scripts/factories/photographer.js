b81b06d533d2e77808812557aa162243d4f19734
nassmim <nassmim972@gmail.com>
2023-01-23 04:18:42 +0100

    design mis en place

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,6 +7,7 @@ function photographerFactory(data, priceBasis) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
+        img.setAttribute("alt", name)
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
@@ -16,10 +17,16 @@ function photographerFactory(data, priceBasis) {
             taglineDiv = document.createElement('div'),
             priceDiv = document.createElement('div');
 
+        photographerParagraph.setAttribute("aria-label", "Paragraphe de présentation du photographe")
         locationDiv.textContent = `${city}, ${country}`;
         taglineDiv.textContent = tagline;
         priceDiv.textContent = price + priceBasis;
 
+        article.classList.add("photographer-view")
+        photographerParagraph.classList.add("photographer-view__presentation")
+        locationDiv.classList.add("photographer-view__location")
+        taglineDiv.classList.add("photographer-view__tagline")
+        priceDiv.classList.add("photographer-view__price")
         
         article.appendChild(img);
         article.appendChild(h2);
