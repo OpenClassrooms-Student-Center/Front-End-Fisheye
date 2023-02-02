f8310691b3a75931779eaeda2dd0af27c92e62f2
nassmim <nassmim972@gmail.com>
2023-02-02 23:26:09 +0100

    correction sur la fonction de tri de sorte à conserver une cohérence par rapport à l'affichage des critères de tri

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -13,11 +13,12 @@ async function sortPortfolio(medias, sortType) {
 const customSelectElement = document.querySelector('.custom-select'),
     selectElement = customSelectElement.querySelector('select'),
     selectOptionsLength = selectElement.length
+    // selectOptions = Array.from(selectElement.children).map(child => child.textContent)
+
 
 const customSelectedOptionElement = document.createElement('div')
 customSelectedOptionElement.classList.add('custom-select__option', 'custom-select__option--selected')
 customSelectedOptionElement.innerHTML = selectElement.options[selectElement.selectedIndex].innerHTML
-
 
 customSelectElement.appendChild(customSelectedOptionElement)
 
@@ -27,30 +28,52 @@ dividerLine.classList.add('divider')
 const customOptionsElement = document.createElement('div')
 customOptionsElement.classList.add('custom-select__options', 'custom-select__options--hidden')
 
-for(let i=1; i < selectOptionsLength; i++) {
+for(let i=0; i < selectOptionsLength; i++) {
 
     const customOptionElement = document.createElement('div')
     customOptionElement.classList.add('custom-select__option', 'custom-select__option--hidden')
 
+    if (i===0) {
+        customOptionElement.classList.add('invisible')
+    }
+
     customOptionElement.innerHTML = selectElement.options[i].innerHTML
 
     customOptionElement.addEventListener('click', function(e) {
-        
+                
         let selectedOption = this.parentNode.previousSibling
         const previousContent = selectedOption.textContent
-        
-        for(let j=0; j < selectOptionsLength ; j++) {
+        selectedOption.textContent = this.textContent
 
+        let customOptions = Array.from(customOptionsElement.children)
+        const positionSelectedOption = customOptions.indexOf(this)
+
+        const lastOptions = customOptions.slice(-(selectOptionsLength-positionSelectedOption))
+        const firstOptions = customOptions.slice(0, -(selectOptionsLength-positionSelectedOption))
+        customOptions = lastOptions.concat(firstOptions)
+        // const selectedOptionToMove = customOptions.shift()
+        // customOptions.push(selectedOptionToMove)
+        customOptionsElement.append(...customOptions)
+
+        for(let j=0; j < selectOptionsLength ; j++) {
+            
             if (this.innerHTML == selectElement.options[j].innerHTML) {
                 selectElement.selectedIndex = j;
                 selectElement.dispatchEvent(new Event('change'))
-                selectedOption.textContent = this.textContent
-                this.textContent = previousContent
+                
+                // this.textContent = previousContent
+                // customOptions[j].style.display = 'none'
+                // const newPositionSelectedOption = customOptions.indexOf(this)
+                // customOptions[newPositionSelectedOption].textContent = previousContent
                 break;
-            }
+            } 
+
         }
 
         selectedOption.click()
+        const hiddenOption = this.parentNode.querySelector('.invisible')
+        hiddenOption.classList.remove('invisible')   
+        this.classList.add('invisible')     
     })
 
     customOptionsElement.appendChild(customOptionElement)
