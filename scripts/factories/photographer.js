a99d4a78adfef8c5ff044d9306c74b9f5d2bf2c1
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-02-08 15:11:25 +0100

    (feat) added infos about photographers in index.js and adapted the factory to return all infos in the page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,53 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, tagline, city, price, country } = data;
 
+
+    // Getting our Pictures
     const picture = `assets/photographers/${portrait}`;
+    console.log(picture)
+
+    // Our Ids
+    const getId = `${id}`
+    console.log(getId)
+
+    // Our taglines
+    const getTagline = `${tagline}`
+    console.log(getTagline)
+
+    // Our Cities
+    const getCity = `${city}`
+    console.log(getCity)
+
+    // Our Prices
+    const getPrices = `${price}`
+    console.log(getPrices)
+
+    // Our Countries
+    const getCountries = `${country}`
+    console.log(getCountries)
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        // For accessibility, we add an alt = photo of photographer and an aria-label to tell that you can click on the image to see more infos
+        img.setAttribute("alt", `profil de ${name}`)
+        img.setAttribute("aria-label", `cliquez pour consulter le profil de ${name}`)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement('h3')
+        h3.textContent = getTagline
+        const h4 = document.createElement('h4')
+        h4.textContent = getCountries
+        const h5 = document.createElement('h5')
+        h5.textContent = getPrices+" € /h"
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(h4);
+        article.appendChild(h5);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    // We added every data of our photographers
+    return { name, portrait, id, tagline, city, price, country, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,30 @@
+    // Our datas before fetch
+    let photographers = [];
+    // Array that will contain the result of the fetch
+    let InfosPhotographers = []
+    
+    // Function to fetch and return data
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
+       // We fetch our json file with a GET method
+        await fetch(`../data/photographers.json`, {
+        method: 'GET'
+       })
+       .then(response => {
+        return response.json();
+      }).then(photographers => {
+        // We retrieve our datas and insert them into our array infosPhotographers
+       for(i= 0; i < photographers.photographers.length; i++){
+        InfosPhotographers.push(photographers.photographers[i])     
+        console.log(InfosPhotographers)
+        console.log(InfosPhotographers)
+       }       
+      }).catch(err => {
+        console.log("error")
+      });
+        
+        // We return our array 
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: InfosPhotographers})
     }
 
     async function displayData(photographers) {
