b6f26c7d79127b7d5481466e0594f23f384e8761
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-02-13 18:26:23 +0100

    (feat) added style to photographer page

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -11,18 +11,14 @@ function getMediaCardDOM() {
     const h2 = document.createElement( 'h2' );
     h2.textContent = title;
     const h3 = document.createElement('h3')
-    h3.innerHTML = likes +`<svg class="svg" fill="#000000" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 471.701 471.701" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1 c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3 l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4 C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3 s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4 c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3 C444.801,187.101,434.001,213.101,414.401,232.701z"></path> </g> </g></svg>`
-    /*const h4 = document.createElement('h4')
-    h4.textContent = getCountries
-    const h5 = document.createElement('h5')
-    h5.textContent = getPrices+" € /h"
-    const a = document.createElement('a')
-    a.textContent = "Voir le profil"
-   // a.href=getPhotographer;*/
-    
+    h3.innerHTML = likes +`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>`
+    const span = document.createElement('span')
+    span.classList.add("infosAboutPhoto")
+
     article.appendChild(img);
-    article.appendChild(h2);
-   article.appendChild(h3);
+    article.appendChild(span);
+    span.appendChild(h2);
+    span.appendChild(h3);
     /* article.appendChild(h4);
     article.appendChild(h5);*/
 
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,5 +1,7 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    const contactButton = document.querySelector(".contact_button");
+    contactButton.style.visibility = "hidden";
 	modal.style.display = "block";
     // Added behaviour for the rest of the page
     const mediasSection = document.querySelector(".medias_section");
@@ -8,8 +10,10 @@ function displayModal() {
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const contactButton = document.querySelector(".contact_button");
     modal.style.display = "none";
     // Added behaviour for the rest of the page
+    contactButton.style.visibility = "visible";
     const mediasSection = document.querySelector(".medias_section");
     mediasSection.style.display = "grid";
 }
