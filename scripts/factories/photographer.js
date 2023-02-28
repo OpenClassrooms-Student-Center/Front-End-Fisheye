4a40f7535cfce240b741d1d40e35221dfc4066bc
Lemon <be.philibert@icloud.com>
2023-02-28 15:53:51 +0100

    add title to img + link

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,41 +7,40 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' )
 
         // create link for each photographer with id
-        const link = document.createElement("a");
-		link.href = `./photographer.html?id=${id}`;
+        const link = document.createElement("a")
+		link.href = `./photographer.html?id=${id}`
+        link.title=`Aller vers la page de ${name}`
 		article.append(link);
 
         //img
         const img = document.createElement('img')
         img.setAttribute('src', picture)
-        img.title=" "
+        img.title=`${portrait}`.replace(/.jpg/g,'')
         article.appendChild(img)
-        // put <img> into <a>
-        link.appendChild(img)
+        link.appendChild(img) // put <img> into <a>
 
         // title
         const h2 = document.createElement('h2')
         h2.textContent = name
         article.appendChild(h2)
-        // put <h2> into <a>
-        link.appendChild(h2)
+        link.appendChild(h2) // put <h2> into <a>
 
         // location (city + country)
         const location = document.createElement('span')
         location.textContent = `${city} , ${country}`
-        location.classList.add("photograph-location");
+        location.classList.add("photograph-location")
         article.appendChild(location)
 
         // paragraph
         const paragraph = document.createElement('p')
         paragraph.textContent = tagline
-        paragraph.classList.add("photograph-tagline");
+        paragraph.classList.add("photograph-tagline")
         article.appendChild(paragraph)
 
         // price
         const pricing = document.createElement('span')
         pricing.textContent = `${price}/jour`
-        pricing.classList.add("photograph-price");
+        pricing.classList.add("photograph-price")
         article.appendChild(pricing)
 
         // return article
