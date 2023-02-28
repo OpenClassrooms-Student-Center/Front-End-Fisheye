ef209df7b684d1b0f778c2179e986b39102e925d
Lemon <be.philibert@icloud.com>
2023-02-28 13:55:31 +0100

    fetch data

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,33 +1,14 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // get datas with fetch
+        const photographer = await fetch('../data/photographers.json')
+            // then > function => response
+            .then((data) => data.json())
+        // return variable
+        return photographer
     }
 
     async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+        const photographersSection = document.querySelector('.photographer_section');
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
