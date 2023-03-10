3a621f699dc48d232c300bb85377202d130089a9
Malho Stéphane <stephane.malho@gmail.com>
2023-03-10 10:22:17 +0100

    F01 Add Fetched datas from data/photographers.json

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,19 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        let photographers = []; // On crée un tableau vide pour les photographes
+
+        // On appel les data/photographers.json dans un fetch
+        // On récupère les données du fichier json
+        const response = await fetch("data/photographers.json");
+        photographers = await response.json();
+
+        console.log(photographers);
+
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers : photographers.photographers // On retourne le tableau photographers
+        });
     }
 
     async function displayData(photographers) {
