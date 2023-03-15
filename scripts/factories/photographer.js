3da1eb526eb8c8151fdfa0ae5f81f5aa75549a58
DamienL47 <damien.lataste@lapiscine.pro>
2023-03-15 12:45:08 +0100

    Récupération data photographers et affichage sur page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,12 +1,14 @@
 function photographerFactory(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/portraitsPhotographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("role", "bouton");
+        img.setAttribute("alt", name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
