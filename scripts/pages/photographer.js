3d3517bd0f4f327ef22626381f78cc7d12c34362
Belarif <b.ocine@live.fr>
2023-03-16 16:38:13 +0100

    FE1: renaming files

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,27 @@
-//Mettre le code JavaScript lié à la page photographer.html
+"use_strict";
+
+// get photographers
+async function getPhotographers() {
+    const response = await fetch("../data/photographers.json");
+    const photographers = response.json();
+
+    return photographers;
+}
+
+async function getPhotographer() {
+    const { photographers } = await getPhotographers();
+
+    const query = window.location.search;
+    const name = new URLSearchParams(query).get('name');
+
+    const photographer = photographers.filter(function(photographer) {
+        return photographer.name == name;
+    });
+    
+    return photographer;
+}
+
+getPhotographer();
+
+
+
