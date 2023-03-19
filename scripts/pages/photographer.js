e61c0599af3f016082b9e3c05f0a0b98933735eb
stephane.malho <stepahen.malho@gmail.com>
2023-03-19 15:10:05 +0100

    ✨ F03: get user total Likes and fix modal display

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,7 +2,15 @@
 async function getPhotographerData() {
   const url = new URL(window.location.href);
   const id = url.searchParams.get("id");
-  console.log(id);
+  const response = await fetch("../data/photographers.json");
+  const data = await response.json();
+  const photographer = data.photographers.filter(
+    (photographer) => photographer.id == id
+  );
+  const responseMedia = await fetch("../data/photographers.json");
+  const dataMedia = await responseMedia.json();
+  const media = dataMedia.media.filter((media) => media.photographerId == id);
+  console.log(media);
 
   function getLikes(photographerId) {
     let likes = 0;
@@ -14,24 +22,15 @@ async function getPhotographerData() {
     return likes;
   }
 
-  // Récupérer les données du photographe en fonction de l'id de l'url ainsi que les médias
-  const response = await fetch("../data/photographers.json");
-  const data = await response.json();
-  const photographer = data.photographers.filter((photographer) => photographer.id == id);
-  console.log(photographer);
-
-  const responseMedia = await fetch("../data/photographers.json");
-  const dataMedia = await responseMedia.json();
-  const media = dataMedia.media.filter((media) => media.photographerId == id);
-  console.log(media);
-
   // Créer la carte du photographe
   const photographerCard = document.querySelector(".photograph-header");
-  
+
   photographerCard.innerHTML = `
   <figure>
     <figcaption tabindex=${photographer[0].tabindex}>
-      <img src="assets/photographers/${photographer[0].portrait}" alt="portrait de ${photographer[0].name}" tabindex=${photographer[0].tabindex}>
+      <img src="assets/photographers/${
+        photographer[0].portrait
+      }" alt="portrait de ${photographer[0].name}" >
     </figcaption>
     <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
     <div class="photograph-info">
@@ -42,32 +41,33 @@ async function getPhotographerData() {
       </div>
     </div>
     </figure>
-    <p class="banner"><span>${getLikes(id)}<i class="fas fa-heart"></i></span> ${photographer[0].price}€/jour</p>
+    <p class="banner"><span>${getLikes(
+      id
+    )}<i class="fas fa-heart"></i></span> ${photographer[0].price}€/jour</p>
   `;
 
-  // Créer la liste des photos du photographe en créeant un élément html div 
+  // Créer la liste des photos du photographe en créeant un élément html div
   const mediaList = document.querySelector(".photograph-media");
-  
+
   media.forEach((photographer) => {
-    console.log(photographer);
-    console.log("The title: "+ photographer.title);
     let mediaElement;
-  
-    if (photographer.hasOwnProperty('video')) {
+
+    if (photographer.hasOwnProperty("video")) {
       // Si le média contient une vidéo, créer une balise vidéo et y ajouter l'URL de la vidéo
-      mediaElement = document.createElement('video');
+      mediaElement = document.createElement("video");
       mediaElement.src = `assets/videos/${photographer.video}`;
       mediaElement.controls = true;
     } else {
       // Si le média ne contient pas de vidéo, créer une balise img et y ajouter l'URL de l'image
-      mediaElement = document.createElement('img');
+      mediaElement = document.createElement("img");
       mediaElement.src = `assets/photos/${photographer.image}`;
       mediaElement.alt = photographer.title;
     }
-  
+
     // Créer un élément div pour chaque média et y ajouter le mediaElement et les autres éléments
-    const mediaCard = document.createElement('div');
-    mediaCard.classList.add('cards');
+    const mediaCard = document.createElement("div");
+    mediaCard.classList.add("cards");
+    mediaCard.setAttribute("tabindex", "0");
     mediaCard.innerHTML = `
     <div class="descriptionBox">
     <h2>${photographer.title}</h2>
@@ -75,15 +75,18 @@ async function getPhotographerData() {
     </div>
     `;
     mediaCard.appendChild(mediaElement);
-    
+
     // Ajouter la carte du média à la liste des médias
     mediaList.appendChild(mediaCard);
+  });
 
-});
+  const namedForm = document.querySelector(".contact_header");
+
+  namedForm.innerHTML = `
+  <h2>Contacter:</h2>
+  <button onclick="closeModal()">X</button>
+  <h3>${photographer[0].name}</h3>
+  `;
 }
 
 getPhotographerData();
-
-
-
-
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,27 @@
+
+let isOpen = false;
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  isOpen = true;
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
+  hiddeBanner();
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  isOpen = false;
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+  hiddeBanner();
 }
+
+function hiddeBanner() {
+  if (isOpen) {
+    const banner = document.querySelector(".banner");
+    banner.style.display = "none";
+  } else {
+    const banner = document.querySelector(".banner");
+    banner.style.display = "block";
+  }
+}
+
