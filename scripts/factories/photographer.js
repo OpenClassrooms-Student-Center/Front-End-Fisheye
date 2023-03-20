bfb00b43e38459503982b1ad5ee5be0e8c8baf71
Adrien <59972394+AdrienM2b@users.noreply.github.com>
2023-03-20 14:55:42 +0100

    fabrication de la card des photographes pour l'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,46 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline, id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers_ID_Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("aria-label", "lien vers le portrait de " + name);
+
+        const lienArstist = document.createElement('a');
+        lienArstist.setAttribute("href", "#");
+        lienArstist.setAttribute("aria-label", "lien vers le portrait de " + name);
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", "photo de profil de l'artiste " + name);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        h2.classList.add("artist-title");
+
+        const artistContent = document.createElement('p');
+        artistContent.classList.add("artist-content");
+
+        const location = document.createElement('p');
+        location.textContent = city + ", " + country;
+        location.style.color = "#901C1C"
+
+        const artistTagLine = document.createElement('p');
+        artistTagLine.textContent = tagline;
+
+        const artistPrice = document.createElement('p');
+        artistPrice.textContent = price + " €/jour";
+        artistPrice.style.color = "#757575"
+
+
+        article.appendChild(lienArstist);
+        lienArstist.appendChild(img);
+        lienArstist.appendChild(h2);
+        article.appendChild(artistContent);
+        artistContent.appendChild(location);
+        artistContent.appendChild(artistTagLine);
+        artistContent.appendChild(artistPrice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
