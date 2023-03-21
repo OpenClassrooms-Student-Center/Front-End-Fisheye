bdf9fd545fff8c477453e335cf77696d2163e0ed
Belarif <b.ocine@live.fr>
2023-03-21 18:44:24 +0100

    FE1:  renaming functions and variables used to display photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,79 +1,79 @@
 "use_strict";
 
 // header main elements
-const main = document.querySelector("main");
-const divMain = document.querySelector("main .photograph-header");
-const header = document.createElement("header");
-const ul = document.createElement("ul");
-const liData = document.createElement("li");
-const pName = document.createElement("p");
-const pCountry = document.createElement("p");
-const pTagline = document.createElement("p");
-const liContact = document.createElement("li");
-const contact = document.querySelector("button[class='contact_button']");
-const liImg = document.createElement("li");
-const img = document.createElement("img");
+const mainElt = document.querySelector("main");
+const divMainElt = document.querySelector("main .photograph-header");
+const headerMainElt = document.createElement("header");
+const ulHeaderElt = document.createElement("ul");
+const liHeaderElt = document.createElement("li");
+const nameElt = document.createElement("p");
+const countryElt = document.createElement("p");
+const taglineElt = document.createElement("p");
+const liContactElt = document.createElement("li");
+const buttonElt = document.querySelector("button[class='contact_button']");
+const liImgElt = document.createElement("li");
+const imgElt = document.createElement("img");
 
-// functions 
-function headerMain(header) {
-    header.setAttribute("class","photograph-header");
+// header functions 
+function addAttributeToHeaderMain(headerMainElt) {
+    headerMainElt.setAttribute("class","photograph-header");
 }
 
 // append ul element in header main
-function ulHeader(header, ul) {
-    ul.style.margin = "0";
-    ul.style.padding = "0 40px";
-    ul.style.display = "flex";
-    ul.style.width = "100%";
-    ul.style.justifyContent = "space-between";
-    ul.style.alignItems = "center";
-    header.appendChild(ul);
+function addUlHeader(headerMainElt, ulHeaderElt) {
+    ulHeaderElt.style.margin = "0";
+    ulHeaderElt.style.padding = "0 40px";
+    ulHeaderElt.style.display = "flex";
+    ulHeaderElt.style.width = "100%";
+    ulHeaderElt.style.justifyContent = "space-between";
+    ulHeaderElt.style.alignItems = "center";
+    headerMainElt.appendChild(ulHeaderElt);
 }
 
-function liPhotographerData(liData, ul) {
-    liData.style.display = "block";
-    ul.appendChild(liData);
+function addLiHeader(liHeaderElt, ulHeaderElt) {
+    liHeaderElt.style.display = "block";
+    ulHeaderElt.appendChild(liHeaderElt);
 }
 
 // displaying photographer name
-function photographerName(pName, liData, name) {
-    pName.style.margin = "10px 0";
-    pName.textContent = name;
-    pName.style.fontSize = "2.4rem";
-    pName.style.color = "#D3573C";
-    liData.appendChild(pName);
+function setPhotographerName(nameElt, liHeaderElt, name) {
+    nameElt.style.margin = "10px 0";
+    nameElt.textContent = name;
+    nameElt.style.fontSize = "2.4rem";
+    nameElt.style.color = "#D3573C";
+    liHeaderElt.appendChild(nameElt);
 }
 
 // displaying photographer country
-function photographerCountry(pCountry, liData, city, country) {
-    pCountry.style.margin = "10px 0";
-    pCountry.style.color = "#D3573C";
-    pCountry.style.fontSize = "1.2rem";
-    pCountry.textContent = `${city}, ${country}`;
-    liData.appendChild(pCountry);
+function setPhotographerCountry(countryElt, liHeaderElt, city, country) {
+    countryElt.style.margin = "10px 0";
+    countryElt.style.color = "#D3573C";
+    countryElt.style.fontSize = "1.2rem";
+    countryElt.textContent = `${city}, ${country}`;
+    liHeaderElt.appendChild(countryElt);
 }
 
 // displaying photographer tagline
-function photographerTagline(liData, pTagline, tagline) {
-    pTagline.style.margin = "10px 0";
-    pTagline.textContent = tagline;
-    liData.appendChild(pTagline);
+function setPhotographerTagline(liHeaderElt, taglineElt, tagline) {
+    taglineElt.style.margin = "10px 0";
+    taglineElt.textContent = tagline;
+    liHeaderElt.appendChild(taglineElt);
 }
 
-function contactButton(liContact, ul, contact) {
-    liContact.style.display = "block";
-    ul.appendChild(liContact).appendChild(contact);
+function addContactButton(liContactElt, ulHeaderElt, buttonElt) {
+    liContactElt.style.display = "block";
+    ulHeaderElt.appendChild(liContactElt).appendChild(buttonElt);
 }
 
 // displaying photographer image
-function photographerImg(ul, liImg, img, picture) {
-    liImg.style.display = "block";
-    ul.appendChild(liImg);
-    img.setAttribute("src", picture);
-    img.style.width = "150px";
-    img.style.height = "150px";
-    img.style.borderRadius = "50%";
-    liImg.appendChild(img);
+function setPhotographerImg(ulHeaderElt, liImgElt, imgElt, picture) {
+    liImgElt.style.display = "block";
+    ulHeaderElt.appendChild(liImgElt);
+    imgElt.setAttribute("src", picture);
+    imgElt.style.width = "150px";
+    imgElt.style.height = "150px";
+    imgElt.style.borderRadius = "50%";
+    liImgElt.appendChild(imgElt);
 }
 
 // displaying photographer data on header main
@@ -81,126 +81,127 @@ function photographerFactory(photographer) {
     const [{ name, portrait, city, country, tagline }] = photographer;
     const picture = `assets/photographers/${portrait}`;
 
-    headerMain(header);
-    ulHeader(header, ul);
-    liPhotographerData(liData, ul);
-    photographerName(pName, liData, name);
-    photographerCountry(pCountry, liData, city, country);
-    photographerTagline(liData, pTagline, tagline);
-    contactButton(liContact, ul, contact);
-    photographerImg(ul, liImg, img, picture);
-    
-    divMain.replaceWith(header);
+    divMainElt.replaceWith(headerMainElt);
+    addAttributeToHeaderMain(headerMainElt);
+    addUlHeader(headerMainElt, ulHeaderElt);
+    addLiHeader(liHeaderElt, ulHeaderElt);
+    setPhotographerName(nameElt, liHeaderElt, name);
+    setPhotographerCountry(countryElt, liHeaderElt, city, country);
+    setPhotographerTagline(liHeaderElt, taglineElt, tagline);
+    addContactButton(liContactElt, ulHeaderElt, buttonElt);
+    setPhotographerImg(ulHeaderElt, liImgElt, imgElt, picture);
 }
 
-function sectionMedia(section) {
+// media section functions 
+
+function addSectionMedia(section) {
     section.style.margin = "60px 100px";
     section.setAttribute("id","media");
-    main.appendChild(section);
+    mainElt.appendChild(section);
 }
 
-function ulSectionMedia(section, ulMedia) {
-    ulMedia.style.padding = "0";
-    ulMedia.style.display = "flex";
-    ulMedia.style.gap = "80px";
-    ulMedia.style.flexWrap= "wrap";
-    section.appendChild(ulMedia);
+function addUlMedia(section, ulMediaElt) {
+    ulMediaElt.style.padding = "0";
+    ulMediaElt.style.display = "flex";
+    ulMediaElt.style.gap = "80px";
+    ulMediaElt.style.flexWrap= "wrap";
+    section.appendChild(ulMediaElt);
 }
 
-function liSectionMedia(liMedia, ulMedia) {
-    liMedia.style.display = "block";
-    ulMedia.appendChild(liMedia);
+function addLiMedia(liMediaElt, ulMediaElt) {
+    liMediaElt.style.display = "block";
+    ulMediaElt.appendChild(liMediaElt);
 }
 
-function linkSectionMedia(liMedia, linkMedia) {
-    linkMedia.setAttribute("href","#");
-    linkMedia.style.textDecoration = "none";
-    liMedia.appendChild(linkMedia);
+function addLinkMedia(liMediaElt, linkMediaElt) {
+    linkMediaElt.setAttribute("href","#");
+    linkMediaElt.style.textDecoration = "none";
+    liMediaElt.appendChild(linkMediaElt);
 }
 
-function figureSection(figureMedia, linkMedia) {
-    figureMedia.style.margin = "0";
-    linkMedia.appendChild(figureMedia);
+function addFigureMedia(figureMediaElt, linkMediaElt) {
+    figureMediaElt.style.margin = "0";
+    linkMediaElt.appendChild(figureMediaElt);
 }
 
-function imgSection(imgMedia, figureMedia, picture) {
-    imgMedia.setAttribute("src", picture);
-    imgMedia.style.width = "300px";
-    imgMedia.style.height = "300px";
-    imgMedia.style.borderRadius = "5px";
-    figureMedia.appendChild(imgMedia);
+function setImgMedia(imgMediaElt, figureMediaElt, picture) {
+    imgMediaElt.setAttribute("src", picture);
+    imgMediaElt.style.width = "300px";
+    imgMediaElt.style.height = "300px";
+    imgMediaElt.style.borderRadius = "5px";
+    figureMediaElt.appendChild(imgMediaElt);
 }
 
-function figcaptionSection(figCaptionMedia, figureMedia) {
-    figCaptionMedia.style.display = "flex";
-    figCaptionMedia.style.justifyContent = "space-between";
-    figCaptionMedia.style.alignItems = "center";
-    figCaptionMedia.style.margin = "8px 0";
-    figureMedia.appendChild(figCaptionMedia);
+function addFigcaptionMedia(figCaptionMediaElt, figureMediaElt) {
+    figCaptionMediaElt.style.display = "flex";
+    figCaptionMediaElt.style.justifyContent = "space-between";
+    figCaptionMediaElt.style.alignItems = "center";
+    figCaptionMediaElt.style.margin = "8px 0";
+    figureMediaElt.appendChild(figCaptionMediaElt);
 }
 
-function figcaptionHeaderSection(figcaptionHeader, figCaptionMedia) {
-    figcaptionHeader.style.height = "0";
-    figCaptionMedia.appendChild(figcaptionHeader);
+function addFigcaptionHeaderMedia(figcaptionHeaderElt, figCaptionMediaElt) {
+    figcaptionHeaderElt.style.height = "0";
+    figCaptionMediaElt.appendChild(figcaptionHeaderElt);
 }
 
-function titleMediaSection(title, titleMedia, figcaptionHeader) {
-    titleMedia.textContent = title;
-    titleMedia.style.color = "#901C1C";
-    figcaptionHeader.appendChild(titleMedia);
+function setTitleMedia(title, titleElt, figcaptionHeaderElt) {
+    titleElt.textContent = title;
+    titleElt.style.color = "#901C1C";
+    figcaptionHeaderElt.appendChild(titleElt);
 }
 
-function figureLikeSection(figureLike, figCaptionMedia) {
-    figureLike.style.margin = "0";
-    figureLike.style.color = "#901C1C";
-    figCaptionMedia.appendChild(figureLike);
+function addLikeMedia(figureLikeElt, figCaptionMediaElt) {
+    figureLikeElt.style.margin = "0";
+    figureLikeElt.style.color = "#901C1C";
+    figCaptionMediaElt.appendChild(figureLikeElt);
 }
 
-function likeMediaSection(likeMedia, figureLike, likes) {
-    likeMedia.textContent = likes;
-    likeMedia.style.paddingRight = "5px";
-    figureLike.appendChild(likeMedia);
+function setLikesMedia(spanLikeElt, figureLikeElt, likes) {
+    spanLikeElt.textContent = likes;
+    spanLikeElt.style.paddingRight = "5px";
+    figureLikeElt.appendChild(spanLikeElt);
 }
 
-function iconMediaSection(figureLike, iconMedia) {
-    iconMedia.setAttribute("class","fa-solid fa-heart");
-    figureLike.appendChild(iconMedia);
+function addIconLikeMedia(figureLikeElt, iMediaElt) {
+    iMediaElt.setAttribute("class","fa-solid fa-heart");
+    figureLikeElt.appendChild(iMediaElt);
 }
 
 // displaying photographerMedia data on section main
 function photographerMediaFactory(photographerMedia) {
 
-    const section = document.createElement("section");
-    const ulMedia = document.createElement("ul");
+    const sectionElt = document.createElement("section");
+    const ulMediaElt = document.createElement("ul");
 
-    sectionMedia(section);
-    ulSectionMedia(section, ulMedia);
+    addSectionMedia(sectionElt);
+    addUlMedia(sectionElt, ulMediaElt);
 
     photographerMedia.forEach(media => {
         const { title, image, likes, date, price } = media;
         const picture = `assets/media/${image}`;
 
         // section main elements
-        const liMedia = document.createElement("li");
-        const linkMedia = document.createElement("a");
-        const figureMedia = document.createElement("figure");
-        const imgMedia = document.createElement("img");
-        const figCaptionMedia = document.createElement("figcaption");
-        const figcaptionHeader = document.createElement("header");
-        const titleMedia = document.createElement("h3");
-        const figureLike = document.createElement("figure");
-        const likeMedia = document.createElement("span");
-        const iconMedia = document.createElement("i");
+        const liMediaElt = document.createElement("li");
+        const linkMediaElt = document.createElement("a");
+        const figureMediaElt = document.createElement("figure");
+        const imgMediaElt = document.createElement("img");
+        const figCaptionMediaElt = document.createElement("figcaption");
+        const figcaptionHeaderElt = document.createElement("header");
+        const titleElt = document.createElement("h3");
+        const figureLikeElt = document.createElement("figure");
+        const spanLikeElt = document.createElement("span");
+        const iMediaElt = document.createElement("i");
 
-        liSectionMedia(liMedia, ulMedia);
-        linkSectionMedia(liMedia, linkMedia);
-        figureSection(figureMedia, linkMedia);
-        imgSection(imgMedia, figureMedia, picture);
-        figcaptionSection(figCaptionMedia, figureMedia);
-        figcaptionHeaderSection(figcaptionHeader, figCaptionMedia);
-        titleMediaSection(title, titleMedia, figcaptionHeader);
-        figureLikeSection(figureLike, figCaptionMedia);
-        likeMediaSection(likeMedia, figureLike, likes);
-        iconMediaSection(figureLike, iconMedia);
+        addLiMedia(liMediaElt, ulMediaElt);
+        addLinkMedia(liMediaElt, linkMediaElt);
+        addFigureMedia(figureMediaElt, linkMediaElt);
+        setImgMedia(imgMediaElt, figureMediaElt, picture);
+        addFigcaptionMedia(figCaptionMediaElt, figureMediaElt);
+        addFigcaptionHeaderMedia(figcaptionHeaderElt, figCaptionMediaElt);
+        setTitleMedia(title, titleElt, figcaptionHeaderElt);
+        addLikeMedia(figureLikeElt, figCaptionMediaElt);
+        setLikesMedia(spanLikeElt, figureLikeElt, likes);
+        addIconLikeMedia(figureLikeElt, iMediaElt);
     });
 }
