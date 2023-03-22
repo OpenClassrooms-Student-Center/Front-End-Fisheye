69e1580fe2db953cd91846b860894918b50f8769
Manoa Rasolomalala <manoatony.pro@gmail.com>
2023-03-22 15:10:21 +0100

    Import data fetch and display photographers

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,72 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+class PhotographerFactory {
+  constructor(data) {
+    this.name = data.name;
+    this.id = data.id;
+    this.city = data.city;
+    this.country = data.country;
+    this.tagline = data.tagline;
+    this.price = data.price;
+    this.portrait = data.portrait;
 
-    const picture = `assets/photographers/${portrait}`;
+    // Init ID and Picture
+    const hrefPhotographer = `./photographer.html?id=${this.id}`;
+    const picture = `./assets/photographers/aIDPhotographersImages/${this.portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+    //DOM elements of photographer
+    this.h1 = document.createElement("h1");
+    this.h2 = document.createElement("h2");
+    this.article = document.createElement("article");
+    this.a = document.createElement("a");
+    this.img = document.createElement("img");
+    this.divCity = document.createElement("div");
+    this.divTagline = document.createElement("div");
+    this.divPrice = document.createElement("div");
+    this.divPhotographerInfos = document.createElement("div");
+
+    //Set attributes and class for the CSS
+    this.img.setAttribute("src", picture);
+    this.a.setAttribute("href", hrefPhotographer);
+    this.a.setAttribute("aria-label", this.name);
+    this.divCity.classList.add("city");
+    this.divTagline.classList.add("tagline");
+    this.divPrice.classList.add("price");
+    this.divPhotographerInfos.classList.add("photographer-infos");
+  }
+
+  getUserCardDOM() {
+    //Text injected in HTML elements
+    this.h2.textContent = this.name;
+    this.divCity.textContent = `${this.city}, ${this.country}`;
+    this.divTagline.textContent = this.tagline;
+    this.divPrice.textContent = `${this.price}€/jour`;
+
+    //Add creates element in the DOM
+    this.article.appendChild(this.a);
+    this.a.appendChild(this.img);
+    this.a.appendChild(this.h2);
+    this.article.appendChild(this.divCity);
+    this.article.appendChild(this.divTagline);
+    this.article.appendChild(this.divPrice);
+
+    const article = this.article;
+    const price = this.price;
+    return { article, price };
+  }
+
+  getPhotographerInfos() {
+    //Text injected in HTML elements
+    this.h1.textContent = this.name;
+    this.divCity.textContent = `${this.city}, ${this.country}`;
+    this.divTagline.textContent = this.tagline;
+
+    //Add created elements in the DOM
+    this.divPhotographerInfos.appendChild(this.h1);
+    this.divPhotographerInfos.appendChild(this.divCity);
+    this.divPhotographerInfos.appendChild(this.divTagline);
+
+    const photographerName = this.name;
+    const photographerPic = this.img;
+    const divPhotographerInfos = this.divPhotographerInfos;
+    return { photographerPic, photographerName, divPhotographerInfos };
+}
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,34 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+   // Fetch JSON
+   return fetch("./data/photographers.json")
+      .then(function (res) {
+         if (res.ok) {
+            return res.json();
+         }
+      })
+      .then(function (data) {
+         return data;
+      })
+      .catch(function (err) {
+         alert("An errors occurs : " + err);
+      });
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+   // Display photographers
+   const photographersSection = document.querySelector("#photographer_section");
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+   photographers.forEach((photographer) => {
+      const photographerModel = new PhotographerFactory(photographer);
+      const userCardDOM = photographerModel.getUserCardDOM();
+      photographersSection.appendChild(userCardDOM.article);
+   });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
-    
+async function init() {
+   // Récupère les datas des photographes
+   const { photographers } = await getPhotographers();
+   displayData(photographers);
+};
+
+init();
