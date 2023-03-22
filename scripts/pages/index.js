a477b1f74fd38b812e12291745f0b977cfc6ab20
Manoa Rasolomalala <manoatony.pro@gmail.com>
2023-03-22 16:55:45 +0100

    Fix folder and files

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,34 +1,34 @@
 async function getPhotographers() {
-   // Fetch JSON
-   return fetch("./data/photographers.json")
-      .then(function (res) {
-         if (res.ok) {
-            return res.json();
-         }
-      })
-      .then(function (data) {
-         return data;
-      })
-      .catch(function (err) {
-         alert("An errors occurs : " + err);
-      });
+  // Fetch JSON
+  return fetch("./data/photographers.json")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (err) {
+      alert("An errors occurs : " + err);
+    });
 }
 
 async function displayData(photographers) {
-   // Display photographers
-   const photographersSection = document.querySelector("#photographer_section");
+  // Display photographers
+  const photographersSection = document.querySelector("#photographer_section");
 
-   photographers.forEach((photographer) => {
-      const photographerModel = new PhotographerFactory(photographer);
-      const userCardDOM = photographerModel.getUserCardDOM();
-      photographersSection.appendChild(userCardDOM.article);
-   });
+  photographers.forEach((photographer) => {
+    const photographerModel = new PhotographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM.article);
+  });
 }
 
 async function init() {
-   // Récupère les datas des photographes
-   const { photographers } = await getPhotographers();
-   displayData(photographers);
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
 };
 
 init();
