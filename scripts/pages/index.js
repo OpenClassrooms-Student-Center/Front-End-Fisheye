694f5b196eb83546dd95451d1a98cd58f33a1ddc
rachon34 <rachon3441@gmail.com>
2023-04-10 21:56:59 +0200

    mise en place des données + debut css accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,10 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        let response = await fetch("../data/photographers.json")
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        photographers = await response.json();
+        return photographers;
     }
 
     async function displayData(photographers) {
