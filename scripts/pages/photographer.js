837d5e0fe9b635e1e751c786e83a736ac61ab6b0
Léo Llorca <leollorca@proton.me>
2023-04-16 17:51:13 +0200

    display home and photographer pages with data

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,45 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographer() {
+  const response = await fetch("data/photographers.json");
+  const data = await response.json();
+  const photographers = data.photographers;
+  const urlParams = new URL(document.location).searchParams;
+  const id = urlParams.get("id");
+  return photographers.find((photographer) => photographer.id == id);
+}
+
+async function displayHeaderData(photographer) {
+  const photographerHeader = document.querySelector(".photographer-header");
+  const photographerModel = photographerFactory(photographer);
+  const { infos, avatar } = photographerModel.getPhotographerHeaderElements();
+
+  avatar.setAttribute("class", "avatar");
+
+  photographerHeader.prepend(infos);
+  photographerHeader.appendChild(avatar);
+}
+
+async function displayMedias(photographer) {
+  const main = document.querySelector("#main");
+
+  const mediasSection = document.createElement("section");
+  mediasSection.setAttribute("class", "medias");
+
+  const photographerModel = photographerFactory(photographer);
+  const medias = await photographerModel.getMedias();
+
+  medias.forEach((media) => {
+    const mediaModel = mediaFactory(media);
+    const mediaCard = mediaModel.getMediaCard();
+    mediasSection.appendChild(mediaCard);
+  });
+
+  main.appendChild(mediasSection);
+}
+
+async function init() {
+  const photographer = await getPhotographer();
+  displayHeaderData(photographer);
+  displayMedias(photographer);
+}
+
+init();
