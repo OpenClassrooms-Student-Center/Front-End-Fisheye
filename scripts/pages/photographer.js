1322b3d0160fa0bd99fc7276727a19f3357f8178
halima derrahi <halima.derrahi@gmail.com>
2023-04-17 13:08:10 +0200

    update

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
 //Mettre le code JavaScript lié à la page photographer.html
+//import { getPhotographers } from "index.js";
+
+
+//const photographHeader = document.querySelector(".photograph-header");
+
+
+
+
+
