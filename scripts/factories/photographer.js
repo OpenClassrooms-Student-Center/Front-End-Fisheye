a89033a4405f29431b8de3bc81420db043befffe
halima derrahi <halima.derrahi@gmail.com>
2023-04-17 13:08:57 +0200

    update

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+/* eslint-disable indent */
+// Function Photographers
+export function photographerFactory(data) {
+	const { id, name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+	function getUserCardDOM() {
+        
+        const a = document.createElement("a");
+        a.setAttribute("href",`photographer.html?id=${id}`);
+		a.setAttribute ("title", `${name}`);
+		
+		const article = document.createElement( "article" );
+
+		const img = document.createElement( "img" );
+		img.setAttribute("src", picture);
+
+		const h2 = document.createElement( "h2" );
+		h2.textContent = name;
+
+		const h3 = document.createElement( "h3" );
+		h3.textContent = city + ", " + country;
+
+        const h4 = document.createElement( "h4" );
+        h4.textContent = tagline;
+
+        const p = document.createElement( "p" );
+        p.textContent = price + "€" + "/jour";
+		
+		a.appendChild(img);
+		a.appendChild(h2);
+
+		article.append(a , h3 , h4 , p);
+		
         return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	}
+	return { name, picture, getUserCardDOM };
 }
