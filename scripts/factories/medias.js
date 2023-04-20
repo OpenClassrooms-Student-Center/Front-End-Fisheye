dd01488f8e01e00aff8c2a02325e1ff00cbb95d1
Lemon <be.philibert@icloud.com>
2023-04-20 14:50:25 +0200

    fix video autoplay and other stuff in lightbox view

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -21,20 +21,36 @@ function mediaFactory(media) {
         mediaElement.classList.add('photograph-media-video');
         mediaElement.src = `assets/images/${media.photographerId}/${media.video}`;
         mediaElement.setAttribute('role', 'img');
-        mediaElement.setAttribute('mediaID', `${media.id}`)
+        mediaElement.setAttribute('mediaID', `${media.id}`);
 
         mediaLinkElement = mediaElement.cloneNode(true);
         mediaLinkElement.href = `assets/images/${media.photographerId}/${media.video}`;
         mediaLinkElement.target = '_blank';
 
-        // Create a new <video> element and append it to both the .photograph-media-link and .media-lightbox elements
+        // Create a new <video> element and append it to the .photograph-media-link element
         const mediaLinkVideoElement = mediaElement.cloneNode(true);
-        mediaLinkVideoElement.classList.add('photograph-media-img');
         mediaLink.appendChild(mediaLinkVideoElement);
-        const mediaLightboxVideoElement = mediaElement.cloneNode(true);
+
+        // Create a new <video> element for the mediaLightboxElement and add the necessary attributes
+        const mediaLightboxVideoElement = document.createElement('video');
+        mediaLightboxVideoElement.classList.add('photograph-media-video');
+        mediaLightboxVideoElement.src = `assets/images/${media.photographerId}/${media.video}`;
+        mediaLightboxVideoElement.setAttribute('role', 'img');
+        mediaLightboxVideoElement.setAttribute('mediaID', `${media.id}`);
+        mediaLightboxVideoElement.setAttribute('autoplay', 'on');
+        mediaLightboxVideoElement.setAttribute('loop', '');
+
+        // Add a <source> element to the mediaLightboxVideoElement to specify the video file type
+        const sourceElement = document.createElement('source');
+        sourceElement.src = `assets/images/${media.photographerId}/${media.video}`;
+        sourceElement.type = 'video/mp4';
+        mediaLightboxVideoElement.appendChild(sourceElement);
+
+        // Create a new <div> element for the media lightbox and append the mediaLightboxVideoElement
         mediaLightboxElement = document.createElement('div');
         mediaLightboxElement.classList.add('media-lightbox');
         mediaLightboxElement.appendChild(mediaLightboxVideoElement);
+
 
       } else {
         const mediaElement = document.createElement('img');
@@ -60,6 +76,7 @@ function mediaFactory(media) {
 
       mediaDOM.appendChild(mediaLink);
 
+      // create .media-lightbox-container 
       const mediaLightboxContainer = document.createElement('div');
       mediaLightboxContainer.classList.add('media-lightbox-container');
       mediaLightboxContainer.appendChild(mediaLightboxElement);
