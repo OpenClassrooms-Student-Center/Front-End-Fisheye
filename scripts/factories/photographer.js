2de6ca12a673e500c84f42a0b8475a45304c8c33
Thomas Chevron <chevron.thomas33@gmail.com>
2023-05-05 18:07:49 +0200

    début de la gestion de la modal de contact

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -8,17 +8,22 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
         img.alt = `Photo de ${name}`;
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
         const pVille = document.createElement( 'p' );
         pVille.textContent = `${city}, ${country}`;
         pVille.classList.add('ville');
+
         const pTagline = document.createElement( 'p' );
         pTagline.textContent = tagline;
         pTagline.classList.add('tagline');
+
         const pPrice = document.createElement( 'p' );
         pPrice.textContent = `${price}€/jour`;
         pPrice.classList.add('price');
+        
         const link = document.createElement( 'a' );
         link.href = `photographer.html?id=${id}`;
         
@@ -31,7 +36,37 @@ function photographerFactory(data) {
                 
         return (article);
     }
-    return { name, picture, id, city, country, tagline, price, getUserCardDOM }
+
+    function getPhotographerDOM(){
+        const div = document.createElement( 'div' );
+        div.classList.add('infoPhotographer');
+
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+
+        const pVille = document.createElement( 'p' );
+        pVille.textContent = `${city}, ${country}`;
+        pVille.classList.add('ville');
+
+        const pTagline = document.createElement( 'p' );
+        pTagline.textContent = tagline;
+        pTagline.classList.add('tagline');
+
+        const img = document.createElement( 'img' );
+        img.src = picture;
+        img.alt = `Photo de ${name}`;
+        img.classList.add('imgPhotographer');
+
+        div.appendChild(h2);
+        div.appendChild(pVille);
+        div.appendChild(pTagline);
+
+        return {div, img};
+    }
+
+    return { name, picture, id, city, country, tagline, price, getUserCardDOM, getPhotographerDOM }
+    
+
     
 }
 export { photographerFactory };
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,58 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Mettre le code JavaScript lié à la page photographer.html ici.
+import { photographerFactory } from "../factories/photographer.js";
+// import { mediaFactory } from "../factories/mediaFactory.js";
+let mediaPhotographer = [];
+async function dataPhotographer(file) {
+    const response = await fetch(file);
+    const data = await response.json();
+    return data;
+};
+
+const foundPhotographer = (photographers, id) =>{
+    const photographer = photographers.find(photographer => photographer.id === id);
+    return photographer;
+};
+
+
+
+const foundMedia = (media, id) =>{
+    const mediaPhotographer = media.filter(media => media.photographerId === id);
+    return mediaPhotographer;
+};
+
+function header(photographer){
+    const factoryPhotographer = photographerFactory(photographer);
+    const photographHeader = document.querySelector(".photograph-header");
+    const photographerDOM = factoryPhotographer.getPhotographerDOM();
+    const {div, img} = photographerDOM;
+    const boutonContact = document.querySelector(".contact_button");
+    photographHeader.insertBefore(div, boutonContact);
+    photographHeader.insertBefore(img, boutonContact.nextSibling);
+    const modalContact = document.getElementById("contact_modal");
+    modalContact.setAttribute("aria-label", `Contactez-moi ${photographer.name}`);
+}
+
+
+
+
+
+async function init(){
+    const data = await dataPhotographer('../../data/photographers.json');
+    const {photographers, media} = data;
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const idPhotographer = parseInt(urlParams.get('id'));
+
+    const photographer = foundPhotographer(photographers, idPhotographer);
+    const photographerName = document.querySelector(".photographerName");
+    photographerName.textContent = photographer.name;
+    
+    mediaPhotographer = foundMedia(media, idPhotographer);
+    
+    header(photographer);
+    foundMedia(media, idPhotographer);
+    foundPhotographer(photographers, idPhotographer);
+    
+};
+
+init();
