efc46ad3c32aa24a5257968bd79b32cc125eb2f0
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-05-07 17:15:20 +0200

    hover and focus on btn

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,9 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.querySelector(".contact__modal");
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    const modal = document.querySelector(".contact__modal");
     modal.style.display = "none";
 }
