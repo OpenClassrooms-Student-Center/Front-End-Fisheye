dbc1635316bd81cad9f218f9449dae6dbe789caf
Adrien <59972394+AdrienM2b@users.noreply.github.com>
2023-05-08 16:59:37 +0200

    changements

--- scripts/factories/media-factory.js
+++ scripts/factories/media-factory.js
@@ -6,26 +6,31 @@ function mediaFactory(dataMedia){
     const mp4 = `assets/photographers/Sample_Photos/${photographerId}/${video}`
 
     function createMedia(){
-        //donc en gros on doit avoir 
+        //Une div qui contient le lien image + le contenu sous l'image 
+        const mediaContainer = document.createElement('div');
+        mediaContainer.classList.add('media-container'); 
         // une ligne pour creer l'image
         const mediaPhotograph = video
             ? document.createElement('video')
             : document.createElement('img');
-        //Une ligne qui créer une div pour contenir les élements 
-        const boxMedia = document.createElement('div');
-        boxMedia.classList.add('media')
+        //Une ligne qui créer un lien pour contenir les élements 
+        const mediaLink = document.createElement('a');
+        mediaLink.classList.add('media')
+        mediaLink.setAttribute('href', video ? mp4 : picture)
         // Définir l'attribut `src` de l'élément `video` ou `img` en fonction de la valeur de `video`
         mediaPhotograph.setAttribute('src', video ? mp4 : picture);
 
         //afficher le content de l'image
         const imgContent = document.createElement('div')
         imgContent.classList.add('img-content')
+
         //une ligne qui va chercher le titre dans le json
         const imgTitle = document.createElement('p');
         imgTitle.textContent = title;
         // creation du bouton contenant le nbr de like et le bouton coeur
         const likeButton = document.createElement('button');
         likeButton.classList.add('like-button');
+
         //une ligne qui va chercher le nombre de likes dans le json
         const nbrOfLikes = document.createElement('p');
         nbrOfLikes.classList.add('nbr_of_likes');
@@ -36,13 +41,17 @@ function mediaFactory(dataMedia){
 
         imgContent.appendChild(imgTitle)
         imgContent.appendChild(likeButton)
+
         likeButton.appendChild(nbrOfLikes)
         likeButton.appendChild(icone)
-        boxMedia.appendChild(mediaPhotograph)
-        boxMedia.appendChild(imgContent)
+
+        mediaLink.appendChild(mediaPhotograph)
+    
+        mediaContainer.appendChild(mediaLink)
+        mediaContainer.appendChild(imgContent)
 
         //et on retourne la div
-        return boxMedia
+        return mediaContainer
     }
 
     return {picture , video, createMedia}
