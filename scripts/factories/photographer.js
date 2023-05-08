8db6f4fa5bd037b164287ca522df7122bebc1088
Camille Linan <camille.linan@hotmail.com>
2023-05-08 03:01:19 +0200

    add accessibility for home page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,14 +6,53 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = 
-        `<article class="photographe_article">
-            <a href="./photographer.html?id=${id}" class="photographe_article_link">
-                <img src="${picture}" alt="${name}" class="photographe_article_img" />
-                <h2 class="photographe_article_name">${name}</h2>
+        `<article 
+            class="photographe_article" 
+            tabindex="0" 
+            aria-label="Résumé du photographe ${name}"
+        >
+            <a 
+                href="./photographer.html?id=${id}" 
+                tabindex="0" class="photographe_article_link" 
+                title="Page du photographe ${name}" 
+                aria-label="Visiter la page du photographe ${name}"
+            >
+                <img 
+                    src="${picture}" 
+                    alt="${name}" 
+                    aria-label="Photo de profil de ${name}" 
+                    class="photographe_article_img" tabindex="0" 
+                />
+                <h2 
+                    class="photographe_article_name" 
+                    aria-label="Nom du photographe : ${name}" 
+                    tabindex="0"
+                >
+                    ${name}
+                </h2>
             </a>
-            <p class="photographe_article_infos photographe_article_city">${city}, ${country}</p>
-            <p class="photographe_article_infos photographe_article_tagline">${tagline}</p>
-            <p class="photographe_article_infos photographe_article_price">${price}€/jour</p>
+            <p 
+                class="photographe_article_infos photographe_article_city" 
+                aria-label="Ville de ${name} : ${city}, ${country}" 
+                tabindex="0"
+            >
+                ${city}, ${country}
+            </p>
+            <p 
+                class="photographe_article_infos 
+                photographe_article_tagline" 
+                aria-label="Slogan de ${name} : ${tagline}" 
+                tabindex="0"
+            >
+                ${tagline}
+            </p>
+            <p 
+                class="photographe_article_infos photographe_article_price" 
+                aria-label="Prix par jour de ${name} : ${price} euros par jour" 
+                tabindex="0"
+            >
+                ${price}€/jour
+            </p>
         </article>`
 
         const section = document.createElement('section');
