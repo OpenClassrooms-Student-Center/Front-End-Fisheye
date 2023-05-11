610b0070e181b46d4c44cbd5dd47d07f9b460df0
dokaNc <cirpan.dogukan5959@gmail.com>
2023-05-12 00:12:41 +0200

    home-page :sparkles: add photographer details & stylize

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait, price } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -8,13 +8,22 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3 = document.createElement( 'h3' );
+        const p = document.createElement( 'p' );
         const h5 = document.createElement( 'h5' );
         h2.textContent = name;
+        h3.textContent = `${city}, ${country}`;
+        p.textContent = tagline;
         h5.textContent = `${price} €/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
         article.appendChild(h5);
         return (article);
     }
+
+    // function qui affichera la page d'un seul photographe (id)
+
     return { name, picture, getUserCardDOM }
 }
