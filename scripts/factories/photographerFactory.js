4bb156dcb0fa1da510fa078de180fe90151a9cf0
dokaNc <cirpan.dogukan5959@gmail.com>
2023-05-12 00:42:32 +0200

    home-page :sparkles: add accessibility ARIA HTML

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -6,20 +6,27 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         const h3 = document.createElement( 'h3' );
         const p = document.createElement( 'p' );
         const h5 = document.createElement( 'h5' );
+
+        img.setAttribute("src", picture)
+        img.setAttribute("alt",`Photo du photographe`);
+        img.setAttribute("aria-label",`Photo du photographe ${name}`);
+        h5.setAttribute("aria-label", "Le prix journalier du photographe")
+
         h2.textContent = name;
         h3.textContent = `${city}, ${country}`;
         p.textContent = tagline;
         h5.textContent = `${price} €/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3);
         article.appendChild(p);
         article.appendChild(h5);
+        
         return (article);
     }
 
