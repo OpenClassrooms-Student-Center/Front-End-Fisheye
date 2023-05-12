221c5373459d9ea3fcd69e83bcee79249608f4d3
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-05-12 17:07:24 +0200

    add a no-scroll class to body when modal is open

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,12 @@
+const body = document.querySelector("body");
 const main = document.querySelector("#main");
 const modal = document.querySelector(".modal");
 
 export function displayModal() {
 	main.setAttribute("aria-hidden", "true");
     modal.setAttribute("aria-hidden", "false");
-    modal.style.display = "flex"
+    modal.style.display = "flex";
+    body.classList.add("no-scroll");
 }
 
 
@@ -12,5 +14,6 @@ export function closeModal() {
     main.setAttribute("aria-hidden", "false");
     modal.setAttribute("aria-hidden", "true");
     modal.style.display = "none"
+    body.classList.remove("no-scroll");
 
 }
