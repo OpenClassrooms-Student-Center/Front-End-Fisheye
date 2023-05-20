be210c4881c79b49f5a2a93caf97a832e13ec592
Omar Rabie <omar.rabie@hotmail.fr>
2023-05-20 15:26:49 +0200

    finir les cartes sur la page d'acceuil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,48 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, country, tagline, price, description, link, id } = data;    
     const picture = `assets/photographers/${portrait}`;
+    
     // card creation
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        // create different elements in the DOM
+        const article = document.createElement('article');
+        const personalPhoto = document.createElement('section');
+        personalPhoto.setAttribute('tabindex', '0');
+        personalPhoto.setAttribute('role' , 'region');
+        personalPhoto.setAttribute("aria-label" , name);
+        personalPhoto.setAttribute("id" , id);
+        const a = document.createElement ('a');
+        a.setAttribute("href", link);
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", description);
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h4 = document.createElement('h4');
+        h4.textContent = `${city}, ${country}`
+        const p1 = document.createElement('p');
+        const personalInfo = document.createElement('section');
+        personalInfo.setAttribute('tabindex' , '0');
+        personalInfo.setAttribute('role' , 'region');
+        personalInfo.setAttribute("aria-labelledby" , 'photographer information');
+        p1.textContent = tagline;
+        const p2 = document.createElement('p');
+        p2.textContent = `${price}€/jour`;
+        // attach elements to their parents after craetion in the DOM
+        article.appendChild(personalPhoto);
+        personalPhoto.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(personalInfo);
+        personalInfo.appendChild(h4);
+        personalInfo.appendChild(p1);
+        personalInfo.appendChild(p2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {getUserCardDOM}
 }
+
+const onClick = (event) => {
+    console.log(event.target.id);
+  }
+  window.addEventListener('click', onClick);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,5 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// js code for the page photographer.html
+const h1 = document.createElement('h1');
+const h3 = document.createElement('h3');
+const p = document.createElement('p');
+const photo = document.createElement('img');
