cde8a38cc8ab3c896d1ce217505612501301f48b
Camille Linan <camille.linan@hotmail.com>
2023-05-31 17:07:23 +0200

    refacto contactForm

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -46,96 +46,86 @@ const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 
-const firstnameError = document.getElementById("error-firstname");
-const lastnameError = document.getElementById("error-lastname");
-const emailError = document.getElementById("error-email");
-const messageError = document.getElementById('error-message');
-
-// Regex
-const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i;
-const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-
-// Valid Inputs
-const validFirstname = function(inputFirstname) {
-    if (inputFirstname.value.trim() === "" || inputFirstname.value.length < 2) {
-        firstnameError.textContent = "Veuillez saisir votre prénom (2 caractères minimum).";
-        firstnameError.ariaHidden = false;
-        firstnameError.tabIndex = 0;
-        firstname.classList.add("invalid");
-        firstname.ariaInvalid = true;
-        return false;
-    } else if (!nameRegex.test(inputFirstname.value)) {
-        firstnameError.textContent = "Veuillez saisir un prénom valide, sans chiffre, ni caractère spécial.";
-        firstnameError.ariaHidden = false;
-        firstnameError.tabIndex = 0;
-        firstname.classList.add("invalid");
-        firstname.ariaInvalid = true;
+// VALID INPUTS
+const validField = function(inputField, errorField, errorMessage, regex) {
+    if (inputField.value.trim() === "" || inputField.value.length < 2 || (regex && !regex.test(inputField.value))) {
+        errorField.textContent = errorMessage;
+        errorField.ariaHidden = false;
+        errorField.tabIndex = 0;
+        inputField.classList.add("invalid");
+        inputField.ariaInvalid = true;
         return false;
     } else {
-        firstnameError.textContent = "";
-        firstnameError.tabIndex = -1;
-        firstname.classList.remove("invalid");
-        firstname.removeAttribute('aria-invalid');
-        firstname.removeAttribute('aria-describedby');
+        errorField.textContent = "";
+        errorField.tabIndex = -1;
+        inputField.classList.remove("invalid");
+        inputField.removeAttribute('aria-invalid');
         return true;
     }
+};
+
+const validFirstname = function(inputFirstname) {
+    const firstnameError = document.getElementById("error-firstname");
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i;
+
+    return validField(
+        inputFirstname,
+        firstnameError,
+        "Veuillez saisir votre prénom, sans chiffre ni caractère spécial (2 caractères minimum).",
+        nameRegex
+    );
 }
 
 const validLastname = function(inputLastname) {
-    if (inputLastname.value.trim() === "" || inputLastname.value.length < 2) {
-        lastnameError.textContent = "Veuillez saisir votre nom (2 caractères minimum).";
-        lastname.classList.add("invalid");
-        return false;
-    } else if (!nameRegex.test(inputLastname.value)) {
-        lastnameError.textContent = "Veuillez saisir un nom valide, sans chiffre, ni caractère spécial.";
-        lastname.classList.add("invalid");
-        return false;
-    } else {
-        lastnameError.textContent = "";
-        lastname.classList.remove("invalid");
-        return true;
-    }
+    const lastnameError = document.getElementById("error-lastname");
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/i;
+
+    return validField(
+        inputLastname,
+        lastnameError,
+        "Veuillez saisir votre nom, sans chiffre ni caractère spécial (2 caractères minimum).",
+        nameRegex
+    );
 }
 
 const validEmail = function(inputEmail) {
-    if (inputEmail.value.trim() === "" || !emailRegex.test(email.value)) {
-        emailError.textContent = "Veuillez saisir une adresse e-mail valide.";
-        email.classList.add("invalid");
-        return false;
-    } else {
-        emailError.textContent = "";
-        email.classList.remove("invalid");
-        return true;
-    }
+    const emailError = document.getElementById("error-email");
+    const emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+
+    return validField(
+        inputEmail,
+        emailError,
+        "Veuillez saisir une adresse e-mail valide.",
+        emailRegex
+    );
 }
 
 const validMessage = function(inputMsg) {
-    if (inputMsg.value.trim() === "") {
-      messageError.textContent = "Veuillez saisir un message.";
-      message.classList.add("invalid");
-      return false;
-    } else {
-      messageError.textContent = "";
-      message.classList.remove("invalid");
-      return true;
-    }
+    const messageError = document.getElementById('error-message');
+    
+    return validField(
+        inputMsg,
+        messageError,
+        "Veuillez saisir un message.",
+        null
+    );
 }
 
 // Listen changes in Input
-firstname.addEventListener('change', function() {
+firstname.addEventListener('input', function() {
     validFirstname(this);
 });
-lastname.addEventListener('change', function() {
+lastname.addEventListener('input', function() {
     validLastname(this);
 });
-email.addEventListener('change', function() {
+email.addEventListener('input', function() {
     validEmail(this);
 });
-message.addEventListener('change', function() {
+message.addEventListener('input', function() {
     validMessage(this);
 });
 
-// If form is valid, create msg with values
+// If inputs are valid, create msg with values
 function validFormAndGetValues() {
     if (validFirstname(firstname) && validLastname(lastname) && validEmail(email) && validMessage(message)) {
         const userMsg = {
@@ -152,7 +142,7 @@ function validFormAndGetValues() {
     }
 }
 
-// Submit form
+// SUBMIT FORM
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     validFormAndGetValues();
