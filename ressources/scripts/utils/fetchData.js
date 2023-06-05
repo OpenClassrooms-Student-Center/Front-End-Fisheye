a2fddb40f5e798027d31da3375056afe05807390
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-06-05 15:42:28 +0200

    fixing issue with index.js, new fetchData.js

--- ressources/scripts/utils/fetchData.js
+++ ressources/scripts/utils/fetchData.js
@@ -1,6 +1,14 @@
 export async function getPhotographers() {
-  const response = await fetch("../ressources/data/photographers.json");
-  const datas = await response.json();
-  const { photographers, media} = datas
-  return { photographers, media };
+  // const response = await fetch("../ressources/data/photographers.json");
+  // const datas = await response.json();
+  // const { photographers, media} = datas
+  // return { photographers, media };
+  try {
+    const response = await fetch("../ressources/data/photographers.json");
+    const result = await response.json();
+    const { photographers, media} = result;
+    return { photographers, media };
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
