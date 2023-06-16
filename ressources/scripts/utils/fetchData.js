4c6563339059d180d6f8e04b29bdbe983045b770
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2023-06-16 16:33:29 +0200

    log

--- ressources/scripts/utils/fetchData.js
+++ ressources/scripts/utils/fetchData.js
@@ -1,5 +1,5 @@
-
 export async function getPhotographers() {
+  console.log("fetch");
   const response = await fetch("../../data/photographers.json");
   const datas = await response.json();
   const { photographers, media} = datas
