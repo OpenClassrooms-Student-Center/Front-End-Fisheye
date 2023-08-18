d4fbb51eab1f3afd1e448d66325773a663592758
Manidoux41 <lherissondu41@gmail.com>
2023-08-18 22:05:19 +0200

    test2

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,25 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait,city, country } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const link = document.createElement( 'a' );
+        link.href=`./photographer.html?id=${data.id}`
         const img = document.createElement( 'img' );
+        const divLocation = document.createElement( 'div' );
+        divLocation.classList.add('location');
+        divLocation.textContent = `${city}, ${country}`;
         img.setAttribute("src", picture)
+        img.classList.add('img')
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        link.appendChild(divLocation);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
