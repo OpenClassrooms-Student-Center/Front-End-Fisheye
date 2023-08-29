597b8bf1846a7773c5bde795bb155c8d6fc0aec6
nburd01 <n.burdon@protonmail.com>
2023-08-29 18:06:02 +0200

    filtering photographer done

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,39 +1,42 @@
-// Get the photographer ID from the URL parameter
-let id = new URLSearchParams(window.location.search).get("id");
-
-//------------------------------------------------------------------------
-
-async function getPhotographers() {
+async function getPhotographerById(id) {
   // Retrieve photographers
   const photographersResponse = await fetch("photographers.json");
   const photographersData = await photographersResponse.json();
   console.log("photographersData", photographersData);
 
-  return photographersData;
+  const photographer = photographersData.find(
+    (photographer) => photographer.id === Number(id)
+  );
+
+  return photographer;
 }
 
 //------------------------------------------------------------------------
 
-async function displayData(photographers) {
+async function displayData() {
   // Find the photographer with the matching id
-  const photographer = photographers.find(
-    (photographer) => photographer.id === Number(id)
-  );
-  if (!photographer) {
-    console.log("Photographer not found");
-    return;
+  let id = new URLSearchParams(window.location.search).get("id");
+
+  try {
+    const photographer = await getPhotographerById(id);
+    if (!photographer) {
+      console.log("Photographer not found");
+      return;
+    }
+
+    // Create the photographer's template
+    const photographerModel = photographerPageTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+
+    // Add the elements to the "photograph-header" class
+    const photographerInfo = document.querySelector(".photograph-header");
+    photographerInfo.appendChild(userCardDOM);
+
+    // Update the page title
+    document.title = `Fisheye - ${photographer.name}`;
+  } catch (error) {
+    console.error("Error fetching photographer data:", error);
   }
-
-  // Create the photographer's template
-  const photographerModel = photographerPageTemplate(photographer);
-  const userCardDOM = photographerModel.getUserCardDOM();
-
-  // Add the elements to the "photograph-header" class
-  const photographerInfo = document.querySelector(".photograph-header");
-  photographerInfo.appendChild(userCardDOM);
-
-  // Update the page title
-  document.title = `Fisheye - ${photographer.name}`;
 }
 
 //------------------------------------------------------------------------
@@ -41,7 +44,7 @@ async function displayData(photographers) {
 async function init() {
   // Get photographer data
   try {
-    const photographers = await getPhotographers();
+    const photographers = await getPhotographerById();
     displayData(photographers);
   } catch (error) {
     console.error("Error fetching photographers data:", error);
