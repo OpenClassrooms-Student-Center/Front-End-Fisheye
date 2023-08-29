6321281c8d3564700edac809f3d263ce1b6257d6
nburd01 <n.burdon@protonmail.com>
2023-08-29 16:05:59 +0200

    functioning without factory

--- scripts/api/api.js
+++ scripts/api/api.js
@@ -1,32 +1,32 @@
-class Api {
-  /**
-   *
-   * @param {string} url
-   */
-  constructor(url) {
-    this._url = url;
-  }
+// class Api {
+//   /**
+//    *
+//    * @param {string} url
+//    */
+//   constructor(url) {
+//     this._url = url;
+//   }
 
-  async get() {
-    return fetch(this._url)
-      .then((res) => res.json())
-      .then((res) => res.data)
-      .catch((err) => console.log("an error occurs", err));
-  }
-}
+//   async get() {
+//     return fetch(this._url)
+//       .then((res) => res.json())
+//       .then((res) => res.data)
+//       .catch((err) => console.log("an error occurs", err));
+//   }
+// }
 
-class PhotographersApi extends Api {
-  /**
-   *
-   * @param {string} url
-   */
-  constructor(url) {
-    super(url);
-  }
+// class PhotographersApi extends Api {
+//   /**
+//    *
+//    * @param {string} url
+//    */
+//   constructor(url) {
+//     super(url);
+//   }
 
-  async getPhotographers() {
-    return await this.get("res.photographers");
-  }
-}
+//   async getPhotographers() {
+//     return await this.get("res.photographers");
+//   }
+// }
 
-export { PhotographersApi };
+// export { PhotographersApi };
