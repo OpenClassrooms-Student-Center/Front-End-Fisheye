a67cb25a025837a350640bef46d7d72d896ccca9
Annabelle Braye <annabelle.braye@gmail.com>
2023-09-06 20:58:06 +0200

    feat(photographer.html): Creation of the photographer's page, get photographer data and media, create photograph_header with factory, update contactForm functions to use classList, add font-awesome icon, create getTotalLikes function.

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -1,8 +1,9 @@
-function photographerTemplate(data) {
-  const { id, name, portrait, city, country, tagline, price } = data;
+function photographerTemplate(photographerData, mediasData) {
+  const { id, name, portrait, city, country, tagline, price } = photographerData;
 
   const picture = `assets/photographers/${portrait}`;
   const url = `photographer.html?id=${id}`;
+  const totalLikes = getTotalLikes(mediasData);
 
   /**
    * Photographer article factory
@@ -14,10 +15,11 @@ function photographerTemplate(data) {
     // Image and title photograph
     const imgTitleLink = document.createElement('a');
     imgTitleLink.href = url;
-    const img = document.createElement( 'img' );
+    const img = document.createElement('img');
+    img.classList.add('profile_picture');
     img.src = picture;
     img.alt = name;
-    const h2 = document.createElement( 'h2' );
+    const h2 = document.createElement('h2');
     h2.textContent = name;
     
     imgTitleLink.appendChild(img);
@@ -39,17 +41,83 @@ function photographerTemplate(data) {
     return (article);
   }
 
-  /**
-   * 
-   */
-  function getPhotographerDescription() {
+  function getPhotographerDetails() {
+    const photographHeader = document.createElement('section');
+    photographHeader.classList.add('photograph_header');
+
+    // Create photographer informations div
+    const photographerInformations = document.createElement('div');
+    console.log(`photographHeader`, photographHeader);
+    const h2 = document.createElement( 'h2' );
+    h2.textContent = name;
+    const h3 = document.createElement('h3');
+    h3.textContent = `${city}, ${country}`;
+    const p = document.createElement('p');
+    p.textContent = tagline;
+
+    photographerInformations.appendChild(h2);
+    photographerInformations.appendChild(h3);
+    photographerInformations.appendChild(p);
+
+    photographHeader.appendChild(photographerInformations);
     
+    // Create contact button
+    const buttonContainer = document.createElement('div');
+    buttonContainer.classList.add('btn_container');
+    const contactMeButton = document.createElement('button');
+    contactMeButton.classList.add('contact_button');
+    contactMeButton.textContent = 'Contactez-moi';
+    contactMeButton.addEventListener('click', displayModal);
+    buttonContainer.appendChild(contactMeButton)
+    photographHeader.appendChild(buttonContainer);
+
+    // Create profile picture
+    const photographImgContainer = document.createElement('div');
+    photographImgContainer.classList.add('profile_picture_container');
+    const photographImg = document.createElement('img');
+    photographImg.classList.add('profile_picture');
+    photographImg.src = picture;
+    photographImg.alt = name;
+
+    photographImgContainer.appendChild(photographImg);
+    photographHeader.appendChild(photographImgContainer);
+    console.log(`photographerHeader`, photographHeader);
+
+    // Create likes sum and price container
+    const likeAndPriceContainer = document.createElement('div');
+    likeAndPriceContainer.classList.add('like_and_sum_container');
+
+    const likesContainer = document.createElement('div');
+    likesContainer.classList.add('likes_container');
+    const likesCount = document.createElement('p');
+    likesCount.textContent = totalLikes;
+    const likesIcon = document.createElement('span');
+    likesIcon.className = 'fa-solid fa-heart';
+
+    const priceInformation = document.createElement('p');
+    priceInformation.textContent = `${price}€ / jour`;
+
+    likesContainer.appendChild(likesCount);
+    likesContainer.appendChild(likesIcon);
+    likeAndPriceContainer.appendChild(likesContainer);
+    likeAndPriceContainer.appendChild(priceInformation);
+    photographHeader.appendChild(likeAndPriceContainer);
+
+    return photographHeader
   }
 
   return {
     name,
     picture,
     getUserCardDOM,
-    getPhotographerDescription
+    getPhotographerDetails
   }
+}
+
+function getTotalLikes(medias) {
+  let likes = 0;
+  medias.forEach(media => {
+    likes += media.likes;
+  });
+  return likes;
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,20 @@
 //Mettre le code JavaScript lié à la page photographer.html
+let photographerData = null;
+
+function displayData(photographer, media) {
+  const mainElt = document.querySelector('main');
+
+  const photographerModel = photographerTemplate(photographer, media);
+  const photographerDetails = photographerModel.getPhotographerDetails();
+  mainElt.appendChild(photographerDetails);
+}
+
+async function init() {
+  const params =  new URL(document.location).searchParams;
+  const id = parseInt(params.get('id'));
+  photographerData = await getPhotographerById(id);
+  displayData(photographerData.photographer, photographerData.media);
+  console.log(`photographerData`,  photographerData);
+}
+
+init();
