3977de2970b7e05cdd5453251262a0db5f559d29
sarahvar <sarahvarin95@gmail.com>
2023-09-07 00:33:31 +0200

    style.css+mise-en-forme

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,32 +1,32 @@
 //Fonction asynchrone avec l'API fetch qui récupère les données des photographes 
 //Asynchronous function with the fetch API which retrieves data from photographers
 async function getPhotographers() {
-  try {
-    const response = await fetch("./data/photographers.json");
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return { photographers: [] };
-  }
+    try {
+        const response = await fetch("./data/photographers.json");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        return { photographers: [] };
+    }
 }
 
 async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
-  //répéte sur chaque photographe dans les données récupérées pour créer une card
-  //repeat on each photographer in the retrieved data to create a card
-  photographers.forEach((photographers) => {
-    const photographerModel = photographerTemplate(photographers);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
+    const photographersSection = document.querySelector(".photographer_section");
+    //répéte sur chaque photographe dans les données récupérées pour créer une card
+    //repeat on each photographer in the retrieved data to create a card
+    photographers.forEach((photographers) => {
+        const photographerModel = photographerTemplate(photographers);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
 }
 
 async function init() {
-  // Récupère les datas des photographes en appelant la fonction getPhotographers 
-  // Get data from photographers by calling the getPhotographers function
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
+    // Récupère les datas des photographes en appelant la fonction getPhotographers 
+    // Get data from photographers by calling the getPhotographers function
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
 }
 
 init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,46 +1,46 @@
 //fonction pour créer et afficher un photographe sur la page d'index
 //function to create and display a phtographer on index page
 function photographerTemplate(data) {
-  const { name, id, city, portrait, country, tagline, price } = data;
+    const { name, id, city, portrait, country, tagline, price } = data;
 
-  const picture = `../../assets/photographers/${portrait}`;
+    const picture = `../../assets/photographers/${portrait}`;
 
-  function getUserCardDOM() {
-    const article = document.createElement("article");
-    const link = document.createElement("a");
+    function getUserCardDOM() {
+        const article = document.createElement("article");
+        const link = document.createElement("a");
 
-    link.href = "./photographer.html?id=" + id;
-    link.ariaLabel = name;
+        link.href = "./photographer.html?id=" + id;
+        link.ariaLabel = name;
 
-    const img = document.createElement("img");
+        const img = document.createElement("img");
 
-    img.setAttribute("src", picture);
-    img.alt = "Photo du profil de " + name;
+        img.setAttribute("src", picture);
+        img.alt = "Photo du profil de " + name;
 
-    const h2 = document.createElement("h2");
+        const h2 = document.createElement("h2");
 
-    h2.textContent = name;
+        h2.textContent = name;
 
-    article.appendChild(link);
-    link.appendChild(img);
-    link.appendChild(h2);
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
 
-    const div = document.createElement("div");
-    const locationInfos = document.createElement("p");
-    const taglineInfos = document.createElement("p");
-    const priceInfos = document.createElement("p");
+        const div = document.createElement("div");
+        const locationInfos = document.createElement("p");
+        const taglineInfos = document.createElement("p");
+        const priceInfos = document.createElement("p");
 
-    locationInfos.textContent = city + ", " + country;
-    taglineInfos.textContent = tagline;
-    priceInfos.textContent = price + "€/jour";
+        locationInfos.textContent = city + ", " + country;
+        taglineInfos.textContent = tagline;
+        priceInfos.textContent = price + "€/jour";
 
-    div.appendChild(locationInfos);
-    div.appendChild(taglineInfos);
-    div.appendChild(priceInfos);
-    article.appendChild(div);
+        div.appendChild(locationInfos);
+        div.appendChild(taglineInfos);
+        div.appendChild(priceInfos);
+        article.appendChild(div);
 
-    return article;
-  }
+        return article;
+    }
 
-  return { getUserCardDOM };
+    return { getUserCardDOM };
 }
