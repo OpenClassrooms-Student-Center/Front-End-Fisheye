e5a05306b79781ecc4e7a355996900338e53269b
sarahvar <sarahvarin95@gmail.com>
2023-09-06 23:24:01 +0200

    photographer.html

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,28 @@
-    async function getPhotographers() {
-        try {
-            const response = await fetch('./data/photographers.json')
-            const data = await response.json()
-            return data
-        } catch (error) {
-            console.error(error)
-            return { photographers: [] }
-        }
-    }
-    
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function getPhotographers() {
+  try {
+    const response = await fetch("./data/photographers.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { photographers: [] };
+  }
+}
 
-        photographers.forEach((photographers) => {
-            const photographerModel = photographerTemplate(photographers);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+  photographers.forEach((photographers) => {
+    const photographerModel = photographerTemplate(photographers);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
