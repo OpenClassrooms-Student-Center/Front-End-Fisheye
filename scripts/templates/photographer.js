6a781f9b3e58e0e0eec508654e0eb6f221c31caa
sarahvar <sarahvarin95@gmail.com>
2023-09-06 22:44:15 +0200

    datas

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,44 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, id, city, portrait, country, tagline, price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+	const picture = `../../assets/photographers/${portrait}`
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+	function getUserCardDOM() {
+		const article = document.createElement('article')
+		const link = document.createElement('a')
+
+		link.href = './photographer.html?id=' + id
+		link.ariaLabel = name
+
+		const img = document.createElement('img')
+
+		img.setAttribute('src', picture)
+		img.alt = 'Photo du profil de ' + name
+
+		const h2 = document.createElement('h2')
+
+		h2.textContent = name
+
+		article.appendChild(link)
+		link.appendChild(img)
+		link.appendChild(h2)
+
+		const div = document.createElement('div')
+		const locationInfos = document.createElement('p')
+		const taglineInfos = document.createElement('p')
+		const priceInfos = document.createElement('p')
+
+		locationInfos.textContent = city + ', ' + country
+		taglineInfos.textContent = tagline
+		priceInfos.textContent = price + '€/jour'
+
+		div.appendChild(locationInfos)
+		div.appendChild(taglineInfos)
+		div.appendChild(priceInfos)
+		article.appendChild(div)
+
+		return article
+	}
+
+	return { getUserCardDOM }
 }
