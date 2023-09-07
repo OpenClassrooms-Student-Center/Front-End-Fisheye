0f94838590b07db89c1cfde2b83ecb58e298bf89
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-09-07 20:58:16 +0200

    Replaced dynamic elements with html template

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,20 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/images/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const elements = `
+            <article>
+                <img src="${picture}" alt="">
+                <h2>${name}</h2>
+                <p>${city}, ${country}</p>
+                <p>${tagline}</p>
+                <small>${price}/jour</small>
+            </article>
+        `;
+
+        return ( elements );
     }
     return { name, picture, getUserCardDOM }
 }
