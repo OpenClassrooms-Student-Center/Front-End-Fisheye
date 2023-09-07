8c858f4d5006680d1915771fc69349ff22084306
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-09-07 20:55:12 +0200

    Fetched json data

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,38 +1,27 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        try {
+            const data = await fetch('../../data/photographers.json')
+                .then(response => {
+                    if(!response.ok) throw new Error('Un problème est survenu lors de la récupération des données');
+
+                    return response.json();
+                });
+            
+               
+                return ({ photographers: [ ...data.photographers ]});
+
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+        const photographersSection = document.querySelector(".photographer-section");
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
+            photographersSection.innerHTML += userCardDOM;
         });
     }
 
