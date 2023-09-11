42dcf430c19701e4f33ef6c9e913f82d8b8112a4
Annabelle Braye <annabelle.braye@gmail.com>
2023-09-11 15:33:12 +0200

    feat(form): Update existing form to add fields, for id and name attributes, manage open and close modal, set photographer name, close modal on submit, background on form with 0.4 opacity, set pointer on buttons lightbox.

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,10 +1,24 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.classList.remove('hidden');
+const contactModal = document.getElementById("contact_modal");
+const form = document.getElementById('contact_form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    closeContactModal();
+});
+let photographerName = '';
+
+const closeModalBtn = document.querySelector('.contact_me_container img');
+closeModalBtn.addEventListener('click', closeContactModal);
+
+function openContactModal(photographerData) {
+    contactModal.classList.remove('hidden');
+    const photographerNameElt = document.querySelector('.modal header > h2');
+    photographerNameElt.textContent = photographerData.name;
+    const formDatas = document.querySelectorAll('#contact_form .form_data input, #contact_form .form_data textarea');
+    formDatas.forEach(field => field.value = '');
 }
 
 function closeContactModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.classList.toggle('hidden');
-
+    contactModal.classList.toggle('hidden');
 }
+
+
