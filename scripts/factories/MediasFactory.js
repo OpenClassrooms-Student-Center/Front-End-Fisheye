f77a59fd7a96210942ed887c0b1457adc19cb6e6
nburd01 <n.burdon@protonmail.com>
2023-09-11 15:16:23 +0200

    work in progress: constructor functions in photographer page. Need to filter and display data

--- scripts/factories/MediasFactory.js
+++ scripts/factories/MediasFactory.js
@@ -4,6 +4,7 @@ import { Image, Video } from "../class/media.js";
 class MediasFactory {
   constructor(data) {
     if (data.image) {
+      console.log("this");
       return new Image(data);
     } else if (data.video) {
       return new Video(data);
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,87 +1,108 @@
 import { MediasApi } from "../api/api.js";
-import { photographerPageTemplate } from "../templates/photographerPage.js";
+// import { photographerPageTemplate } from "../templates/photographerPage.js";
 
-async function getPhotographerById(id) {
-  // Retrieve photographers
-  const photographersResponse = await fetch("data/photographers.json");
-  const photographersData = await photographersResponse.json();
+// async function getPhotographerById(id) {
+//   // Retrieve photographers
+//   const photographersResponse = await fetch("data/photographers.json");
+//   const photographersData = await photographersResponse.json();
 
-  const fetchedPhotographer = photographersData.photographers.find(
-    (photographer) => photographer.id === Number(id)
-  );
+//   const fetchedPhotographer = photographersData.photographers.find(
+//     (photographer) => photographer.id === Number(id)
+//   );
 
-  return fetchedPhotographer;
-}
-
-//------------------------------------------------------------------------
-
-async function displayData() {
-  // Find the photographer with the matching id
-  let id = new URLSearchParams(window.location.search).get("id");
-
-  try {
-    const photographer = await getPhotographerById(id);
-    if (!photographer) {
-      console.log("Photographer not found");
-      return;
-    }
-
-    // Create the photographer's template
-    const photographerModel = photographerPageTemplate(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-
-    // Add the elements to the "photograph-header" class
-    const photographerInfo = document.querySelector(".photograph-header");
-    photographerInfo.appendChild(userCardDOM);
-
-    // Update the page title
-    document.title = `Fisheye - ${photographer.name}`;
-  } catch (error) {
-    console.error("Error fetching photographer data:", error);
-  }
-}
+//   return fetchedPhotographer;
+// }
 
 //------------------------------------------------------------------------
 
+// async function displayData() {
+//   // Find the photographer with the matching id
+//   let id = new URLSearchParams(window.location.search).get("id");
+
+//   try {
+//     const photographer = await getPhotographerById(id);
+//     if (!photographer) {
+//       console.log("Photographer not found");
+//       return;
+//     }
+
+//     // Create the photographer's template
+//     const photographerModel = photographerPageTemplate(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+
+//     // Add the elements to the "photograph-header" class
+//     const photographerInfo = document.querySelector(".photograph-header");
+//     photographerInfo.appendChild(userCardDOM);
+
+//     // Update the page title
+//     document.title = `Fisheye - ${photographer.name}`;
+//   } catch (error) {
+//     console.error("Error fetching photographer data:", error);
+//   }
+// }
+
+//------------------------------------------------------------------------
+
+// class PhotographerPages {
+//   constructor() {
+//     this.mediasApi = new MediasApi("../../data/photographers.json");
+//     console.log("MediasApi instance:", this.mediasApi);
+//   }
+
+//   async medias() {
+//     try {
+//       console.log("MediasApi URL:", this.mediasApi._url); // Assuming _url is the URL property in MediasApi
+//       const photographer = await this.photographer();
+//       const mediasData = await this.media();
+
+//       mediasData.forEach((media) => {
+//         const template = new MediaCard(media, photographer);
+//         this.$mediasWrapper.appendChild(template.createMediaCard());
+//       });
+
+//       // Log the filtered media data
+//       console.log("mediasDataFiltered:", mediasData);
+
+//       // You can also log other data or variables here
+//     } catch (error) {
+//       console.error("Error fetching media data:", error);
+//     }
+//   }
+// }
+
+//------------------------------------------------------------------------
 class PhotographerPages {
   constructor() {
-    this.mediasApi = new MediasApi("assets/images");
-
-    this.media = async () => {
-      const mediasData = await this.mediasApi.getMedias();
-      mediasData.map((media) => new MediasFactory(media));
-      const mediasDataFiltered = mediasData.filter(
-        (photographer) => photographer.photographerId == id
-      );
-
-      console.log("mediasDataFiltered", mediasDataFiltered);
-      return mediasDataFiltered;
-    };
+    this.$mediasWrapper = document.querySelector(".medias-wrapper");
+    this.mediasApi = new MediasApi("/data/photographers.json");
+    // this.mediasApi = new MediasApi("/datar/movie-data.json");
+    // this.moviesApi = new MovieApi("/datar/movie-data.json");
   }
-  async medias() {
-    const photographer = await this.photographer();
-    const mediasData = await this.media();
 
-    mediasData.forEach((media) => {
-      const template = new MediaCard(media, photographer);
-      this.$mediasWrapper.appendChild(template.createMediaCard());
-    });
-    newValue("about-photographer-likes-count", allLikes);
+  async main() {
+    try {
+      const medias = await this.mediasApi.getMedias();
+      console.log("medias", medias);
+    } catch (error) {
+      console.error("An error occurred while fetching media data:", error);
+    }
   }
 }
 
+const app = new PhotographerPages();
+app.main();
 //------------------------------------------------------------------------
 
-async function init() {
-  const photographerPages = new PhotographerPages();
+// async function init() {
+//   const photographerPages = new PhotographerPages();
 
-  // Get photographer data
-  try {
-    const photographers = await getPhotographerById();
-    displayData(photographers);
-  } catch (error) {
-    console.error("Error fetching photographers data:", error);
-  }
-}
+//   // Get photographer data
+//   try {
+//     const photographers = await getPhotographerById();
+//     displayData(photographers);
+//   } catch (error) {
+//     console.error("Error fetching photographers data:", error);
+//   }
+// }
 
-init();
+// init();
