6d9e851bfb82e2f349de8e843ce9e0e71633fd8d
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-09-13 23:00:23 +0200

    Add photographer header

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,38 @@
-//Mettre le code JavaScript lié à la page photographer.html
+const setPhotographerData = async () => {
+    const ID = new URL(document.location.href).searchParams.get('id');
+
+    const data = await getPhotographers();
+
+    const filteredPhotographer = data.photographers.find(photographer => photographer.id == ID);
+    const filteredPhotographerMedia = data.media.filter(media => media.photographerId == ID);
+    
+    return { filteredPhotographer, filteredPhotographerMedia };
+}
+
+
+const setPhotographer = async () => {
+    const { filteredPhotographer, filteredPhotographerMedia } = await setPhotographerData();
+
+    const { id, name, city, country, tagline, price, portrait } = filteredPhotographer;
+
+    const picture = `assets/images/photographers/${portrait}`;
+
+    const elements = `
+        <div class="content">
+            <h1>${name}</h1>
+            <p>${city}, ${country}</p>
+            <p>${tagline}</p>
+            <!-- <small>200€/jour</small> -->
+        </div>
+        <button class="contact_button" onclick="displayModal()">
+            Contactez-moi
+        </button>
+        <img src="${picture}" alt="${name}">
+    `;
+
+    const photographersSection = document.querySelector(".photograph-header");
+
+    photographersSection.innerHTML = elements;
+}
+
+ setPhotographer();
