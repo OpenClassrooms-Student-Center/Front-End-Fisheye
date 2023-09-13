bfb15da9acfde0357b96ab9c0e3fde107e4fe2ec
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-09-13 14:14:43 +0200

    Modified home page cards structure

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,20 +1,26 @@
 function photographerTemplate(data) {
-    const { name, city, country, tagline, price, portrait } = data;
+    const { id, name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/images/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const elements = `
             <article>
-                <img src="${picture}" alt="">
-                <h2>${name}</h2>
-                <p>${city}, ${country}</p>
-                <p>${tagline}</p>
-                <small>${price}/jour</small>
+                <a href="${id}">
+                <div>
+                    <img src="${picture}" alt="${name}">
+                    <h2>${name}</h2>
+                </div>
+                </a>
+                <div>
+                    <p>${city}, ${country}</p>
+                    <p>${tagline}</p>
+                    <small>${price}/jour</small>
+                </div>
             </article>
         `;
 
         return ( elements );
     }
     return { name, picture, getUserCardDOM }
-}
+} 
