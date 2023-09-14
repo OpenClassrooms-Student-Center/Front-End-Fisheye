55c5b0bee336dcd1ce46a86a6d0183760943d839
sarahvar <sarahvarin95@gmail.com>
2023-09-14 13:28:41 +0200

    photographers

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,3 +2,16 @@
 //code de la page d'un seul photographe
 //code of the page of a single photographer
 //avec des classes
+async function getPhotographers() {
+    try {
+        const response = await fetch("../../data/photographers.json");
+        const data = await response.json();
+        return await data;
+    } catch (error) {
+        console.error(error);
+        return { photographers: [] };
+    }
+};
+const data = getPhotographers().then( data => {
+    console.log(data)
+})
