ab8cddde1eebf0b50e01d433a1f47f630b79cec0
sarahvar <sarahvarin95@gmail.com>
2023-09-20 17:44:08 +0200

    insertMedias

--- scripts/factories/onePhotographer.js
+++ scripts/factories/onePhotographer.js
@@ -8,6 +8,11 @@ export class Onephotographer {
     const photographer = data.photographers.find(
       (onePhotographer) => onePhotographer.id === id
     );
+    const allMedias = data.media.filter((dataMediaPhotographer) => dataMediaPhotographer.photographerId === id) 
+
+    for (const element of allMedias) {
+      this.insertMedias(element, photographer)
+    }
     console.log(photographer);
     this.insertHeaderPhotographer(photographer);
   }
@@ -29,5 +34,33 @@ export class Onephotographer {
             </div>
             <img src="${picture}" alt="${name}" aria-label="${name}">
             `;
+  }
+  async insertMedias(media, photographer) {
+    const containerCards = document.getElementById("containerCards")
+    const card = document.createElement("div")
+    let {title, image, id, likes, video} = media
+    const regexName = /^\w+/; // permets de supprimé le nom de famille de name du fichier json
+  const result = await photographer.name.match(regexName)[0]
+  let picture, mediaHtml;
+  if (image?.endsWith(".jpg")) {
+    picture = `assets/images/${result}/${image}`;
+    mediaHtml = `<img src="${picture}" alt="${title}" tabIndex="0" />`;
+  } else if (video?.endsWith(".mp4")) {
+    picture = `assets/images/${result}/${video}`;
+    mediaHtml = `<video src="${picture}" tabIndex="0"></video>`;
+  }
+  card.innerHTML = `
+  <div class="card" id="card" >
+    ${mediaHtml}
+    <div class="containerInfo" >
+      <h2>${title}</h2>
+      <div>
+       <span class="like" id="like-${id}">${likes}</span>
+       <i id="heart-${id}" class="fa-solid fa-heart heart" aria-label="likes" tabIndex="0"></i>
+      </div>
+    </div>
+  </div>
+`;
+  containerCards.append(card);
   }
 }
