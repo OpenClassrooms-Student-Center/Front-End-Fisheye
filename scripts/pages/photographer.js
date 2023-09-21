b2cb463ece5166e1a7855104571d6032c4e52ec4
Mukhtar SULAIMAN <84712917+MukhtarSulaiman@users.noreply.github.com>
2023-09-21 23:31:57 +0200

    Add factory fuction pattern

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,5 @@
-const setPhotographerData = async () => {
+// Gets photograper
+const getPhotographerData = async () => {
     const ID = new URL(document.location.href).searchParams.get('id');
 
     const data = await fetchPhotographers();
@@ -9,15 +10,15 @@ const setPhotographerData = async () => {
     return { filteredPhotographer, filteredPhotographerMedia };
 }
 
-
+// Sets photograper
 const setPhotographer = async () => {
-    const { filteredPhotographer, filteredPhotographerMedia } = await setPhotographerData();
+    const { filteredPhotographer, filteredPhotographerMedia } = await getPhotographerData();
 
     const { id, name, city, country, tagline, price, portrait } = filteredPhotographer;
 
     const picture = `assets/images/photographers/${portrait}`;
 
-    const elements = `
+    const header = `
         <div class="content">
             <h1>${name}</h1>
             <p>${city}, ${country}</p>
@@ -30,9 +31,45 @@ const setPhotographer = async () => {
         <img src="${picture}" alt="${name}">
     `;
 
-    const photographersSection = document.querySelector(".photograph-header");
+    // Creates factory pattern
+    const mediaFactory = (objet) => {  
+       
+        const media = {
+           title: objet.title,
+           mediaUrl: objet.image ? objet.image : objet.video,
+           price: objet.price,
 
-    photographersSection.innerHTML = elements;
+            createMedia: () => {
+                const ExtentionType = media.mediaUrl.split('.')[1];
+                let mediaElement = '';
+
+                mediaElement = ExtentionType === 'mp4' ?
+                   `<video controls><source src="./assets/images/photographersWorks/${name.split(' ')[0]}/${media.mediaUrl}"></source></video>`
+                   : `<img src="./assets/images/photographersWorks/${name.split(' ')[0]}/${media.mediaUrl}" alt="${media.title}">`                
+               
+                return `
+                    <div class="card">
+                        ${ mediaElement }
+                        <div>
+                            <h2>${media.title}</h2>
+                            <div>
+                                <small>${media.price}</small>
+                                <i>♥️</i>
+                            </div>
+                        </div>
+                    </div>`;
+            },
+        }       
+        return media;
+    }
+
+    const photographerHeader = document.querySelector(".photograph-header");
+    const photographerMedia = document.querySelector(".galary-wrapper");
+
+    photographerHeader.innerHTML = header;
+    photographerMedia.innerHTML = filteredPhotographerMedia.map(media => {
+        return mediaFactory(media).createMedia();
+    });
 }
 
  setPhotographer();
