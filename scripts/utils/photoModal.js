7660a93a28a336bfe5e4ec97e49ac774cadbfc9c
nburd01 <n.burdon@protonmail.com>
2023-10-02 15:41:49 +0200

    work in progress: implementing lightbox

---
+++ scripts/utils/photoModal.js
@@ -0,0 +1,42 @@
+// Global DOM var
+const body = document.body;
+const openModalBtn = document.querySelector(".open-modal-btn");
+const mainWrapper = document.querySelector(".main-wrapper");
+const modal = document.querySelector(".photo_modal");
+const modalTitle = document.querySelector(".modal-title");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+
+// Func
+const onOpenModal = () => {
+  mainWrapper.setAttribute("aria-hidden", "true");
+  modal.setAttribute("aria-hidden", "false");
+  body.classList.add("no-scroll");
+  modal.style.display = "flex";
+  modalCloseBtn.focus();
+};
+
+const onCloseModal = () => {
+  mainWrapper.setAttribute("aria-hidden", "false");
+  modal.setAttribute("aria-hidden", "true");
+  body.classList.remove("no-scroll");
+  modal.style.display = "none";
+  openModalBtn.focus();
+};
+
+// Event
+openModalBtn.addEventListener("click", () => {
+  onOpenModal();
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  onCloseModal();
+});
+
+// Close modal when escape key is pressed
+document.addEventListener("keydown", (e) => {
+  const keyCode = e.keyCode ? e.keyCode : e.which;
+
+  if (modal.getAttribute("aria-hidden") === "false" && keyCode === 27) {
+    onCloseModal();
+  }
+});
