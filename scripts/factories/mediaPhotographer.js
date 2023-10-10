ab9a6ad0c2499ec8ec6e7a74873d5c233995b236
sarahvar <sarahvarin95@gmail.com>
2023-10-10 23:41:38 +0200

    media.js-fonctionnel

--- scripts/factories/mediaPhotographer.js
+++ scripts/factories/mediaPhotographer.js
@@ -1,4 +1,3 @@
-import { mediaFactory } from "../factories/medias.js"
 export class Mediaphotographer {
   constructor() {
     this.totalLikes = 0 ; // Variable pour suivre le total des likes
@@ -16,7 +15,7 @@ export class Mediaphotographer {
       (dataMediaPhotographer) => dataMediaPhotographer.photographerId === id
     );
     for (const media of allMedias) {
-      mediaFactory(media, photographer);
+      this.insertMedias(media, photographer);
       this.totalLikes += media.likes
     }
      // Mettez à jour l'élément HTML avec le total des likes
@@ -34,7 +33,7 @@ export class Mediaphotographer {
         section.innerHTML = "";
         filteredMedia = [...allMedias].sort((a, b) => b.likes - a.likes);
         filteredMedia.forEach((element) => {
-          this.mediaFactory(element, photographer);
+          this.insertMedias(element, photographer);
         });
       } else if (selectedValue === "date") {
         section.innerHTML = "";
@@ -42,7 +41,7 @@ export class Mediaphotographer {
           (a, b) => new Date(a.date) - new Date(b.date)
         );
         filteredMedia.forEach((element) => {
-          this.mediaFactory(element, photographer);
+          this.insertMedias(element, photographer);
         });
       } else if (selectedValue === "title") {
         section.innerHTML = "";
@@ -56,7 +55,7 @@ export class Mediaphotographer {
           return 0;
         });
         filteredMedia.forEach((element) => {
-          this.mediaFactory(element, photographer);
+          this.insertMedias(element, photographer);
         });
       }
     });
@@ -83,52 +82,51 @@ export class Mediaphotographer {
             <img src="${picture}" alt="${name}" aria-label="${name}">
             `;
   }
-//   async insertMedias(media, photographer) {
-//     const containerCards = document.getElementById("containerCards");
-//     const card = document.createElement("div");
-//     let { title, image, id, likes, video } = media;
-//     const regexName = /^\w+/; // permets de supprimé le nom de famille de name du fichier json
-//     const result = await photographer.name.match(regexName)[0];
-//     let picture, mediaHtml;
-//     if (image?.endsWith(".jpg")) {
-//       picture = `assets/images/${result}/${image}`;
-//       picture = `assets/images/${result}/${image}`;
-//       mediaHtml = `<img lightbox-media=${title} src="${picture}" alt="${title}" tabIndex="0" />`;
-//     } else if (video?.endsWith(".mp4")) {
-//       picture = `assets/images/${result}/${video}`;
-//       mediaHtml = `<video lightbox-media=${title} src="${picture}" tabIndex="0"></video>`;
-//     }
-//     card.innerHTML = `
-//   <div class="card" id="card" >
-//     ${mediaHtml}
-//     <div class="containerInfos" >
-//       <h2>${title}</h2>
-//       <div class="containerLikes_i">
-//        <span class="totalLikes" id="like-${id}">${likes}</span>
-//        <i id="heart-${id}" class="fa-solid fa-heart heart" aria-label="likes" tabIndex="0"></i>
-//       </div>
-//     </div>
-//   </div>
-// `;
-//     containerCards.append(card);
-//     const heartId = document.getElementById(`heart-${id}`);
-//     const likeClass = document.getElementById(`like-${id}`);
+   insertMedias(media, photographer) {
+    const containerCards = document.getElementById("containerCards");
+    const card = document.createElement("div");
+    let { title, image, id, likes, video } = media;
+    const regexName = /^\w+/; // permets de supprimé le nom de famille de name du fichier json
+    const result =  photographer.name.match(regexName)[0];
+    let picture, mediaHtml;
+    if (image?.endsWith(".jpg")) {
+      picture = `assets/images/${result}/${image}`;
+      mediaHtml = `<img lightbox-media=${title} src="${picture}" alt="${title}" tabIndex="0" />`;
+    } else if (video?.endsWith(".mp4")) {
+      picture = `assets/images/${result}/${video}`;
+      mediaHtml = `<video lightbox-media=${title} src="${picture}" tabIndex="0"></video>`;
+    }
+    card.innerHTML = `
+  <div class="card" id="card" >
+    ${mediaHtml}
+    <div class="containerInfos" >
+      <h2>${title}</h2>
+      <div class="containerLikes_i">
+       <span class="totalLikes" id="like-${id}">${likes}</span>
+       <i id="heart-${id}" class="fa-solid fa-heart heart" aria-label="likes" tabIndex="0"></i>
+      </div>
+    </div>
+  </div>
+`;
+    containerCards.append(card);
+    const heartId = document.getElementById(`heart-${id}`);
+    const likeClass = document.getElementById(`like-${id}`);
 
-//     heartId.addEventListener("click", () => {
-//      const totalLikesElement = document.getElementById("total-likes");
-//       if (likeClass.classList.contains("likes")) {
-//         // Si l'élément a déjà été "aimé", supprimez le like
-//         likeClass.classList.remove("likes");
-//         likes -= 1;
-//         totalLikesElement.innerText = this.totalLikes -=1
-//       } else {
-//         // Sinon, ajoutez un like
-//         likeClass.classList.add("likes");
-//         likes += 1;
-//         totalLikesElement.innerText = this.totalLikes +=1
-//       }
-//       // Mettez à jour le texte de l'élément HTML avec le total des likes
-//       likeClass.innerText = likes;
-//     });
-//   }
+    heartId.addEventListener("click", () => {
+     const totalLikesElement = document.getElementById("total-likes");
+      if (likeClass.classList.contains("likes")) {
+        // Si l'élément a déjà été "aimé", supprimez le like
+        likeClass.classList.remove("likes");
+        likes -= 1;
+        totalLikesElement.innerText = this.totalLikes -=1
+      } else {
+        // Sinon, ajoutez un like
+        likeClass.classList.add("likes");
+        likes += 1;
+        totalLikesElement.innerText = this.totalLikes +=1
+      }
+      // Mettez à jour le texte de l'élément HTML avec le total des likes
+      likeClass.innerText = likes;
+    });
+  }
 }
--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -1,52 +1,55 @@
-export function mediaFactory({
-    title, image, id, likes, video, name, portrait
-}){
-    const picture = `../assets/photographers/${portrait}`;
-    //const getMediasCardDOM = ()=>{
-      //  const card = document.createElement("article")
-        //const regexName = /^\w+/; // permets de supprimé le nom de famille de name du fichier json
-    
-//     let picture, mediaHtml;
-//     if (image?.endsWith(".jpg")) {
-//       picture = `assets/images/${name}/${image}`;
-//       mediaHtml = `<img lightbox-media=${title} src="${picture}" alt="${title}" tabIndex="0" />`;
-//     } else if (video?.endsWith(".mp4")) {
-//       picture = `assets/images/${name}/${video}`;
-//       mediaHtml = `<video lightbox-media=${title} src="${picture}" tabIndex="0"></video>`;
-//     }
-//         card.innerHTML = `
-//         <div class="card" id="card" >
-//           ${mediaHtml}
-//           <div class="containerInfos" >
-//             <h2>${title}</h2>
-//             <div class="containerLikes_i">
-//              <span class="totalLikes" id="like-${id}">${likes}</span>
-//              <i id="heart-${id}" class="fa-solid fa-heart heart" aria-label="likes" tabIndex="0"></i>
-//             </div>
-//           </div>
-//         </div>
-//       `;
-//       containerCards.append(card);
-//       const heartId = document.getElementById(`heart-${id}`);
-//       const likeClass = document.getElementById(`like-${id}`);
-  
-//       heartId.addEventListener("click", () => {
-//        const totalLikesElement = document.getElementById("total-likes");
-//         if (likeClass.classList.contains("likes")) {
-//           // Si l'élément a déjà été "aimé", supprimez le like
-//           likeClass.classList.remove("likes");
-//           likes -= 1;
-//           totalLikesElement.innerText = this.totalLikes -=1
-//         } else {
-//           // Sinon, ajoutez un like
-//           likeClass.classList.add("likes");
-//           likes += 1;
-//           totalLikesElement.innerText = this.totalLikes +=1
-//         }
-//         // Mettez à jour le texte de l'élément HTML avec le total des likes
-//         likeClass.innerText = likes;
-//       });
-//       return card 
-     //}
-   //return {name, picture, getMediasCardDOM}
-       }
+export class MediaFactory {
+  constructor(portrait) {
+    this.picture = `../assets/photographers/${portrait}`;
+  }
+
+  getMediasCardDOM(name, image, video, title, id, likes) {
+    const card = document.createElement("article");
+
+    let picture, mediaHtml;
+
+    if (image?.endsWith(".jpg")) {
+      picture = `assets/images/${name}/${image}`;
+      mediaHtml = `<img lightbox-media="${title}" src="${picture}" alt="${title}" tabIndex="0" />`;
+    } else if (video?.endsWith(".mp4")) {
+      picture = `assets/images/${name}/${video}`;
+      mediaHtml = `<video lightbox-media="${title}" src="${picture}" tabIndex="0"></video>`;
+    }
+
+    card.innerHTML = `
+      <div class="card" id="card">
+        ${mediaHtml}
+        <div class="containerInfos">
+          <h2>${title}</h2>
+          <div class="containerLikes_i">
+            <span class="totalLikes" id="like-${id}">${likes}</span>
+            <i id="heart-${id}" class="fa-solid fa-heart heart" aria-label="likes" tabIndex="0"></i>
+          </div>
+        </div>
+      </div>
+    `;
+
+    const containerCards = document.getElementById("containerCards");
+    containerCards.append(card);
+
+    const heartId = document.getElementById(`heart-${id}`);
+    const likeClass = document.getElementById(`like-${id}`);
+
+    heartId.addEventListener("click", () => {
+      if (likeClass.classList.contains("likes")) {
+        // If the element has already been liked, remove the like
+        likeClass.classList.remove("likes");
+        likes -= 1;
+      } else {
+        // Otherwise, add a like
+        likeClass.classList.add("likes");
+        likes += 1;
+      }
+
+      // Update the text of the element with the total likes
+      likeClass.innerText = likes;
+    });
+
+    return card;
+  }
+}
