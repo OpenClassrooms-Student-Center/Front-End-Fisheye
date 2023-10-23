c30a39a1fa6899d9e6f7d745cf040cc50675603b
nburd01 <n.burdon@protonmail.com>
2023-10-23 15:06:47 +0200

    refacto modal : renamed id

--- scripts/templates/lightbox.js
+++ scripts/templates/lightbox.js
@@ -4,30 +4,30 @@ class Lightbox {
     this._photographer = photographer;
   }
 
-  createLightbox() {
+  createLightbox(media, photographer) {
     const modalContent = document.querySelector("#modal-content");
     modalContent.classList.add("mx-auto", "my-0", "mySlides");
 
     // Create new elements for the media title and media
     const mediaTitleElement = document.createElement("h2");
-    mediaTitleElement.innerHTML = this._media.title;
+    mediaTitleElement.innerHTML = media.title;
 
-    if (this._media.image) {
+    if (media.image) {
       // If it's an image, create an <img> element
       const mediaImageElement = document.createElement("img");
       mediaImageElement.setAttribute("id", "modal-picture");
-      const imagePath = `assets/images/${this._photographer.name}/${this._media.image}`;
+      const imagePath = `assets/images/${photographer.name}/${media.image}`;
 
       mediaImageElement.src = imagePath;
-      mediaImageElement.alt = this._photographer.name;
+      mediaImageElement.alt = photographer.name;
 
       // Append the new elements to modalContent
       modalContent.appendChild(mediaImageElement);
-    } else if (this._media.video) {
+    } else if (media.video) {
       // If it's a video, create a <video> element
       const mediaVideoElement = document.createElement("video");
       mediaVideoElement.setAttribute("id", "modal-video");
-      const videoPath = `assets/images/${this._photographer.name}/${this._media.video}`;
+      const videoPath = `assets/images/${photographer.name}/${media.video}`;
 
       mediaVideoElement.src = videoPath;
       mediaVideoElement.controls = true;
@@ -40,7 +40,7 @@ class Lightbox {
   }
 
   goToNextImage() {
-    this._currentIndex = (this._currentIndex + 1) % this._mediaArray.length;
+    currentIndex = (currentIndex + 1) % mediaArray.length;
     this.createLightbox();
   }
 }
