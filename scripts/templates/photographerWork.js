2d038535ed1fc9bececdd8266a389cabacfa58ba
nburd01 <n.burdon@protonmail.com>
2023-10-23 11:17:52 +0200

    refacto modal : showing video and img proprely

--- scripts/templates/photographerWork.js
+++ scripts/templates/photographerWork.js
@@ -23,28 +23,6 @@ class PhotographerWork {
       const mediaImg = document.createElement("a");
       mediaImg.classList.add(`media-img-${media.id}`);
 
-      //append to link
-      const imageElement = document.createElement("img");
-      if (media.image) {
-        mediaTypeElement.innerHTML = "Type: Image";
-
-        const imagePath = `assets/images/${photographer.name}/${media.image}`;
-        imageElement.src = imagePath;
-        imageElement.alt = `${photographer.name}`;
-      } else if (media.video) {
-        mediaTypeElement.innerHTML = "Type: Video";
-        // Construct the path to the video using the correct folder structure
-        const videoPath = `assets/images/${photographer.name}/${media.video}`;
-
-        // Create a <video> element for displaying the video
-        const videoElement = document.createElement("video");
-        videoElement.src = videoPath;
-        videoElement.controls = true;
-
-        // Append the video element to the mediaImg container
-        mediaImg.appendChild(videoElement);
-      }
-
       const mediaDetails = document.createElement("div");
       mediaDetails.classList.add("media-details");
 
@@ -68,7 +46,33 @@ class PhotographerWork {
       const mediaLikeHeartElement = document.createElement("i");
       mediaLikeHeartElement.classList.add("fa-solid", "fa-heart");
 
-      //   Construct the path to the image using the correct folder structure
+      if (media.image) {
+        mediaTypeElement.innerHTML = "Type: Image";
+        // Construct the path to the image using the correct folder structure
+        const imagePath = `assets/images/${photographer.name}/${media.image}`;
+
+        // Create an <img> element for displaying the image
+        const imageElement = document.createElement("img");
+        imageElement.src = imagePath;
+        imageElement.alt = media.title;
+        imageElement.setAttribute("id", `media-img-${media.id}`);
+        imageElement.onclick = onOpenPic;
+
+        // Append the image element to the mediaImg container
+        mediaImg.appendChild(imageElement);
+      } else if (media.video) {
+        mediaTypeElement.innerHTML = "Type: Video";
+        // Construct the path to the video using the correct folder structure
+        const videoPath = `assets/images/${photographer.name}/${media.video}`;
+
+        // Create a <video> element for displaying the video
+        const videoElement = document.createElement("video");
+        videoElement.src = videoPath;
+        videoElement.controls = true;
+
+        // Append the video element to the mediaImg container
+        mediaImg.appendChild(videoElement);
+      }
 
       // mediaDetails.appendChild(mediaIdElement);
       mediaDetails.appendChild(mediaTitleElement);
@@ -77,7 +81,6 @@ class PhotographerWork {
 
       // Add the media details container to the media container
       mediaContainer.appendChild(mediaImg);
-      mediaImg.appendChild(imageElement);
       mediaContainer.appendChild(mediaDetails);
       mediaContainer.appendChild(mediaDetails);
       mediaDetails.appendChild(mediaLikeContainer);
