8788ca2171ba2cbc7397424fc957f4d06c00e786
cindon <nicolascindon@gmail.com>
2023-10-25 16:58:36 +0200

    fetch phptographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,27 +1,9 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
+        let photographers = await fetch("data/photographers.json")
+            .then(response => response.json())
+            .then(data => data.photographers);
+        
+
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
     }
@@ -37,7 +19,6 @@
     }
 
     async function init() {
-        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
