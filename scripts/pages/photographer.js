fd1b61c63b974d431979fb85f770e860a543acd6
Samanta DA SILVA <developpement.sam@gmail.com>
2023-10-26 19:24:23 +0200

    sorting working correctly

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -12,6 +12,8 @@ export async function getDataPhotographers() {
   return dataPhotographers.json();
 }
 
+/////////////////////HEADER
+
 function displayInfo(photographer) {
   const photographerInfo = document.querySelector(".photograph-info");
   const photographerPictureSection = document.querySelector(
@@ -27,17 +29,95 @@ function displayInfo(photographer) {
   photographerPictureSection.appendChild(userPicture);
 }
 
-function displayMedias(photographer, medias) {
-  const sectionMedia = document.querySelector(".media");
+//////////////SORTED LIST
 
-  medias
-    .filter((media) => media.photographerId == photographer.id)
-    .forEach((media) => {
-      const mediaModel = mediasTemplate(photographer, media);
-      sectionMedia.appendChild(mediaModel);
-    });
-  return sectionMedia;
+////DOM ELEMENTS
+const sortButton = document.getElementById("sort-button");
+const sortOptionContainer = document.getElementById("sort-option-container");
+const sortOptions = document.querySelectorAll(".sort-option");
+const arrowDiv = document.getElementById("sort-arrow");
+
+let isMenuOpen = false;
+
+////DISPLYING MENU LIST
+
+sortButton.addEventListener("click", menuToggle);
+
+function menuToggle() {
+  if (isMenuOpen) {
+    sortOptionContainer.style.display = "none";
+    sortButton.setAttribute("aria-expanded", "false");
+    sortButton.style.padding = "20px 15px";
+    arrowDiv.classList.add("arrow-down");
+    arrowDiv.classList.remove("arrow-up");
+    isMenuOpen = false;
+  } else {
+    sortOptionContainer.style.display = "block";
+    sortButton.style.padding = "20px 15px 0";
+    sortButton.setAttribute("aria-expanded", "true");
+    arrowDiv.classList.add("arrow-up");
+    arrowDiv.classList.remove("arrow-down");
+
+    isMenuOpen = true;
+  }
 }
+
+sortOptions.forEach((option) => {
+  option.addEventListener("click", function () {
+    sortButton.querySelector("span").textContent = this.textContent;
+
+    menuToggle();
+
+    sortOptions.forEach((item) => {
+      item.classList.toggle("display-none", item === this);
+    });
+
+    sortButton.setAttribute("aria-expanded", "false");
+  });
+});
+
+/////////////////MEDIAS
+
+function displayMediaUpdate(sortedMedia, targetElement) {
+  // Clear the current content
+  targetElement.innerHTML = "";
+
+  // Loop through the sorted media and append them to the target element
+  sortedMedia.forEach((mediaItem) => {
+    targetElement.appendChild(mediaItem);
+  });
+}
+
+function sortedByPopularityMedia(photographer, allMedia) {
+  return allMedia
+    .filter((media) => media.photographerId === photographer.id)
+    .sort((a, b) => b.likes - a.likes)
+    .map((media) => {
+      const mediaModel = mediasTemplate(photographer, media);
+      return mediaModel;
+    });
+}
+
+function sortedByDateMedia(photographer, allMedia) {
+  return allMedia
+    .filter((media) => media.photographerId == photographer.id)
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .map((media) => {
+      const mediaModel = mediasTemplate(photographer, media);
+      return mediaModel;
+    });
+}
+
+function sortedByTitleMedia(photographer, allMedia) {
+  return allMedia
+    .filter((media) => media.photographerId == photographer.id)
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .map((media) => {
+      const mediaModel = mediasTemplate(photographer, media);
+      return mediaModel;
+    });
+}
+
 //Function init
 
 async function init() {
@@ -60,7 +140,42 @@ async function init() {
   }
 
   displayInfo(photographer);
-  displayMedias(photographer, media);
+
+  //MEDIAS
+  const sectionMedia = document.querySelector(".media");
+
+  ///Sorted medias
+
+  const popularOption = document.querySelector(
+    ".sort-option[data-value='popularity']"
+  );
+  const dateOption = document.querySelector(".sort-option[data-value='date']");
+  const titleOption = document.querySelector(
+    ".sort-option[data-value='title']"
+  );
+
+  const sortedByPopularity = sortedByPopularityMedia(photographer, media);
+
+  //by default
+  displayMediaUpdate(sortedByPopularity, sectionMedia);
+
+  //by popularity (the most liked media first)
+  popularOption.addEventListener("click", () => {
+    displayMediaUpdate(sortedByPopularity, sectionMedia);
+  });
+
+  //by date (the most recent first)
+  const sortedByDate = sortedByDateMedia(photographer, media);
+
+  dateOption.addEventListener("click", () => {
+    displayMediaUpdate(sortedByDate, sectionMedia);
+  });
+
+  //by title (alphabetical order)
+  titleOption.addEventListener("click", () => {
+    const sortedByTitle = sortedByTitleMedia(photographer, media);
+    displayMediaUpdate(sortedByTitle, sectionMedia);
+  });
 }
 
 init();
