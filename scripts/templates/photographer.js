9265409bc74cefac3a31e773d61cf0847903a0e1
Samanta DA SILVA <developpement.sam@gmail.com>
2023-10-27 19:03:06 +0200

    simple animation for the likes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -127,8 +127,6 @@ export function mediasTemplate(photographer, media) {
   const mediaFactory = new Media(media, photographer);
   const mediaType = mediaFactory.getMediaCardDom();
 
-  const likeIconMedia = "../../assets/icons/like-icon.png";
-
   const divDescription = document.createElement("div");
   divDescription.classList.add("media-description");
 
@@ -143,17 +141,29 @@ export function mediasTemplate(photographer, media) {
   likesElement.innerText = likes;
   likesElement.classList.add("likes");
 
-  const likesIcon = document.createElement("img");
-  likesIcon.setAttribute("src", likeIconMedia);
-  likesIcon.setAttribute("alt", "j'aimes");
-  likesIcon.classList.add("likes-icon");
+  //////////SVG LIKE ICON - like that we can change the fill opacity in CSS
+
+  const likeIconSvgString = `
+<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+  <path d="M8.59173 16.6835L8.59106 16.6828C6.32899 14.3385 4.48837 12.4301 3.20738 10.6403C1.93332 8.86007 1.25 7.24634 1.25 5.5C1.25 2.63134 3.20427 0.5 5.5625 0.5C6.90697 0.5 8.22698 1.21835 9.09906 2.38875L9.5 2.92683L9.90094 2.38875C10.773 1.21835 12.093 0.5 13.4375 0.5C15.7957 0.5 17.75 2.63134 17.75 5.5C17.75 7.24634 17.0667 8.8601 15.7925 10.6416C14.5135 12.43 12.6766 14.3378 10.4193 16.6821L10.4096 16.6921L10.4086 16.6932L9.50137 17.6299L8.59173 16.6835Z" fill="#901C1C" fill-opacity="0" stroke="#901C1C"/>
+</svg>
+`;
+
+  const parser = new DOMParser();
+  const likesIconParsed = parser.parseFromString(
+    likeIconSvgString,
+    "image/svg+xml"
+  );
+  const svgElement = likesIconParsed.documentElement;
+  svgElement.setAttribute("alt", "j'aimes");
+  svgElement.classList.add("likes-icon");
 
   article.appendChild(mediaType);
   article.appendChild(divDescription);
   divDescription.appendChild(titleElement);
   divDescription.appendChild(divLikes);
   divLikes.appendChild(likesElement);
-  divLikes.appendChild(likesIcon);
+  divLikes.appendChild(svgElement);
 
   return article;
 }
