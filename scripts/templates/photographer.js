adce475d6635c8a593e08f63163a714e7d603db1
A-Melouk <abdoumelouk96@gmail.com>
2023-10-31 17:37:09 +0100

    Récuperer et afficher les ingos des photographes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,40 @@
 function photographerTemplate(data) {
-  const { name, portrait } = data
+  const { name, portrait, city, country, tagline, price } = data
 
   const picture = `assets/photographers/${portrait}`
 
   function getUserCardDOM() {
     const article = document.createElement('article')
-    const img = document.createElement('img')
-    img.setAttribute('src', picture)
-    const h2 = document.createElement('h2')
-    h2.textContent = name
-    article.appendChild(img)
-    article.appendChild(h2)
+
+    const imgContainer = document.createElement('div')
+    imgContainer.classList.add('img_container')
+
+    const profilePicture = document.createElement('img')
+    profilePicture.setAttribute('src', picture)
+
+    const nameHeadline = document.createElement('h2')
+    nameHeadline.textContent = name
+
+    const locationP = document.createElement('p')
+    locationP.textContent = `${city}, ${country}`
+    locationP.classList.add('location')
+
+    const tagP = document.createElement('p')
+    tagP.textContent = tagline
+    tagP.classList.add('tag')
+
+    const priceP = document.createElement('p')
+    priceP.textContent = price + '€/jour'
+    priceP.classList.add('price')
+
+    imgContainer.appendChild(profilePicture)
+    article.appendChild(imgContainer)
+    // article.appendChild(profilePicture)
+    article.appendChild(nameHeadline)
+    article.appendChild(locationP)
+    article.appendChild(tagP)
+    article.appendChild(priceP)
+
     return article
   }
   return { name, picture, getUserCardDOM }
