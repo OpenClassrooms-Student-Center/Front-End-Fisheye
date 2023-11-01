4b8a27e61002fcff735bcb1c6d5d5bc2872f1ae6
hichamecode <hichame.code@gmail.com>
2023-11-01 13:40:23 +0100

    création de la page photographes en cours + modale complète

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,55 @@
-    async function getData() { 
+async function getData() { 
 
-        const response = await fetch ("/data/photographers.json") 
-        const photographers = await response.json()
-        return photographers
-    }
-      
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    const response = await fetch ("/data/photographers.json") 
+    return await response.json()
+}
+  
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
-            const {name, id, city, country, tagline, price, portrait} = photographer
-            const userCard = new UserCard(name, id, city, country, tagline, price, portrait)
-            const userCardDOM = userCard.getUserCardDOM()
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    photographers.forEach((photographer) => {
 
-    async function init() {
-        // Récupère les datas des photographes
+        const userCardDOM = new UserCardFactory().createFromPhotographer(photographer)
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
         const { photographers } = await getData(); // déstructuration avec les accolades, pour récup uniquement photographers
         displayData(photographers);
- 
+     }
+
+ init();
+
+class UserCardFactory {
+
+    createFromPhotographer(photographerData) {
+
+        const {name, id, city, country, tagline, price, portrait} = photographerData
+        return new UserCard(name, id, city, country, tagline, price, portrait).create()
     }
+}
+
+
+/*
+class MediaFactory {
+
+constructor(data, type) {
+    if (type === 'photo') {
+        return new MediaPhoto(data)
+    } else if (type === 'video') {
+        return new MediaVideo(data)
+    } else {
+        throw 'Unknown type format'
+    }
+}
+au lieu d'appeler la classe directement, j'appelle MediaFactory */
+
+/* class media globale path ou source et  2 sous classes mediavideo avec attribut video et mediaimage 
+    attribut src qui change en fonction de l'attribut image ou video 
+    pour le dom, factory avec 2 methodes preview : soit image , soit video 
+
     
-     init();
-    
+*/
 
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -2,19 +2,30 @@ class UserCard {
     constructor(name, id, city, country, tagline, price, portrait) {
 
         this.name = name
-        this.id = id
+        this._id = id
         this.city = city
         this.country = country
         this.tagline = tagline
         this.price = price
         this.portrait = portrait
     }
+
+    get id() {
+        return this._id
+    }
  
+    createLink() {
+        const linkTo = document.createElement('a')
+        linkTo.setAttribute('href', 'photographer.html?id='+ this._id) // récuperer l'id dans l'url pour le searchParams
+        linkTo.classList.add('link_to')
+        return linkTo
+    }
+
     createImageElement() {
         const portraitWrapper = document.createElement('div')
         portraitWrapper.classList.add('portrait_wrapper')
         portraitWrapper.setAttribute('role', 'presentation')
-        portraitWrapper.setAttribute('tabindex', '0')
+        // portraitWrapper.setAttribute('tabindex', '0')
         portraitWrapper.setAttribute('id', this.id)
         
         const img = document.createElement('img');
@@ -35,6 +46,7 @@ class UserCard {
     createCityCountryElement() {
         const span = document.createElement('span')
         span.classList.add('city_country')
+        span.setAttribute('role', 'label')
         span.textContent = this.city +', '+ this.country
         return span
     }
@@ -42,6 +54,7 @@ class UserCard {
     createTagLineElement() {
         const p = document.createElement('p')
         p.classList.add('tagline')
+        p.setAttribute('role', 'label')
         p.textContent = this.tagline
         return p
     }
@@ -49,11 +62,12 @@ class UserCard {
     createPriceElement() {
         const p2 = document.createElement('p')
         p2.classList.add('price')
+        p2.setAttribute('role', 'label')
         p2.textContent = this.price+'€/jour'
         return p2
     }
 
-    getUserCardDOM() {
+    create() {
         const article = document.createElement('article');
         const section = document.createElement('section');
         section.classList.add('section_under_name');
@@ -63,15 +77,17 @@ class UserCard {
         const span = this.createCityCountryElement();
         const p = this.createTagLineElement();
         const p2 = this.createPriceElement();
+        const linkTo = this.createLink();
 
-        article.appendChild(portraitWrapper);
+        article.appendChild(linkTo)
+        linkTo.appendChild(portraitWrapper);
         article.appendChild(h2);
         article.appendChild(section);
         section.appendChild(span);
         section.appendChild(p);
         section.appendChild(p2);
-
         return article;
     }
 
 }
+
