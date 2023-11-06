c0b74ea9369a8e3add07c4ece90c7cbf74bfa74e
A-Melouk <abdoumelouk96@gmail.com>
2023-11-06 09:15:21 +0100

    Utiliser Object literals et innerHTML pour générer

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -5,35 +5,14 @@ function photographerTemplate(data) {
 
   function getUserCardDOM() {
     const article = document.createElement('article')
-
-    const imgContainer = document.createElement('div')
-    imgContainer.classList.add('img_container')
-
-    const profilePicture = document.createElement('img')
-    profilePicture.setAttribute('src', picture)
-
-    const nameHeadline = document.createElement('h2')
-    nameHeadline.textContent = name
-
-    const locationP = document.createElement('p')
-    locationP.textContent = `${city}, ${country}`
-    locationP.classList.add('location')
-
-    const tagP = document.createElement('p')
-    tagP.textContent = tagline
-    tagP.classList.add('tag')
-
-    const priceP = document.createElement('p')
-    priceP.textContent = price + '€/jour'
-    priceP.classList.add('price')
-
-    imgContainer.appendChild(profilePicture)
-    article.appendChild(imgContainer)
-    // article.appendChild(profilePicture)
-    article.appendChild(nameHeadline)
-    article.appendChild(locationP)
-    article.appendChild(tagP)
-    article.appendChild(priceP)
+    article.innerHTML = `
+      <div class="img-container">
+        <img src="${picture}" alt="${name}">
+      </div>
+      <h2>${name}</h2>
+      <p class="location">${city}, ${country}</p>
+      <p class="tag">${tagline}</p>
+      <p class="price">${price}€/jour`
 
     return article
   }
