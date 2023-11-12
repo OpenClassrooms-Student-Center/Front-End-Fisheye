4a35c3fb79815bba2aad8fd0e271e5da2c0b9518
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2023-11-12 21:40:32 +0100

    feat: modification de photographer.js

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait, id } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/portrait/${portrait}`;
     const PhotographerLink = `photographer.html?id=${id}`;
@@ -9,17 +9,28 @@ function photographerTemplate(data) {
         const link = document.createElement('a');
         const img = document.createElement( 'img' );
         const h2 = document.createElement( 'h2' );
+        const location = document.createElement('p');
+        const tagLine = document.createElement( 'p' );
+        const PhotographerPrice = document.createElement( 'p' );
 
         link.setAttribute("href", PhotographerLink);      
-        img.setAttribute("src", picture);        
+        img.setAttribute("src", picture);
+
         h2.textContent = name;
+        location.textContent = city + ", " + country;
+        tagLine.textContent = tagline;        
+        PhotographerPrice.textContent =  price + "€/jour";;      
 
         article.appendChild(link);
         link.appendChild(img);
         link.appendChild(h2);
+        link.appendChild(location);
+        link.appendChild(tagLine);
+        link.appendChild(PhotographerPrice);
+
         return (article);
     }
 
-    return { name, picture, getUserCardDOM }
+    return { getUserCardDOM }
 
 }
