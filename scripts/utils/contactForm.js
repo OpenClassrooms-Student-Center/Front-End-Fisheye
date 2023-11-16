3c94a2bea7943d46139e5e7349c2f61b7d2b9142
A-Melouk <abdoumelouk96@gmail.com>
2023-11-16 12:21:37 +0100

    Afficher le formulaire onClick Corriger l'affichage du header

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,15 @@
 function displayModal() {
-  const modal = document.getElementById('contact_modal')
+  const queryString = window.location.search
+  const urlParams = new URLSearchParams(queryString)
+  const id = urlParams.get('id')
+  const name = JSON.parse(sessionStorage.getItem(id)).name
+  const contactMe = document.querySelector('.contact-me')
+  contactMe.innerHTML = `<h2 class="contact-me">Contactez-moi <br>${name}</h2>`
+  const modal = document.getElementById('contact-modal')
   modal.style.display = 'block'
 }
 
 function closeModal() {
-  const modal = document.getElementById('contact_modal')
+  const modal = document.getElementById('contact-modal')
   modal.style.display = 'none'
 }
