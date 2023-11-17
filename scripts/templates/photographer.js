05bd01838c77de37bd28637225a39bd57538ba5a
SafaSELLAM <safa.sellam@outlook.fr>
2023-11-17 11:41:47 +0100

    NEW - add fetch and css to get photographers pictures

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,29 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, id, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const cityName = document.createElement("h3");
+    cityName.textContent = city + ", " + country;
+    const taglineText = document.createElement("p");
+    taglineText.classList.add("taglineText");
+    taglineText.textContent = tagline;
+    const priceTag = document.createElement("p");
+    priceTag.classList.add("priceTag");
+    priceTag.textContent = price + "€/jours";
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(cityName);
+    article.appendChild(taglineText);
+    article.appendChild(priceTag);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
