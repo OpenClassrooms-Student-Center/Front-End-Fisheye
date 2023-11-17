a8a87103f674be498df5d8f652e3a417966dc5d0
A-Melouk <abdoumelouk96@gmail.com>
2023-11-17 17:26:43 +0100

    Ajouter la modale + la rendre accessible

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -4,12 +4,46 @@ function displayModal() {
   const id = urlParams.get('id')
   const name = JSON.parse(sessionStorage.getItem(id)).name
   const contactMe = document.querySelector('.contact-me')
-  contactMe.innerHTML = `<h2 class="contact-me">Contactez-moi <br>${name}</h2>`
-  const modal = document.getElementById('contact-modal')
-  modal.style.display = 'block'
+  contactMe.innerHTML = `Contactez-moi <br>${name}`
+
+  const main = document.querySelector('main')
+  const body = document.querySelector('body')
+  body.classList.add('no-scroll')
+  main.setAttribute('aria-hidden', 'true')
+
+  const closeBtn = document.getElementById('close-modal-btn')
+  const modal = document.querySelector('.modal')
+  modal.classList.replace('closed', 'opened')
+  modal.setAttribute('aria-hidden', 'false')
+  closeBtn.focus()
+  const envoyer = document.querySelector('form button')
+
+  document.addEventListener('keydown', e => {
+    const key = e.key
+    if (modal.getAttribute('aria-hidden') == 'false' && key === 'Escape') closeModal()
+  })
+
+  envoyer.addEventListener('click', e => {
+    e.preventDefault()
+    const inputs = document.querySelectorAll('input')
+    const prenom = inputs[0].value
+    const nom = inputs[1].value
+    const email = inputs[2].value
+    const message = document.querySelector('textarea').value
+    console.log(`Nom: ${nom} \nPrénom: ${prenom} \nEmail: ${email} \nMessage: ${message}`)
+    closeModal()
+  })
 }
 
 function closeModal() {
-  const modal = document.getElementById('contact-modal')
-  modal.style.display = 'none'
+  const main = document.querySelector('main')
+  const body = document.querySelector('body')
+  body.classList.remove('no-scroll')
+  main.setAttribute('aria-hidden', 'false')
+
+  const modal = document.querySelector('.modal')
+  modal.classList.replace('opened', 'closed')
+  modal.setAttribute('aria-hidden', 'true')
+  const contactBtn = document.querySelector('.contact-button')
+  contactBtn.focus()
 }
