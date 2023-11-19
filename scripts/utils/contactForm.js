b452bc1cd7f608f9629c40ec19ef5df382e3f6b2
mabdou <abdoumelouk96@gmail.com>
2023-11-19 17:22:35 +0100

    Fermer la modale avec Echap Corriger l'affichage quand ouverte

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -6,11 +6,13 @@ function displayModal() {
 
   const contactMe = document.querySelector('.contact-me')
   contactMe.innerHTML = `Contactez-moi <br>${name}`
-
+  /*  */
   const main = document.querySelector('main')
   const body = document.querySelector('body')
   body.classList.add('no-scroll')
+  document.querySelector('.sticky').classList.replace('displayed-scrollbar', 'hidden-scrollbar')
   main.setAttribute('aria-hidden', 'true')
+  main.setAttribute('tabindex', '-1')
 
   const closeBtn = document.getElementById('close-modal-btn')
   closeBtn.focus()
@@ -18,6 +20,25 @@ function displayModal() {
   const modal = document.querySelector('.modal')
   modal.classList.replace('closed', 'opened')
   modal.setAttribute('aria-hidden', 'false')
+  modal.setAttribute('tabindex', '0')
+
+  const envoyer = document.querySelector('form button')
+
+  document.addEventListener('keydown', e => {
+    const key = e.key
+    if (modal.getAttribute('aria-hidden') == 'false' && key === 'Escape') closeModal()
+  })
+
+  envoyer.addEventListener('click', e => {
+    e.preventDefault()
+    const inputs = document.querySelectorAll('input')
+    const prenom = inputs[0].value
+    const nom = inputs[1].value
+    const email = inputs[2].value
+    const message = document.querySelector('textarea').value
+    console.log(`Nom: ${nom} \nPrénom: ${prenom} \nEmail: ${email} \nMessage: ${message}`)
+    closeModal()
+  })
 }
 
 function closeModal() {
@@ -25,10 +46,13 @@ function closeModal() {
   const body = document.querySelector('body')
   body.classList.remove('no-scroll')
   main.setAttribute('aria-hidden', 'false')
+  document.querySelector('.sticky').classList.replace('hidden-scrollbar', 'displayed-scrollbar')
 
   const modal = document.querySelector('.modal')
   modal.classList.replace('opened', 'closed')
   modal.setAttribute('aria-hidden', 'true')
+  modal.setAttribute('tabindex', '-1')
+  main.setAttribute('tabindex', '0')
 
   const contactBtn = document.querySelector('.contact-button')
   contactBtn.focus()
