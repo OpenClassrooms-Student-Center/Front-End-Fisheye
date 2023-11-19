ce3caf95456fc2ebf66beef569500b9b61a6cb3f
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2023-11-19 22:57:01 +0100

    feat: ajout de js dans photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,44 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Récupérer l'ID du photographe depuis l'URL
+const urlParams = new URLSearchParams(window.location.search);
+const photographerId = parseInt(urlParams.get('id'));
+
+// Récupérer les données des photographes
+fetch("./data/photographers.json")
+  .then((response) => response.json()) // Convertir la réponse en JSON
+  .then((data) => {
+    const photographers = data.photographers;
+
+    // Trouver le photographe correspondant
+    const photographer = photographers.find((element) => element.id === photographerId);
+
+    // Afficher les détails du photographe
+    if (photographer) {
+      /*const photographerDetails = document.createElement('div');*/
+      const photographerHeader = document.querySelector(".photograph-header");
+      photographerHeader.innerHTML = 
+      `
+      <div class="photograph-text">
+        <h1 class="name">${photographer.name}</h1>
+        <p class="location">${photographer.city}, ${photographer.country}</p>
+        <p class="tagline">${photographer.tagline}</p>
+      </div>
+      <button class="contact_button" onclick="displayModal()">Contactez-moi</button>  
+      <img class="portrait" src="assets/photographers/portrait/${photographer.portrait}" alt="${photographer.name}">
+      `;
+
+      const modalTitleName = document.querySelector(".modal-title_name");
+
+      modalTitleName.append(photographer.name);
+
+
+    } else {
+      // Si aucun photographe correspondant n'est trouvé
+      console.log('Photographer not found');
+    }
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+
+
+  
