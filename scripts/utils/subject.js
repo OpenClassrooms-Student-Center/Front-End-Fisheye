1d4b06cdd0a3b2221533b203449342d9c959f225
nburd01 <n.burdon@protonmail.com>
2023-11-20 20:40:35 +0100

    last version

---
+++ scripts/utils/subject.js
@@ -0,0 +1,28 @@
+class LikeSubject {
+  constructor() {
+    this._observers = [];
+  }
+
+  like(observer) {
+    this._observers.push(observer);
+  }
+
+  unlike(observer) {
+    this._observers = this._observers.filter((obs) => obs !== observer);
+  }
+
+  fire(action) {
+    this._observers.forEach((observer) => observer.update(action));
+  }
+}
+
+export { LikeSubject };
+
+// const mediaLikeHeartElementClicked = document.querySelector(".fas");
+
+// export async function changeHeart() {
+//   console.log(mediaLikeHeartElementClicked);
+//   const heartClicked = mediaLikeHeartElementClicked.style.display;
+
+//   heartClicked === "none" ? "block" : "none";
+// }
