1372c2c35564b079a67ddd9a5f60323d360f7471
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2023-12-01 17:04:53 +0100

    feat: ajout de la class Photographer

---
+++ scripts/models/Photographer.js
@@ -0,0 +1,11 @@
+export default class Photographer {
+    constructor(data) {
+        this.name = data.name;
+        this.id = data.id;
+        this.city = data.city;
+        this.country = data.country;
+        this.tagline = data.tagline;
+        this.price = data.price;
+        this.portrait = data.portrait;
+    }
+}
