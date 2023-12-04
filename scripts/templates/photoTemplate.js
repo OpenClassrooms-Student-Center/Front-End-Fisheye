94469e95894ad41c1a449aec03019b239fa0ae2f
Jean Vuillet <vuillet433@hotmail.com>
2023-12-04 17:12:17 -0500

    integrativ

--- scripts/templates/photoTemplate.js
+++ scripts/templates/photoTemplate.js
@@ -1,17 +1,45 @@
 function photographerTemplate(data) {
-    const  { name, portrait } = data;
+    const  { name,city, country,tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
+
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute(`aria-label`,` ${name}`)
+        const a=document.createElement('a');
+        a.setAttribute('class','link');
+        a.setAttribute(`aria-label`,`lien vers la page de ${name}`)
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute(`alt`,`photo de ${name }`)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p= document.createElement('p');
+        p.setAttribute('class','info');
+
+        const localisation=document.createElement('span');
+        localisation.setAttribute('class','localisation');
+        localisation.textContent=city+', '+country;
+
+        const tag=document.createElement('span');
+        tag.setAttribute('class','tag');
+        tag.textContent=tagline;
+        const prix=document.createElement('span');
+        prix.setAttribute('class','price');
+        prix.textContent=`${price}€/jour`;
+// creating p
+
+        p.appendChild(localisation);
+        p.appendChild(tag);
+        p.appendChild(prix);
+       
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(p);
+        a.appendChild(article);
+        return (a);
     }
     return { name, picture, getUserCardDOM }
 
