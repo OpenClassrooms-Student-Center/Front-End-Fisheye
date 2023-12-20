d84ff537c9e05aebf1cd0d9f19c74d8b66056474
Mehdi Charef <me-charef@hotmail.fr>
2023-12-20 21:49:00 +0100

    fetch datas from json ok

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,58 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+  try {
+    const response = await fetch('./data/photographers.json');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const datas = await response.json();
+    console.log(datas);
+    return datas;
+  } catch (error) {
+    console.log('Erreur lors de la récupération des données:', error);
+  }
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+  let photographers = [
+    {
+      name: 'Ma data test',
+      id: 1,
+      city: 'Paris',
+      country: 'France',
+      tagline: 'Ceci est ma data test',
+      price: 400,
+      portrait: 'account.png',
+    },
+    {
+      name: 'Autre data test',
+      id: 2,
+      city: 'Londres',
+      country: 'UK',
+      tagline: 'Ceci est ma data test 2',
+      price: 500,
+      portrait: 'account.png',
+    },
+  ];
+  // et bien retourner le tableau photographers seulement une fois récupéré
+  return {
+    photographers: [...photographers, ...photographers, ...photographers],
+  };
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+async function displayData(photographers) {
+  const photographersSection = document.querySelector('.photographer_section');
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
