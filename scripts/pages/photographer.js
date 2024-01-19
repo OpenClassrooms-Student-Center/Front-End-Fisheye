b7693e2dd65e95c816a48a3613f52dfdafe37e4c
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-01-19 15:08:32 +0100

    feat: modification de photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -32,27 +32,24 @@ fetch("./data/photographers.json")
       // Récupérer les médias correspondant au photographe
       const photographerMedias = document.querySelector(".photograph-medias");
       const photographerMedia = media.filter((element) => element.photographerId === photographerId);
-
-
-      photographerMedia.forEach((media) => {
-
+      
+      photographerMedia.map((media) => {
         const mediaCard = document.createElement("div");
         mediaCard.classList.add("photograph-media");
 
         const mediaContent = media.image
-        ? ` <img class="" src="./assets/photographers/${photographer.name}/${media.image}" alt="${media.alt}">`
-        : ` <video class="" aria-label="${media.alt}">
-                <source src="./assets/photographers/${photographer.name}/${media.video}" type="video/mp4">
-            </video>`;
+                ? ` <img class="" src="assets/photographers/${photographer.name}/${media.image}" alt="${media.image}">`
+                : ` <video class="" aria-label="${media.alt}">
+                        <source src="./assets/photographers/${photographer.name}/${media.video}" type="video/mp4">
+                    </video>`;
 
-        mediaCard.innerHTML =
-         `${mediaContent}
+            mediaCard.innerHTML = `       
+              ${mediaContent}
           <div class="media-text">
             <h2 class="media-title">${media.title}</h2>
             <span class="media-like">${media.likes} likes</span>
           </div>
-        `;
-
+        `;       
         photographerMedias.appendChild(mediaCard);
       });
     }
