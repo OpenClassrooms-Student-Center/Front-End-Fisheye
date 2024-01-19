fa384ebce1f8b5d88692a3e970d5fcb2ea139c47
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-01-19 15:10:47 +0100

    modification du template photographerCard.js

--- scripts/templates/photographerCard.js
+++ scripts/templates/photographerCard.js
@@ -1,17 +1,18 @@
-function photographerTemplate(data) {
+function photographerCard(data) {
     const { name, id, city, country, tagline, price, portrait } = data;
 
     function getUserCardDOM() {
         
         const article = document.createElement( 'article' );
-        const card =
-                    `<a href="photographer.html?id=${id}" title="${name}">
+        const card =`
+                    <a href="photographer.html?id=${id}" title="${name}">
                         <img class="portrait" src="assets/photographers/portrait/${portrait}" alt="${name}">
                         <h2 class="name">${name}</h2>
                     </a>
                     <p class="location">${city}, ${country}</p>
                     <p class="tagline">${tagline}</p>
-                    <p class="price">${price}€/jour</p>`;
+                    <p class="price">${price}€/jour</p>
+                    `;
 
     article.innerHTML = card;
 
