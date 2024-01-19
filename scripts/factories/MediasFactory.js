855438230da5adbd16cef347f8b0ab5db6938315
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-01-19 17:35:03 +0100

    feat: modification du mediafactory

--- scripts/factories/MediasFactory.js
+++ scripts/factories/MediasFactory.js
@@ -3,12 +3,12 @@ import Video from '../models/Video.js'
 
 // Factory fonction qui crée un média image ou vidéo
 export default class MediasFactory {
-	constructor(data, type) {
+	constructor(data) {
 		// Si le type correspond à image, alors retourne-moi un objet image
-		if (type === 'image') {
+		if (data.image) {
 			return new Image(data)
 		// Sinon retourne-moi un objet video
-		} else if (type === 'video') {
+		} else if (data.video) {
 			return new Video(data)
 		// Une bonne pratique est de déclencher une erreur si le format n'est pas reconnu
 		} else {
