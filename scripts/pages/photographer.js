b1e2d43758b913a232b4448f923d7fb8bb63e5ae
Mehdi Charef <me-charef@hotmail.fr>
2024-01-20 19:49:21 +0100

    photographer page header done

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,7 @@
-//Mettre le code JavaScript lié à la page photographer.html
 import { getPhotographers } from './index.js';
+import { photographerTemplate } from '../templates/photographer.js';
+
+//Mettre le code JavaScript lié à la page photographer.html
 
 // Création d'un objet URL à partir de l'URL actuelle du document
 const url = new URL(document.location);
@@ -10,9 +12,72 @@ const params = url.searchParams;
 // Récupération de la valeur du paramètre 'id'
 const id = params.get('id');
 
-console.log(id);
+console.log('id photographe', id);
 
-async function displayPhotographer() {
-  const datas = await getPhotographers();
-  console.log(datas);
+// async function getPhotographer() {
+//   try {
+//     const datas = await getPhotographers();
+//     const photographer = datas.photographers.filter(
+//       (photographer) => photographer.id === parseInt(id)
+//     );
+//     return photographer;
+//   } catch (error) {
+//     console.error('Erreur lors de la récupération des photographes:', error);
+//   }
+// }
+
+// async function initPhotographerPage() {
+//   const photographerDatas = await getPhotographer();
+
+//   console.log('données photographe', photographerDatas);
+
+//   const photographerModelSinglePage = photographerTemplate(photographerDatas);
+//   const photographerCardDom = photographerModelSinglePage.getUserCardDOM();
+//   const photographerHeader = document.querySelector('.photograph-header');
+//   photographerHeader.appendChild(photographerCardDom);
+// }
+
+// initPhotographerPage();
+
+async function getPhotographer() {
+  try {
+    const datas = await getPhotographers();
+    // Ici, on extrait le premier élément du tableau filtré
+    // ou find
+    const photographer = datas.photographers.filter(
+      (photographer) => photographer.id === parseInt(id)
+    )[0];
+    console.log(datas);
+    const photographerMedias = datas.media.filter(
+      (oneMedia) => oneMedia.photographerId == id
+    );
+    console.log(photographerMedias);
+    const photographerInfos = {
+      photographer,
+      medias: photographerMedias,
+    };
+    return photographerInfos;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des photographes:', error);
+  }
 }
+
+async function initPhotographerPage() {
+  const { photographer, medias } = await getPhotographer();
+  console.log('données photographe', photographer);
+
+  if (photographer) {
+    const photographerModelSinglePage = photographerTemplate(
+      photographer,
+      true,
+      'h1'
+    );
+    const photographerCardDom = photographerModelSinglePage.getUserCardDOM();
+    const photographerHeader = document.querySelector('.photograph-header');
+    photographerHeader.appendChild(photographerCardDom);
+  } else {
+    console.log('Aucun photographe trouvé avec cet ID.');
+  }
+}
+
+initPhotographerPage();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,43 +1,94 @@
-function photographerTemplate(data) {
+export function photographerTemplate(
+  data,
+  isProfilePage = false,
+  titleTag = 'h2'
+) {
   const { name, portrait, tagline, id, city, country, price } = data;
 
   const picture = `assets/photographers/${portrait}`;
+
   function getUserCardDOM() {
-    const article = document.createElement('article');
-    const img = document.createElement('img');
-    img.setAttribute('src', picture);
-    img.setAttribute('alt', `Portrait de ${name}`);
-    const h2 = document.createElement('h2');
-    h2.textContent = name ?? 'nom inconnu';
-    h2.classList.add('photographer__name');
-    const cityP = document.createElement('p');
-    cityP.classList.add('city');
-    cityP.textContent = city ? city + ',' : 'ville inconnue';
-    const countryP = document.createElement('p');
-    // countryP.textContent = country ?? 'pays inconnu'; // si chaîne de caractères vide => string vide
-    countryP.textContent = country || 'pays inconnu'; // => si falsy Une valeur "falsy" inclut false, 0, '' (chaîne vide), null, undefined, et NaN;
-    const locationContainer = document.createElement('div');
-    locationContainer.classList.add('location__container');
-    const quote = document.createElement('p');
-    quote.textContent = tagline ?? '';
-    quote.classList.add('photographer__quote');
-    const priceP = document.createElement('p');
-    priceP.textContent = price ? `${price}€ par jour` : 'prix non spécifié';
-    priceP.classList.add('photographer__price');
-    const cardLink = document.createElement('a');
-    cardLink.classList.add('photographer__link');
-    cardLink.setAttribute('href', `/photographer/${id}`);
-    cardLink.setAttribute('aria-label', `Voir le profil de ${name}`);
-    cardLink.appendChild(img);
-    cardLink.appendChild(h2);
-    article.appendChild(cardLink);
-    //ajout des éléments à article en tant qu'enfant
-    locationContainer.appendChild(cityP);
-    locationContainer.appendChild(countryP);
-    article.appendChild(locationContainer);
-    article.appendChild(quote);
-    article.appendChild(priceP);
-    return article;
+    if (isProfilePage) {
+      const photographHeader = document.querySelector('.photograph-header');
+      const contactButton = document.querySelector('.contact_button');
+      // const article = document.createElement('article');
+      const img = document.createElement('img');
+      img.setAttribute('src', picture);
+      img.setAttribute('alt', `Portrait de ${name}`);
+      const titleTagElement = document.createElement(titleTag); // Utilise le paramètre titleTag
+      titleTagElement.textContent = name ?? 'nom inconnu';
+      titleTagElement.classList.add('photographer__name');
+      const cityP = document.createElement('p');
+      cityP.classList.add('city');
+      cityP.textContent = city ? city + ',' : 'ville inconnue';
+      const countryP = document.createElement('p');
+      // countryP.textContent = country ?? 'pays inconnu'; // si chaîne de caractères vide => string vide
+      countryP.textContent = country || 'pays inconnu'; // => si falsy Une valeur "falsy" inclut false, 0, '' (chaîne vide), null, undefined, et NaN;
+      const locationContainer = document.createElement('div');
+      locationContainer.classList.add('location__container');
+      const quote = document.createElement('p');
+      quote.textContent = tagline ?? '';
+      quote.classList.add('photographer__quote');
+      const priceP = document.createElement('p');
+      priceP.textContent = price ? `${price}€ par jour` : 'prix non spécifié';
+      priceP.classList.add('photographer__price');
+      const cardLink = document.createElement('a');
+      const infosContainer = document.createElement('div');
+      infosContainer.classList.add('infos-container');
+      // cardLink.classList.add('photographer__link');
+      // cardLink.setAttribute('href', `photographer.html?id=${id}`);
+      // cardLink.setAttribute('aria-label', `Voir le profil de ${name}`);
+      //ajout des éléments à article en tant qu'enfant
+      locationContainer.appendChild(cityP);
+      locationContainer.appendChild(countryP);
+      infosContainer.appendChild(titleTagElement);
+      infosContainer.appendChild(locationContainer);
+      infosContainer.appendChild(quote);
+      photographHeader.appendChild(infosContainer);
+      photographHeader.appendChild(contactButton);
+      // photographHeader.appendChild(priceP);
+      photographHeader.appendChild(img);
+      return photographHeader;
+    } else {
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      img.setAttribute('src', picture);
+      img.setAttribute('alt', `Portrait de ${name}`);
+      const titleTagElement = document.createElement(titleTag); // Utilise le paramètre titleTag
+      titleTagElement.textContent = name ?? 'nom inconnu';
+      titleTagElement.classList.add('photographer__name');
+      // const h2 = document.createElement('h2');
+      // h2.textContent = name ?? 'nom inconnu';
+      // h2.classList.add('photographer__name');
+      const cityP = document.createElement('p');
+      cityP.classList.add('city');
+      cityP.textContent = city ? city + ',' : 'ville inconnue';
+      const countryP = document.createElement('p');
+      // countryP.textContent = country ?? 'pays inconnu'; // si chaîne de caractères vide => string vide
+      countryP.textContent = country || 'pays inconnu'; // => si falsy Une valeur "falsy" inclut false, 0, '' (chaîne vide), null, undefined, et NaN;
+      const locationContainer = document.createElement('div');
+      locationContainer.classList.add('location__container');
+      const quote = document.createElement('p');
+      quote.textContent = tagline ?? '';
+      quote.classList.add('photographer__quote');
+      const priceP = document.createElement('p');
+      priceP.textContent = price ? `${price}€ par jour` : 'prix non spécifié';
+      priceP.classList.add('photographer__price');
+      const cardLink = document.createElement('a');
+      cardLink.classList.add('photographer__link');
+      cardLink.setAttribute('href', `photographer.html?id=${id}`);
+      cardLink.setAttribute('aria-label', `Voir le profil de ${name}`);
+      cardLink.appendChild(img);
+      cardLink.appendChild(titleTagElement);
+      article.appendChild(cardLink);
+      //ajout des éléments à article en tant qu'enfant
+      locationContainer.appendChild(cityP);
+      locationContainer.appendChild(countryP);
+      article.appendChild(locationContainer);
+      article.appendChild(quote);
+      article.appendChild(priceP);
+      return article;
+    }
   }
   return {
     name,
