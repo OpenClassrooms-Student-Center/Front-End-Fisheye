291c66ca0f6040594b72763848e5b8b271809ef5
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-01-26 16:21:28 +0100

    ajout de render()

--- scripts/models/Video.js
+++ scripts/models/Video.js
@@ -4,9 +4,14 @@ export default class Video extends Media {
 	constructor(data) {
 		super(data);
 		this.video = data.video;
+		/*this.photographer = data.photographer;*/
 	}
 
 	render() {
-		
+		const video = document.createElement("video");
+		video.src = `assets/photographers/media/${this.video}`;
+		video.alt = this.title;
+	
+		return video;
 	}
 }
