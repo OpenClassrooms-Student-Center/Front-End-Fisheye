0cbff847cdbf79df3ab9112982b8901e6eed3e29
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-01-29 01:10:38 +0100

    feat: modification du js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -45,37 +45,14 @@ fetch("./data/photographers.json")
 
    mediaObjects.map((mediaObject) => {
     
-    /*const mediaCard = mediaObject.createCard();*/
-
-    const mediaThumbnails = mediaObject.render();
-
-
-    /*mediaCard.prepend(mediaThumbnails);*/
+    const mediaCard = mediaObject.render();
 
     mediaWrapper.append(mediaCard);
 
     return mediaCard;
+
   });
-     console.log(mediaObjects)
     
     }
 
   })
-  
-       /* const mediaCard = document.createElement("div");
-        mediaCard.classList.add("photograph-media");
-
-        const mediaContent = media.image
-                ? ` <img class="" src="assets/photographers/${photographer.name}/${media.image}" alt="${media.image}">`
-                : ` <video class="" aria-label="${media.alt}">
-                        <source src="./assets/photographers/${photographer.name}/${media.video}" type="video/mp4">
-                    </video>`;
-
-            mediaCard.innerHTML = `       
-              ${mediaContent}
-          <div class="media-text">
-            <h2 class="media-title">${media.title}</h2>
-            <span class="media-like">${media.likes} likes</span>
-          </div>
-        `;       
-        photographerMedias.appendChild(mediaCard);*/
