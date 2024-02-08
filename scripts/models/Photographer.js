8befe9bb5e54d16322d74987d3b83a184cb4d305
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-02-08 18:13:51 +0100

    feat: maj de Photographer.js

--- scripts/models/Photographer.js
+++ scripts/models/Photographer.js
@@ -8,4 +8,25 @@ export default class Photographer {
         this.price = data.price;
         this.portrait = data.portrait;
     }
+
+
+    createPhotographerHeader() {
+
+        const photographerHeader = document.querySelector(".photograph-header");
+
+        photographerHeader.innerHTML = ` 
+                                      <div class="photograph-text">
+                                        <h1 class="name">${this.name}</h1>
+                                        <p class="location">${this.city}, ${this.country}</p>
+                                        <p class="tagline">${this.tagline}</p>
+                                      </div>
+                                      <button class="contact_button" onclick="displayModal()">Contactez-moi</button>  
+                                      <img class="portrait" src="assets/photographers/portrait/${this.portrait}" alt="${this.name}">
+                                      `;
+
+        const modalTitleName = document.querySelector(".modal-title_name");
+        modalTitleName.append(this.name);
+
+    }
+
 }
