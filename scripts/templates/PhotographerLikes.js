505bea5f55b4d1c076d14b623f5bc79abc3af800
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-02-26 01:16:01 +0100

    feat: maj

--- scripts/templates/PhotographerLikes.js
+++ scripts/templates/PhotographerLikes.js
@@ -5,11 +5,12 @@ export default class PhotographerLikes {
     }
     
     render() {      
+            const medias = this.medias
             const photographer = this.photographer
             const photographerLikesAndPrice = document.querySelector(".likes_and_price");
 
             // Accumuler le nombre total de likes
-            const totalLikes = this.medias.reduce((total, media) => total + media.likes, 0);
+            const totalLikes = medias.reduce((total, media) => total + media.likes, 0);
            
             const LikesAndPrice = `<div class="photographer_likes">
                                       <span class="total_likes">${totalLikes}</span><img src="./assets/icons/heart-black.svg" class="heart"></img>
