1a639c2ea1f3aaa3e3cc0551f207cd4671bb6a0d
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-03-01 15:54:55 +0100

    feat: suppression de ligne vide

--- scripts/models/Image.js
+++ scripts/models/Image.js
@@ -10,15 +10,14 @@ export default class Image extends Media {
 	render() {
 		const mediaWrapper = document.querySelector(".photograph-medias");
 		const mediaLink = document.createElement("a");
-		mediaLink.classList.add("media_link");
 		const image = `<img src="${this.src}" alt="${this.title}"/>`
 		const imageCard = this.createCard();
-
+		
+		mediaLink.classList.add("media_link");
 		mediaLink.href = "#";
 		mediaLink.innerHTML = image;
 
 		imageCard.prepend(mediaLink);
 		mediaWrapper.append(imageCard);
-
 	}
 }
--- scripts/models/Video.js
+++ scripts/models/Video.js
@@ -12,14 +12,13 @@ export default class Video extends Media {
 		const mediaLink = document.createElement("a");
 		const video = `<video src="${this.src}"/>`
 		const videoCard = this.createCard();
-		
+
 		mediaLink.classList.add("media_link");
 		mediaLink.href = "#";
 		mediaLink.innerHTML = video;
 
 		videoCard.prepend(mediaLink);
 		mediaWrapper.append(videoCard);
-
 	}
 	
 }
